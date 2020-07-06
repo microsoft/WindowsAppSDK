4e3f4fee8bddd8f63c3225c23483b21fb3cbcff0
Jon Wiswall <jonwis@microsoft.com>
2020-07-06 13:27:12 -0700

    Add initial solution and code examples (#102)
    * Initial commit of Reunion Common Entrypoint
    
    * Rename Common to SampleWinRT
    
    * Care and Feeding of WinRT code
    
    * Add ARM64 target
    
    * Flat C API sample
    
    * Extend sample slightly, fix publishing files
    
    * Update References
    
    * Add a simple test (that doesn't work yet)
    
    * Move to a shared project
    
    * Replace "Universal" DLL with a regular C++/WinRT DLL
    Publish "flat C" headers to common output directory
    Ensure test execution can find the implementation binary
    
    * Change namespaces, fix up solutions, make test work
    
    * Get a UWP unit test building
    
    * Update to new cppwinrt package
    
    * Include ARM64 in builds, add 'build everything' script
    
    * Add means to test UWP, identity, etc in one shared file
    
    * Rename test projects
    
    * PR feedback
    
    * PR feedback
    
    * PR feedback

---
+++ test/CppTest/pch.h
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#ifndef PCH_H
+#define PCH_H
+#endif //PCH_H
+
+#include <wil/result.h>
+#include <wil/cppwinrt.h>
+#include <wil/resource.h>
+#include "CppUnitTest.h"
+
+// Test-specific headers, lifted here
+#include <SampleFlatC.h>
+#include <winrt/Microsoft.ProjectReunion.h>
