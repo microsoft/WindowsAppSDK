66ee117132896fa58157d03d1a3eb04b438ded91
Elliot H Omiya (EHO) <eho@microsoft.com>
2020-08-11 11:43:45 -0700

    fixup microsoft.project.reunion => microsoft.projectreunion

--- build/NuSpecs/MakeAllAppx.cmd
+++ build/NuSpecs/MakeAllAppx.cmd
@@ -2,27 +2,27 @@
 REM %~dp0 is position of script file
 
 
-if exist %~dp0"..\..\BuildOutput\Debug\x86\Microsoft.Project.Reunion" (
+if exist %~dp0"..\..\BuildOutput\Debug\x86\Microsoft.ProjectReunion" (
 	call %~dp0..\..\tools\MakeAppxHelper.cmd x86 debug %*
 )
-if exist %~dp0"..\..\BuildOutput\Release\x86\Microsoft.Project.Reunion" (
+if exist %~dp0"..\..\BuildOutput\Release\x86\Microsoft.ProjectReunion" (
 	call %~dp0..\..\tools\MakeAppxHelper.cmd x86 release%*
 )
-if exist %~dp0"..\..\BuildOutput\Debug\x64\Microsoft.Project.Reunion" (
+if exist %~dp0"..\..\BuildOutput\Debug\x64\Microsoft.ProjectReunion" (
   call %~dp0..\..\tools\MakeAppxHelper.cmd x64 debug %*
 )
-if exist %~dp0"..\..\BuildOutput\Release\x64\Microsoft.Project.Reunion" (
+if exist %~dp0"..\..\BuildOutput\Release\x64\Microsoft.ProjectReunion" (
   call %~dp0..\..\tools\MakeAppxHelper.cmd x64 release %*
 )
-if exist %~dp0"..\..\BuildOutput\Debug\arm\Microsoft.Project.Reunion" (
+if exist %~dp0"..\..\BuildOutput\Debug\arm\Microsoft.ProjectReunion" (
   call %~dp0..\..\tools\MakeAppxHelper.cmd arm debug %*
 )
-if exist %~dp0"..\..\BuildOutput\Release\arm\Microsoft.Project.Reunion" (
+if exist %~dp0"..\..\BuildOutput\Release\arm\Microsoft.ProjectReunion" (
   call %~dp0..\..\tools\MakeAppxHelper.cmd arm release %*
 )
-if exist %~dp0"..\..\BuildOutput\Debug\arm64\Microsoft.Project.Reunion" (
+if exist %~dp0"..\..\BuildOutput\Debug\arm64\Microsoft.ProjectReunion" (
   call %~dp0..\..\tools\MakeAppxHelper.cmd arm64 debug %*
 )
-if exist %~dp0"..\..\BuildOutput\Release\arm64\Microsoft.Project.Reunion" (
+if exist %~dp0"..\..\BuildOutput\Release\arm64\Microsoft.ProjectReunion" (
   call %~dp0..\..\tools\MakeAppxHelper.cmd arm64 release %*
 )
