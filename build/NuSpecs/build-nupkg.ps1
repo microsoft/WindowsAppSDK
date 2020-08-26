616e1be2f055fd3aeb5603b54db7eba59f53d7fb
Elliot H Omiya (EHO) <eho@microsoft.com>
2020-08-26 10:35:00 -0700

    meta package nuspec and build

--- build/NuSpecs/build-nupkg.ps1
+++ build/NuSpecs/build-nupkg.ps1
@@ -113,8 +113,31 @@ $CommonNugetArgs = "-properties `"BuildOutput=$BuildOutput``;ID=$nupkgtitle``;RU
 
 $NugetArgs = "$CommonNugetArgs -OutputDirectory $OutputDir"
 
+#
+# Build Project Reunion package (with actual contents, i.e. not metapackage)
+#
+
 $nugetExe = "$scriptDirectory\..\..\tools\NugetWrapper.cmd"
 $NugetCmdLine = "$nugetExe pack ProjectReunion.nuspec $NugetArgs -version $version"
+Write-Host 'Building Project Reunion package'
+Write-Host $NugetCmdLine
+Invoke-Expression $NugetCmdLine
+if ($lastexitcode -ne 0)
+{
+    Write-Host "Nuget returned $lastexitcode"
+    Exit $lastexitcode; 
+}
+Write-Host
+
+#
+# Build Project Reunion package meta package (no direct contents, only references)
+#
+
+$nupkgtitle = "Microsoft.ProjectReunion.MetaPackage"
+
+$nugetExe = "$scriptDirectory\..\..\tools\NugetWrapper.cmd"
+$NugetCmdLine = "$nugetExe pack ProjectReunionMetaPackage.nuspec $NugetArgs -version $version"
+Write-Host 'Building Project Reunion Meta Package'
 Write-Host $NugetCmdLine
 Invoke-Expression $NugetCmdLine
 if ($lastexitcode -ne 0)
