d81711336228da6810f51dd7118a67d104bb20c0
Howard Kapustein <howardk@microsoft.com>
2020-09-08 19:42:20 -0700

    Fixed Main.Sidecar's manifest and logos. Updated makemsix.cmd to copy Assets\* if exists, and make logo.png optional (should have one or the other, but probably not both)

--- tools/makemsix.cmd
+++ tools/makemsix.cmd
@@ -8,7 +8,7 @@ SET TargetDir=%4
 SET TargetFileName=%5
 SET TargetName=%6
 
-SET OutDirMsix=%OutDir%\msix
+SET OutDirMsix=%OutDir%msix\
 SET OutMsix=%OutDir%%TargetName%.msix
 
 ECHO rd %OutDirMsix% /s /q
@@ -17,14 +17,23 @@ rd %OutDirMsix% /s /q
 ECHO md %OutDirMsix%
 md %OutDirMsix%
 
-ECHO copy %ProjectDir%appxmanifest.template %OutDirMsix%\appxmanifest.xml
-copy %ProjectDir%appxmanifest.template %OutDirMsix%\appxmanifest.xml
+ECHO copy %ProjectDir%appxmanifest.template %OutDirMsix%appxmanifest.xml
+copy %ProjectDir%appxmanifest.template %OutDirMsix%appxmanifest.xml
 
-ECHO copy %ProjectDir%logo.png %OutDirMsix%\logo.png
-copy %ProjectDir%logo.png %OutDirMsix%\logo.png
+IF NOT EXIST %ProjectDir%logo.png GOTO SkipLogo
+ECHO copy %ProjectDir%logo.png %OutDirMsix%logo.png
+copy %ProjectDir%logo.png %OutDirMsix%logo.png
+:SkipLogo
 
-ECHO copy %TargetDir%\%TargetFileName% %OutDirMsix%\%TargetFileName%
-copy %TargetDir%\%TargetFileName% %OutDirMsix%\%TargetFileName%
+IF NOT EXIST %ProjectDir%Assets GOTO SkipAssets
+ECHO MD %OutDirMsix%Assets
+MD %OutDirMsix%Assets
+ECHO copy %ProjectDir%Assets\* %OutDirMsix%Assets\*
+copy %ProjectDir%Assets\* %OutDirMsix%Assets\*
+:SkipAssets
+
+ECHO copy %TargetDir%%TargetFileName% %OutDirMsix%%TargetFileName%
+copy %TargetDir%%TargetFileName% %OutDirMsix%%TargetFileName%
 
 ECHO makeappx.exe pack /v /h SHA256 /d %OutDirMsix% /p %OutMsix%
 makeappx.exe pack /v /o /h SHA256 /d %OutDirMsix% /p %OutMsix%
@@ -35,8 +44,8 @@ REM SET SignCert=136020001
 REM SET SignPublisher=CN=Microsoft Corporation, O=Microsoft Corporation, L=Redmond, S=Washington, C=US
 REM SET SignFile=%OutMsix%
 REM SimpleSign.exe -i:%SignFile% -c:%SignCert% -s:"%SignPublisher%"
-ECHO signtool.exe sign /a /v /fd SHA256 /f %ProjectDir%\build\MSTest.pfx %OutMsix%
-signtool.exe sign /a /v /fd SHA256 /f %SolutionDir%\build\MSTest.pfx %OutMsix%
+ECHO signtool.exe sign /a /v /fd SHA256 /f %ProjectDir%build\MSTest.pfx %OutMsix%
+signtool.exe sign /a /v /fd SHA256 /f %SolutionDir%build\MSTest.pfx %OutMsix%
 
 :TheEnd
 ENDLOCAL
