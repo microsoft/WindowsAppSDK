ca150d0f0f8ece2ecbe0af5fb9f87912a1a76385
Howard Kapustein <howardk@microsoft.com>
2020-09-11 16:01:02 -0700

    Main.Sidecar3 progress

---
+++ test/DynamicDependency/data/Main.Sidecar3/Main.Sidecar3.ProxyStub/dllmain.cpp
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#include "pch.h"
+
+BOOL APIENTRY DllMain( HMODULE /*hModule*/,
+                       DWORD  ul_reason_for_call,
+                       LPVOID /*lpReserved*/
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
