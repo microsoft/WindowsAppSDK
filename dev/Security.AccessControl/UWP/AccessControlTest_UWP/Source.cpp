b56d34091ec1796d22a9debae90be707275b0d42
Ryan Shepherd <ryansh@microsoft.com>
2020-09-23 15:14:51 -0700

    Add AccessControl unit test skeleton

---
+++ dev/Security.AccessControl/UWP/AccessControlTest_UWP/Source.cpp
@@ -0,0 +1,14 @@
+﻿#include "pch.h"
+#include <WexTestClass.h>
+
+struct AccessControlTest
+{
+    TEST_CLASS(AccessControlTest);
+
+    TEST_METHOD(SimpleTest);
+};
+
+void AccessControlTest::SimpleTest()
+{
+
+}
