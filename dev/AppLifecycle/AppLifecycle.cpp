fff39faa28b8633189e23fb777acddf371bde279
Scott Darnell <20483794+aeloros@users.noreply.github.com>
2020-10-01 15:17:54 -0700

    Initial template for the AppLifecycle object and it's tests.

---
+++ dev/AppLifecycle/AppLifecycle.cpp
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#include <pch.h>
+#include <AppLifecycle.h>
+#include <AppLifecycle.g.cpp>
+
+namespace winrt::Microsoft::ProjectReunion::implementation
+{
+    Windows::ApplicationModel::Activation::IActivatedEventArgs AppLifecycle::GetActivatedEventArgs()
+    {
+        return nullptr;
+    }
+}
---
+++ test/AppLifecycle/ApiTests.cpp
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#include "pch.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ProjectReunionCppTest
+{
+    TEST_CLASS(AppLifecycleApiTests)
+    {
+#if PRTEST_MODE_UWP
+        TEST_METHOD(GetActivatedEventArgsNotNull)
+        {
+            Assert::IsTrue(!!winrt::Microsoft::ProjectReunion::AppLifecycle::GetActivatedEventArgs());
+        }
+#else
+        TEST_METHOD(GetActivatedEventArgsNotNull)
+        {
+            Assert::IsTrue(!!winrt::Microsoft::ProjectReunion::AppLifecycle::GetActivatedEventArgs());
+        }
+#endif
+    };
+}
