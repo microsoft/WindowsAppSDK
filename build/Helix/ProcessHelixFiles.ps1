bb88b83fa95f2620ae4a30f1ae451ffe03fd3a19
jorvan <jorvan@microsoft.com>
2020-10-15 10:31:48 -0700

    different URI

--- build/Helix/ProcessHelixFiles.ps1
+++ build/Helix/ProcessHelixFiles.ps1
@@ -53,8 +53,9 @@ $webClient = New-Object System.Net.WebClient
 
 foreach ($testRun in $testRuns.value)
 {
-    $testRunResultsUri = "$($testRun.url)/results?api-version=5.0"
-    $testResults = Invoke-RestMethod -Uri "$($testRun.url)/results?api-version=5.1" -Method Get -Headers $azureDevOpsRestApiHeaders
+    Write-Host "Retrieving test results..."
+    $testRunResultsUri = "$($testRun.url)/results?`$top=$sizeOfPage&`$skip=$numItemsToSkip&api-version=5.1"
+    $testResults = Invoke-RestMethod -Uri $testRunResultsUri -Method Get -Headers $azureDevOpsRestApiHeaders
     foreach ($testResult in $testResults.value)
     {
         $info = ConvertFrom-Json $testResult.comment
