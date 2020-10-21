2cbd51e0af1c3bcf4e572648e1b866dd96abf0d8
Hui Chen <huichen@microsoft.com>
2020-10-21 13:12:11 -0700

    remove config

--- dev/MRTCore/build/MSBuildFunctions.psm1
+++ dev/MRTCore/build/MSBuildFunctions.psm1
@@ -12,7 +12,7 @@ $vsconfigPath = "$repoRoot\.vsconfig_buildtools"
 # This URI corresponds to MSBuild 16.8. If this ever changes, change the MSBuildVer
 # property above
 $MSBuildInstallURI = "https://download.visualstudio.microsoft.com/download/pr/ecb3860e-5c66-4a3f-8acf-ef190d5f9a96/18162a4d36635d0958bf56654d4a03b211dcd8474b3a4036c8a0a0fb6a0eb053/vs_BuildTools.exe"
-$MSBuildInstallParams = "--norestart --quiet --wait --force --installPath {0} --config $vsconfigPath"
+$MSBuildInstallParams = "--norestart --quiet --wait --force --installPath {0}"
 $MSBuildUninstallParams = "uninstall --norestart --quiet --wait --force --installPath {0}"
 
 function Download-MSBuild([string]$OutFile)
