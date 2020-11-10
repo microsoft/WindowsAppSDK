a38254a6241e5f940e4b9cc7f626c40489d98bd9
Brian Alker <brial@microsoft.com>
2020-11-09 18:29:42 -0800

    Basic stub impl of 2 properties.

---
+++ dev/PowerNotifications/PowerNotifications.cpp
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#include <pch.h>
+#include <PowerNotifications.h>
+#include <PowerManager.g.cpp>
+
+
+namespace winrt::Microsoft::ProjectReunion::implementation
+{
+    winrt::event<Windows::Foundation::EventHandler<Windows::Foundation::IInspectable>> PowerManager::m_energySaverStatusChangedEvent;
+    winrt::event<Windows::Foundation::EventHandler<Windows::Foundation::IInspectable>> PowerManager::m_batteryStatusChangedEvent;
+}
