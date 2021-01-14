8b43e242f55409c64b1ba37137e3bbff022a1e63
Rohan Palaniappan <rohanp@microsoft.com>
2021-01-14 15:15:58 -0800

    Change of approach - C++/WinRT generated files will be created on app build.

--- dev/MRTCore/build/debug-pipeline.cmd
+++ dev/MRTCore/build/debug-pipeline.cmd
@@ -7,6 +7,3 @@ set DOTNET_ROOT
 set DOTNET_ROOT(x86)
 set DOTNET_MULTILEVEL_LOOKUP
 set DOTNET_INSTALL_DIR
-
-powershell "Tree .\dev\MRTCore\ /F | Select-Object -Skip 2"
-powershell "Tree .\BuildOutput\ /F | Select-Object -Skip 2"
