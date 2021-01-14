68acf63fcead21aecad0f7964eb6dbbd5e8084c2
Rohan Palaniappan <rohanp@microsoft.com>
2021-01-14 10:38:35 -0800

    Printing out some folder structures to see where the Generated Files\ are...

--- dev/MRTCore/build/debug-pipeline.cmd
+++ dev/MRTCore/build/debug-pipeline.cmd
@@ -8,6 +8,5 @@ set DOTNET_ROOT(x86)
 set DOTNET_MULTILEVEL_LOOKUP
 set DOTNET_INSTALL_DIR
 
-powershell ls -r Microsoft.ApplicationModel.Resources.0.h
-powershell ls -r Microsoft.ApplicationModel.Resources.h
-powershell ls -r module.g.cpp
+powershell Tree .\dev\MRTCore\ /F | Select-Object -Skip 2
+powershell Tree .\BuildOutput\ /F | Select-Object -Skip 2
