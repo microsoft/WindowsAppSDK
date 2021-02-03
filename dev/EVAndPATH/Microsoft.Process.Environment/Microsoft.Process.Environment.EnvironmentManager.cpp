044b0e5b2861c6a2d7e6eb9031c201a814dabd97
Darren Hoehna <dahoehna@microsoft.com>
2021-02-03 14:28:51 -0800

    IDL and project skeleton

---
+++ dev/EVAndPATH/Microsoft.Process.Environment/Microsoft.Process.Environment.EnvironmentManager.cpp
@@ -0,0 +1,51 @@
+﻿#include "pch.h"
+#include "Microsoft.Process.Environment.EnvironmentManager.h"
+#include "EnvironmentManager.g.cpp"
+
+namespace winrt::Microsoft::Process::Environment::implementation
+{
+
+    EnvironmentManager::EnvironmentManager(Scope const& scope)
+            : m_Scope(scope) { }
+
+    Microsoft::Process::Environment::EnvironmentManager EnvironmentManager::GetForProcess()
+    {
+        throw hresult_not_implemented();
+    }
+    Microsoft::Process::Environment::EnvironmentManager EnvironmentManager::GetForUser()
+    {
+        throw hresult_not_implemented();
+    }
+    Microsoft::Process::Environment::EnvironmentManager EnvironmentManager::GetForMachine()
+    {
+        throw hresult_not_implemented();
+    }
+    Windows::Foundation::Collections::IMapView<hstring, hstring> EnvironmentManager::GetEnvironmentVariables()
+    {
+        throw hresult_not_implemented();
+    }
+    hstring EnvironmentManager::GetEnvironmentVariable(hstring const& name)
+    {
+        throw hresult_not_implemented();
+    }
+    void EnvironmentManager::SetEnvironmentVariable(hstring const& name, hstring const& value)
+    {
+        throw hresult_not_implemented();
+    }
+    void EnvironmentManager::AppendToPath(hstring const& path)
+    {
+        throw hresult_not_implemented();
+    }
+    void EnvironmentManager::RemoveFromPath(hstring const& path)
+    {
+        throw hresult_not_implemented();
+    }
+    void EnvironmentManager::AppendToPathExt(hstring const& pathExt)
+    {
+        throw hresult_not_implemented();
+    }
+    void EnvironmentManager::RemoveFromPathExt(hstring const& pathExt)
+    {
+        throw hresult_not_implemented();
+    }
+}
---
+++ dev/EVAndPATH/Microsoft.Process.Environment/pch.h
@@ -0,0 +1,2 @@
+﻿#pragma once
+#include <winrt/Windows.Foundation.Collections.h>
