f5ca316046736d3dded21d27389e7c5353fbbf92
Paul Purifoy <purifoypaul@microsoft.com>
2021-02-18 12:00:36 -0800

    Added unpackaged/packaged WPN TestApp
    - A basic TestApp that has dependency of ProjectReunion_DLL
    - MSIX packager that allows testing the app as a packaged application

---
+++ WPNWin32TestApp/main.cpp
@@ -0,0 +1,12 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace winrt;
+using namespace winrt::Windows::Foundation;
+using namespace winrt::Microsoft::ProjectReunion;
+
+int main()
+{
+    auto args = AppLifecycle::GetActivatedEventArgs();
+    std::cout << "Hello World!\n";
+    return 0;
+}
