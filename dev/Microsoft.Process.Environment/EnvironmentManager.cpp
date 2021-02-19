2eebcbceac448c9ef892017ecb07c975c50f4204
Darren Hoehna <dhoehna@yahoo.com>
2021-02-19 14:12:51 -0800

    IDL and project skeleton (#416)
    * IDL and project skeleton
    
    * Removing commented out code
    
    * Moving code over to a shared Package
    
    * Renaming two methods.  Changing the label in vcxproj to a better name
    
    * Removing RuntimeComponent1
    
    Co-authored-by: Darren Hoehna <dahoehna@microsoft.com>

---
+++ dev/Microsoft.Process.Environment/EnvironmentManager.cpp
@@ -0,0 +1,47 @@
+﻿#include "pch.h"
+#include <EnvironmentManager.h>
+#include <EnvironmentManager.g.cpp>
+
+namespace winrt::Microsoft::ProjectReunion::implementation
+{
+    Microsoft::ProjectReunion::EnvironmentManager EnvironmentManager::GetForProcess()
+    {
+        throw hresult_not_implemented();
+    }
+    Microsoft::ProjectReunion::EnvironmentManager EnvironmentManager::GetForUser()
+    {
+        throw hresult_not_implemented();
+    }
+    Microsoft::ProjectReunion::EnvironmentManager EnvironmentManager::GetForMachine()
+    {
+        throw hresult_not_implemented();
+    }
+    Windows::Foundation::Collections::IMapView<hstring, hstring> EnvironmentManager::GetEnvironmentVariables()
+    {
+        throw hresult_not_implemented();
+    }
+    hstring EnvironmentManager::GetEnvironmentVariable(hstring const& name)
+    {
+        throw hresult_not_implemented();
+    }
+    void EnvironmentManager::SetEnvironmentVariable(hstring const& name, hstring const& value)
+    {
+        throw hresult_not_implemented();
+    }
+    void EnvironmentManager::AppendToPath(hstring const& path)
+    {
+        throw hresult_not_implemented();
+    }
+    void EnvironmentManager::RemoveFromPath(hstring const& path)
+    {
+        throw hresult_not_implemented();
+    }
+    void EnvironmentManager::AddExecutableFileExtension(hstring const& pathExt)
+    {
+        throw hresult_not_implemented();
+    }
+    void EnvironmentManager::RemoveExecutableFileExtension(hstring const& pathExt)
+    {
+        throw hresult_not_implemented();
+    }
+}
---
+++ dev/Microsoft.Process.Environment/EnvironmentManager.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include <EnvironmentManager.g.h>
+
+namespace winrt::Microsoft::ProjectReunion::implementation
+{
+    struct EnvironmentManager : EnvironmentManagerT<EnvironmentManager>
+    {
+        EnvironmentManager() = default;
+
+        static Microsoft::ProjectReunion::EnvironmentManager GetForProcess();
+        static Microsoft::ProjectReunion::EnvironmentManager GetForUser();
+        static Microsoft::ProjectReunion::EnvironmentManager GetForMachine();
+        Windows::Foundation::Collections::IMapView<hstring, hstring> GetEnvironmentVariables();
+        hstring GetEnvironmentVariable(hstring const& name);
+        void SetEnvironmentVariable(hstring const& name, hstring const& value);
+        void AppendToPath(hstring const& path);
+        void RemoveFromPath(hstring const& path);
+        void AddExecutableFileExtension(hstring const& pathExt);
+        void RemoveExecutableFileExtension(hstring const& pathExt);
+    };
+}
+namespace winrt::Microsoft::ProjectReunion::factory_implementation
+{
+    struct EnvironmentManager : EnvironmentManagerT<EnvironmentManager, implementation::EnvironmentManager>
+    {
+    };
+}
