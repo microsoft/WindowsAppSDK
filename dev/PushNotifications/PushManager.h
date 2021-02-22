9441173546ca844cd4fc60fd013d4d5a897f5381
Venkata Sharath Chandra Manchala <vemancha@microsoft.com>
2021-02-22 15:11:51 -0800

    Address comments

---
+++ dev/PushNotifications/PushManager.h
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#pragma once
+#include "PushManager.g.h"
+
+namespace winrt::Microsoft::ProjectReunion::implementation
+{
+    struct PushManager
+    {
+        PushManager() = default;
+
+        static Windows::Foundation::IAsyncOperationWithProgress<Microsoft::ProjectReunion::ChannelResult, Microsoft::ProjectReunion::ChannelResult> CreateChannelAsync(hstring remoteId);
+    };
+}
+namespace winrt::Microsoft::ProjectReunion::factory_implementation
+{
+    struct PushManager : PushManagerT<PushManager, implementation::PushManager>
+    {
+    };
+}
