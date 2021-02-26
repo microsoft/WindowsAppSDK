26d124c2b2e68a56915e6b7606a83a6c54d88065
Paul Purifoy <33183370+pmpurifoy@users.noreply.github.com>
2021-02-26 12:37:37 -0800

    Added unpackaged/packaged WPN TestApp (#457)
    * Added unpackaged/packaged WPN TestApp
    
    - A basic TestApp that has dependency of ProjectReunion_DLL
    - MSIX packager that allows testing the app as a packaged application
    
    * Removed the TestAppCopy from .sln
    
    - AppLifecycleTestApp - Copy was being built in the soltution
    
    * Renamed TestApp
    
    - Renamed TestApp to not use internal acronyms
    
    * Revert "Renamed TestApp"
    
    This reverts commit 3e4386f2b0cf5d2b64209c5c47f4affb8d1e7ac8.
    
    * Revert "Removed the TestAppCopy from .sln"
    
    This reverts commit 091b79aa25317588bc297a74f66437acae79ea97.
    
    * Revert "Added unpackaged/packaged WPN TestApp"
    
    This reverts commit f5ca316046736d3dded21d27389e7c5353fbbf92.
    
    * Readded AppLifecycleTestApp
    
    - Readded the AppLifecycleTestApp
    - Placed the packager and win32 app in TestApps folder

---
+++ test/TestApps/PushNotificationsWin32App/main.cpp
@@ -0,0 +1,12 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace winrt;
+using namespace winrt::Windows::Foundation;
+using namespace winrt::Microsoft::ProjectReunion;
+
+int main()
+{
+    // Add your code here.
+    std::cout << "Hello World!\n";
+    return 0;
+}
