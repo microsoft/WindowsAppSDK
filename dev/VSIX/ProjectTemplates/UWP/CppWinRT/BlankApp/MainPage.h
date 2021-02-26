21f2fdab7ef85e8ad5d91315ad4c6bb3382ba102
Scott Jones <sjones@microsoft.com>
2021-02-26 15:32:05 -0800

    Initial checkin of Reunion VSIX, ported from WinUI VSIX sources

---
+++ dev/VSIX/ProjectTemplates/UWP/CppWinRT/BlankApp/MainPage.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "MainPage.g.h"
+
+namespace winrt::$safeprojectname$::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void myButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::$safeprojectname$::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
