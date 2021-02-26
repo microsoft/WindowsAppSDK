153654dd81bc28e7b16cfe6bdf25eab94aff5285
Scott Darnell <20483794+aeloros@users.noreply.github.com>
2021-02-25 18:06:33 -0800

    copyright

--- dev/AppLifecycle/shared_memory.h
+++ dev/AppLifecycle/shared_memory.h
@@ -1,4 +1,6 @@
-﻿#pragma once
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+#pragma once
 
 template <typename T>
 class shared_memory
