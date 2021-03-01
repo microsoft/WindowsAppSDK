bfaadc07e6a1075c5f81f91b42026ead7eb6ddb0
Josh Cain <jcain@microsoft.com>
2021-03-01 13:49:52 -0800

    committing just to push to another machine. all placeholders

---
+++ dev/Theming/Theming.cpp
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#include <pch.h>
+#include <Theming.h>
+#include <Theming.g.cpp>
+
+#include "LaunchActivatedEventArgs.h"
+#include "ProtocolActivatedEventArgs.h"
+#include "FileActivatedEventArgs.h"
+#include "Association.h"
+
+namespace winrt::Microsoft::ProjectReunion::implementation
+{
+    bool Theming::IsDarkModeEneabled()
+    {
+    }
+
+    bool Theming::IsDarkModeActive()
+    {
+    }
+}
---
+++ dev/Theming/Theming.h
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+#pragma once
+
+#include <Theming.g.h>
+
+namespace winrt::Microsoft::ProjectReunion::implementation
+{
+    struct Theming
+    {
+        Theming() = default;
+        static bool IsDarkModeEneabled();
+        static bool IsDarkModeActive();
+    };
+}
+
+namespace winrt::Microsoft::ProjectReunion::factory_implementation
+{
+    struct Theming : ThemingT<Theming, implementation::Theming>
+    {
+    };
+}
