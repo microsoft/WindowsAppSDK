38d2414388ee4cb50f9c50d8ff60535e59fc3d5f
David Bennett <dbennett-msft@outlook.com>
2021-03-04 13:33:47 -0800

    Initial creation of the standalone Project Reunion Installer (#444)
    * Initial creation of the standalone Project Reunion Installer
    
    * Add tests and PR feedback
    
    Adds tests and changes from PR feedback.
    
    * PR Feedback updates
    
    * Additional PR Feedback updates
    
    * Update exception handling
    
    Moved code to be exception-based and caught at the highest layer of install.
    
    * move and rename solution paths, add test packages
    
    Refactored the test package generation into a separate solution to be run when needed to generate test packages, and added the test packages, which are small and do not need to be built every time for easier testing and faster building. Also moved the installer up a level as it is a separate set of solutions and not part of the main reunion solution and following same naming patterns.
    
    * Fix a few nits in install.h
    
    * Remove old renamed file
    
    help.* was renamed to console.*, removing obsolete version.
    
    * Fix some nits

---
+++ installer/dev/main.cpp
@@ -0,0 +1,36 @@
+﻿#include "pch.h"
+#include "console.h"
+#include "install.h"
+#include "resource.h"
+
+using namespace winrt;
+
+int wmain(int argc, wchar_t *argv[])
+{
+    init_apartment();
+
+    bool quiet = false;
+
+    for (int i = 1; i < argc; ++i)
+    {
+        auto arg = std::wstring_view(argv[i]);
+        if ((arg == L"-q") || (arg == L"--quiet"))
+        {
+            quiet = true;
+        }
+        else if ((arg == L"-?") || (arg == L"--help"))
+        {
+            ShowHelp();
+            return 0;
+        }
+        else
+        {
+            std::wcerr << "Unknown argument: " << arg.data() << std::endl;
+            ShowHelp();
+            return ERROR_BAD_ARGUMENTS;
+        }
+    }
+
+    return ProjectReunionInstaller::DeployPackages(quiet);
+}
+
