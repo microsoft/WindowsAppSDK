3c6e8bcfa0c2ad82b671b44ea03d05467168eda9
Venkata Sharath Chandra Manchala <vemancha@microsoft.com>
2021-03-07 19:22:51 -0800

    Lock only the remoteIdList

--- dev/PushNotifications/PushManager.cpp
+++ dev/PushNotifications/PushManager.cpp
@@ -11,7 +11,6 @@
 #include <iostream>
 #include <ChannelResult.h>
 #include <winerror.h>
-#include <PushNotificationErrorCodes.h>
 
 using namespace winrt::Windows::Networking::PushNotifications;
 
@@ -22,20 +21,36 @@ namespace winrt::Microsoft::ProjectReunion::implementation
     std::unique_lock<std::mutex> PushManager::s_lock (s_mutex, std::defer_lock); // Avoid locking during the constructor call
     inline constexpr std::uint32_t c_maxBackoffSeconds{ 960 };
 
-    bool PushManager::isChannelRequestRetryable(const winrt::hresult& hrException)
+    const HRESULT WNP_E_NOT_CONNECTED = MAKE_HRESULT(SEVERITY_ERROR, FACILITY_ITF, 0x880403E8L);
+    const HRESULT WNP_E_RECONNECTING = MAKE_HRESULT(SEVERITY_ERROR, FACILITY_ITF, 0x880403E9L);
+    const HRESULT WNP_E_BIND_USER_BUSY = MAKE_HRESULT(SEVERITY_ERROR, FACILITY_ITF, 0x880403FEL);
+
+    bool PushManager::isChannelRequestRetryable(const winrt::hresult& hr)
     {
-        switch (hrException)
+        switch (hr)
         {
         case HRESULT_FROM_WIN32(ERROR_TIMEOUT):
-        case WnpErrors::WNP_E_NOT_CONNECTED:
+        case WNP_E_NOT_CONNECTED:
         case WPN_E_OUTSTANDING_CHANNEL_REQUEST:
-        case WnpErrors:: WNP_E_RECONNECTING:
-        case WnpErrors::WNP_E_BIND_USER_BUSY:
+        case WNP_E_RECONNECTING:
+        case WNP_E_BIND_USER_BUSY:
         case RPC_S_SERVER_UNAVAILABLE:
             return true;
         default:
             return false;
         }
+    }
+
+    bool IsPackagedProcess()
+    {
+        uint32_t packageFullNameLength = 0;
+        const auto rc{ GetCurrentPackageFullName(&packageFullNameLength, nullptr) };
+
+        if (rc != ERROR_INSUFFICIENT_BUFFER)
+        {
+            winrt::throw_hresult(rc);
+        }
+        return rc == ERROR_INSUFFICIENT_BUFFER;
     }
 
     Windows::Foundation::IAsyncOperationWithProgress<Microsoft::ProjectReunion::ChannelResult, Microsoft::ProjectReunion::ChannelResult> PushManager::CreateChannelAsync(winrt::guid remoteId)
@@ -48,23 +63,30 @@ namespace winrt::Microsoft::ProjectReunion::implementation
         s_lock.lock();
         if (std::find(s_remoteIdList.begin(), s_remoteIdList.end(), remoteId) != s_remoteIdList.end())
         {
+            s_lock.unlock();
             progress(winrt::make<winrt::Microsoft::ProjectReunion::implementation::ChannelResult>(nullptr, WPN_E_OUTSTANDING_CHANNEL_REQUEST, ChannelStatus::InProgress));
             channelResult = winrt::make<winrt::Microsoft::ProjectReunion::implementation::ChannelResult>(nullptr, WPN_E_OUTSTANDING_CHANNEL_REQUEST, ChannelStatus::CompletedFailure);
             co_return channelResult;
         }
         else
         {
+            s_lock.lock();
             s_remoteIdList.push_back(remoteId);
+            s_lock.unlock();
         }
-        s_lock.unlock();
 
         PushNotificationChannelManager channelManager{};
+        PushNotificationChannel pushChannel{ nullptr };
 
         for (auto backOffTimeInSeconds = 30; backOffTimeInSeconds <= c_maxBackoffSeconds * 2; backOffTimeInSeconds *= 2)
         {
             try
             {
-                auto pushChannel = co_await channelManager.CreatePushNotificationChannelForApplicationAsync();
+                
+                if (IsPackagedProcess())
+                {
+                    pushChannel = co_await channelManager.CreatePushNotificationChannelForApplicationAsync();
+                }
 
                 if (pushChannel != nullptr)
                 {
