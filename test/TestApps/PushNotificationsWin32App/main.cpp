1d8d28bbc56456b4658ef1e6fac52226c6eb19ce
Paul Purifoy <purifoypaul@microsoft.com>
2021-03-10 11:37:21 -0800

    Added Request to WNS

--- test/TestApps/PushNotificationsWin32App/main.cpp
+++ test/TestApps/PushNotificationsWin32App/main.cpp
@@ -1,10 +1,60 @@
 ﻿#include "pch.h"
 #include <iostream>
+#include <sstream>
 #include <winrt\Windows.Networking.PushNotifications.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Storage.Streams.h>
+#include <winrt/Windows.Web.Http.Headers.h>
 
 using namespace winrt;
+using namespace winrt::Windows::Storage;
+using namespace winrt::Windows::Storage::Streams;
 using namespace winrt::Windows::Foundation;
 using namespace winrt::Microsoft::ProjectReunion;
+using namespace Windows::Web::Http;
+
+
+
+std::wstring BuildNotificationPayload(std::wstring channel)
+{
+    std::wstring channelUri = L"\"ChannelUri\":\"" + channel + L"\"";
+    std::wstring x_wns_type = L"\"X_WNS_Type\": \"wns/raw\"";
+    std::wstring contentType = L"\"Content_Type\": \"application/octet-stream\"";
+    std::wstring payload = L"\"Payload\": \"<toast></toast>\"";
+    std::wstring delay = L"\"Delay\": \"false\"";
+    return { L"{" + channelUri + L"," + x_wns_type + L"," + contentType + L"," + payload + L"," + delay + L"}" };
+}
+
+void sendRequestToServer(winrt::hstring channel)
+{
+    HttpResponseMessage httpResponseMessage;
+    std::wstring httpResponseBody;
+    try {
+        // Construct the HttpClient and Uri. This endpoint is for test purposes only.
+        HttpClient httpClient;
+        Uri requestUri{ L"http://localhost:7071/api/PostPushNotification" };
+
+        // Construct the JSON to post.
+        HttpStringContent jsonContent(
+            BuildNotificationPayload(channel.c_str()),
+            UnicodeEncoding::Utf8,
+            L"application/json");
+
+        // Post the JSON, and wait for a response.
+        httpResponseMessage = httpClient.PostAsync(
+            requestUri,
+            jsonContent).get();
+
+        // Make sure the post succeeded, and write out the response.
+        httpResponseMessage.EnsureSuccessStatusCode();
+        httpResponseBody = httpResponseMessage.Content().ReadAsStringAsync().get();
+        std::wcout << httpResponseBody.c_str() << std::endl;
+    }
+    catch (winrt::hresult_error const& ex)
+    {
+        std::wcout << ex.message().c_str() << std::endl;
+    }
+}
 
 int main()
 {
@@ -43,6 +93,7 @@ int main()
                 auto channelUri = result.Channel().Uri();
                 auto channelExpiry = result.Channel().ExpirationTime();
 
+                sendRequestToServer(channelUri);
                 std::cout << "channelUri: " << winrt::to_string(channelUri) << std::endl;
                 // Persist the channelUri and Expiry in the App Service
             }
