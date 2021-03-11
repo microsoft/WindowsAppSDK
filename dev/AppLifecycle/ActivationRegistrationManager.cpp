91dc45c548e5cef4d03557d959a59ba98c59411d
Scott Darnell <20483794+aeloros@users.noreply.github.com>
2021-03-10 21:11:19 -0800

    Startup activations, AppLifecycle updates, and activations. (#454)
    * Initial StartupTask feature support.
    
    * Update arg resolution to use Uri and protocol launch as the transport for the data.
    
    * Initial files for delegate execute
    Update names and add test.
    Update to use AppUserModelId instead of ProgId.
    
    * remove dupes from merge
    
    * fixups
    
    * fix merge problem with parameter ordering, and make the applifecycle test dependent upon the MSIX packages.
    
    * Removing unneeded debugging code
    
    * Moved install & uninstall of packaged dependencies to be executed at the test class level.
    Removed unneeded unpackaged COM registration code.
    Fixed test code to use the platform to determine the DLL module path of the current code.
    Merged the AppLifecycle tests to be one project.
    Made the test helper functions inline so as to avoid symbol redefinition errors.
    Renamed the test namespace used for AppLifecycle tests.

--- dev/AppLifecycle/ActivationRegistrationManager.cpp
+++ dev/AppLifecycle/ActivationRegistrationManager.cpp
@@ -8,102 +8,171 @@
 #include "ProtocolActivatedEventArgs.h"
 #include "FileActivatedEventArgs.h"
 #include "Association.h"
+#include "ExtensionContract.h"
+#include "EncodedLaunchExecuteCommand.h"
 
 namespace winrt::Microsoft::ApplicationModel::Activation::implementation
 {
-    void ActivationRegistrationManager::RegisterForFileTypeActivation(
-        array_view<hstring const> supportedFileTypes, array_view<hstring const> supportedVerbs,
-        hstring const& applicationDisplayName, hstring const& logo)
+    std::wstring GenerateEncodedLaunchUri(std::wstring const& appUserModelId, std::wstring const& contractId)
     {
-        if (HasIdentity())
-        {
-            throw hresult_illegal_method_call();
-        }
+        Windows::Foundation::Uri uri{ wil::str_printf<std::wstring>(L"%s://%s?%s=%s",
+            c_encodedLaunchSchemeName, appUserModelId.c_str(), c_contractIdKeyName, contractId.c_str()).c_str() };
+        return uri.AbsoluteUri().c_str();
+    }
 
-        auto appId = ComputeAppId();
+    std::wstring GenerateCommandLine(std::wstring const& modulePath)
+    {
+        std::wstring exePath{ modulePath.empty() ? GetModulePath() : modulePath };
+
+        return wil::str_printf<std::wstring>(L"%s %s%s%s", exePath.c_str(), c_argumentPrefix,
+            c_protocolArgumentString, c_argumentSuffix);
+    }
+
+    void ActivationRegistrationManager::RegisterForFileTypeActivation(
+        array_view<hstring const> supportedFileTypes, hstring const& logo, hstring const& displayName,
+        array_view<hstring const> supportedVerbs, hstring const& exePath)
+    {
+        THROW_HR_IF(E_ILLEGAL_METHOD_CALL, HasIdentity());
+        THROW_HR_IF(E_INVALIDARG, !supportedFileTypes.size());
+
+        auto appId = ComputeAppId(exePath.c_str());
         auto type = AssociationType::File;
         auto progId = ComputeProgId(appId, type);
 
-        if (supportedFileTypes.size())
-        {
-            RegisterProgId(progId.c_str(), L"", applicationDisplayName.c_str(), logo.c_str());
-            RegisterApplication(appId.c_str());
-        }
+        RegisterProgId(progId.c_str(), L"", L"", displayName.c_str(), logo.c_str());
+        RegisterApplication(appId.c_str());
 
-        for (auto extension : supportedFileTypes)
+        for (auto fileType : supportedFileTypes)
         {
-            RegisterFileExtension(extension.c_str());
+            RegisterFileExtension(fileType.c_str());
             
             for (auto verb : supportedVerbs)
             {
-                auto command = GetModulePath();
-                command += L" ----" + c_fileArgumentString + L":" + verb.c_str() + L",%1";
+                auto command = GenerateCommandLine(exePath.c_str()) + GenerateEncodedLaunchUri(L"App",
+                    c_fileContractId) + L"&Verb=" + verb.c_str() + L"&File=%1";
+
                 RegisterVerb(progId, verb.c_str(), command);
             }
 
-            RegisterAssociationHandler(appId, extension.c_str(), type);
+            RegisterAssociationHandler(appId, fileType.c_str(), type);
         }
     }
 
-    void ActivationRegistrationManager::RegisterForProtocolActivation(hstring const& scheme,
-        hstring const& applicationDisplayName, hstring const& logo)
+    void ActivationRegistrationManager::RegisterForProtocolActivation(hstring const& schemeName,
+        hstring const& logo, hstring const& displayName, hstring const& exePath)
     {
-        if (scheme.empty())
-        {
-            throw hresult_invalid_argument();
-        }
+        THROW_HR_IF(E_ILLEGAL_METHOD_CALL, HasIdentity());
+        THROW_HR_IF(E_INVALIDARG, schemeName.empty());
 
-        if (HasIdentity())
-        {
-            throw hresult_illegal_method_call();
-        }
-
-        RegisterProtocol(scheme.c_str());
-
-        auto appId = ComputeAppId();
-        auto type = AssociationType::Protocol;
-        auto progId = ComputeProgId(appId, type);
-
-        RegisterProgId(progId.c_str(), L"", applicationDisplayName.c_str(), logo.c_str());
-
-        auto command = GetModulePath();
-        command += L" ----" + c_protocolArgumentString + L":%1";
-        RegisterVerb(progId.c_str(), L"open", command);
-
-        RegisterApplication(appId.c_str());
-        RegisterAssociationHandler(appId, scheme.c_str(), type);
+        RegisterForProtocolActivationInternal(schemeName.c_str(), L"", logo.c_str(),
+            displayName.c_str(), exePath.c_str());
     }
 
-    void ActivationRegistrationManager::UnregisterForFileTypeActivation(hstring const& fileType)
+    void ActivationRegistrationManager::RegisterForStartupActivation(hstring const& taskId,
+        hstring const& exePath)
     {
-        if (HasIdentity())
-        {
-            throw hresult_illegal_method_call();
-        }
+        THROW_HR_IF(E_ILLEGAL_METHOD_CALL, HasIdentity());
+        THROW_HR_IF(E_INVALIDARG, taskId.empty());
 
-        auto appId = ComputeAppId();
+        // Example: HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run
+        wil::unique_hkey key;
+        THROW_IF_WIN32_ERROR(::RegOpenKeyEx(HKEY_CURRENT_USER, c_runKeyPath, 0, KEY_WRITE,
+            key.put()));
+
+        // Pass a command line that will make sense while constructing the args object.
+        auto command = GenerateCommandLine(exePath.c_str()) + GenerateEncodedLaunchUri(L"App",
+            c_startupTaskContractId) + L"&TaskId=" + taskId;
+
+        // Name: taskId
+        // Value: commandLine
+        THROW_IF_WIN32_ERROR(::RegSetValueEx(key.get(), taskId.c_str(), 0, REG_SZ,
+            reinterpret_cast<BYTE const*>(command.c_str()),
+            static_cast<uint32_t>((command.size() + 1) * sizeof(wchar_t))));
+    }
+
+    void ActivationRegistrationManager::UnregisterForFileTypeActivation(array_view<hstring const> fileTypes,
+        hstring const& exePath)
+    {
+        THROW_HR_IF(E_ILLEGAL_METHOD_CALL, HasIdentity());
+        THROW_HR_IF(E_INVALIDARG, !fileTypes.size());
+
+        auto appId = ComputeAppId(exePath.c_str());
         auto type = AssociationType::File;
         auto progId = ComputeProgId(appId, type);
-        UnregisterAssociationHandler(appId, fileType.c_str(), type);
+
+        for (auto fileType : fileTypes)
+        {
+            UnregisterAssociationHandler(appId, fileType.c_str(), type);
+            UnregisterProgId(progId);
+        }
+    }
+
+    void ActivationRegistrationManager::UnregisterForProtocolActivation(hstring const& schemeName,
+        hstring const& exePath)
+    {
+        THROW_HR_IF(E_ILLEGAL_METHOD_CALL, HasIdentity());
+        THROW_HR_IF(E_INVALIDARG, schemeName.empty());
+
+        auto appId = ComputeAppId(exePath.c_str());
+        auto type = AssociationType::Protocol;
+        auto progId = ComputeProgId(appId, type);
+        UnregisterAssociationHandler(appId, schemeName.c_str(), type);
         UnregisterProgId(progId);
     }
 
-    void ActivationRegistrationManager::UnregisterForProtocolActivation(hstring const& scheme)
+    void ActivationRegistrationManager::UnregisterForStartupActivation(hstring const& taskId)
     {
-        if (scheme.empty())
-        {
-            throw hresult_invalid_argument();
-        }
+        THROW_HR_IF(E_ILLEGAL_METHOD_CALL, HasIdentity());
+        THROW_HR_IF(E_INVALIDARG, taskId.empty());
 
-        if (HasIdentity())
+        // Example: HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run
+        wil::unique_hkey key;
+        if (::RegOpenKeyEx(HKEY_CURRENT_USER, c_runKeyPath, 0, KEY_WRITE, key.put())
+            == ERROR_SUCCESS)
         {
-            throw hresult_illegal_method_call();
+            ::RegDeleteValue(key.get(), taskId.c_str());
         }
+    }
 
-        auto appId = ComputeAppId();
+    void ActivationRegistrationManager::RegisterForProtocolActivationInternal(std::wstring const& schemeName,
+        std::wstring const& appUserModelId, std::wstring const& logo, std::wstring const& displayName,
+        std::wstring const& exePath)
+    {
+        THROW_HR_IF(E_ILLEGAL_METHOD_CALL, HasIdentity());
+        THROW_HR_IF(E_INVALIDARG, schemeName.empty());
+
+        RegisterProtocol(schemeName.c_str());
+
+        auto appId = ComputeAppId(exePath.c_str());
         auto type = AssociationType::Protocol;
         auto progId = ComputeProgId(appId, type);
-        UnregisterAssociationHandler(appId, scheme.c_str(), type);
-        UnregisterProgId(progId);
+
+        RegisterProgId(progId.c_str(), L"", appUserModelId.c_str(), displayName.c_str(),
+            logo.c_str());
+
+        auto command = GenerateCommandLine(exePath.c_str()) + L"%1";
+        RegisterVerb(progId.c_str(), c_openVerbName, command);
+
+        RegisterApplication(appId.c_str());
+        RegisterAssociationHandler(appId, schemeName.c_str(), type);
+    }
+
+    void ActivationRegistrationManager::RegisterEncodedLaunchCommand()
+    {
+        RegisterProtocol(c_launchSchemeName);
+
+        auto delegateExecute = __uuidof(EncodedLaunchExecuteCommandFactory);
+        RegisterVerb(c_launchSchemeName, c_openVerbName, L"", &delegateExecute);
+    }
+
+    void ActivationRegistrationManager::RegisterEncodedLaunchSupport(std::wstring const& appUserModelId,
+        std::wstring const& exePath)
+    {
+        // Make sure the encoded launch delegate execute command is registered on the system.
+        RegisterEncodedLaunchCommand();
+
+        // Register the current app to receive launch requests.
+        RegisterForProtocolActivationInternal(c_encodedLaunchSchemeName, appUserModelId,
+            L"", L"Encoded Launch Target", exePath);
     }
 }
