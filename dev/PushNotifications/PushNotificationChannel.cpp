53089fd8ed620976c8d65a0d517b91a691c6385e
Venkata Sharath Chandra Manchala <vemancha@microsoft.com>
2021-03-13 13:53:21 -0800

    Temp

---
+++ dev/PushNotifications/PushNotificationChannel.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "PushNotificationChannel.h"
+#include "PushNotificationChannel.g.cpp"
+
+namespace winrt::Microsoft::ProjectReunion::implementation
+{
+    PushNotificationChannel::PushNotificationChannel(hstring const& uri, Windows::Foundation::DateTime const& expiration)
+    {
+        m_uri = uri;
+        m_expiration = expiration;
+    }
+    hstring PushNotificationChannel::Uri()
+    {
+        return m_uri;
+    }
+    Windows::Foundation::DateTime PushNotificationChannel::ExpirationTime()
+    {
+        return m_expiration;
+    }
+    void PushNotificationChannel::Close()
+    {
+        throw hresult_not_implemented();
+    }
+}
---
+++ dev/PushNotifications/PushNotificationChannel.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include "PushNotificationChannel.g.h"
+
+namespace winrt::Microsoft::ProjectReunion::implementation
+{
+    struct PushNotificationChannel : PushNotificationChannelT<PushNotificationChannel>
+    {
+        PushNotificationChannel() = default;
+
+        PushNotificationChannel(hstring const& uri, Windows::Foundation::DateTime const& expiration);
+        hstring Uri();
+        Windows::Foundation::DateTime ExpirationTime();
+        void Close();
+
+    private:
+        hstring m_uri;
+        Windows::Foundation::DateTime m_expiration;
+    };
+}
+namespace winrt::Microsoft::ProjectReunion::factory_implementation
+{
+    struct PushNotificationChannel : PushNotificationChannelT<PushNotificationChannel, implementation::PushNotificationChannel>
+    {
+    };
+}
