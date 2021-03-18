0e67c1c585172c8a3a957d45d3515a493b56d152
Huzaifa Danish <modanish@microsoft.com>
2021-03-18 11:13:48 -0700

    Initial test setup
    Howard method
    Compiles and builds
    Tests fail before even executing first method

---
+++ PowerNotificationsTests/PowerTests.cpp
@@ -0,0 +1,40 @@
+﻿#include "pch.h"
+
+using PowerEventHandle =
+winrt::Windows::Foundation::EventHandler<winrt::Windows::Foundation::IInspectable>;
+
+namespace PowerMangerTest
+{
+    class Mocked
+    {
+    public:
+        BEGIN_TEST_CLASS(Mocked)
+            TEST_CLASS_PROPERTY(L"IsolationLevel", L"Method")
+            TEST_CLASS_PROPERTY(L"ThreadingModel", L"MTA")
+            TEST_CLASS_PROPERTY(L"RunAs:Class", L"RestrictedUser")
+        END_TEST_CLASS()
+
+        TEST_CLASS_SETUP(Setup)
+        {
+            ::Test::Bootstrap::Setup();
+            //insert additional code here
+            return true;
+        }
+
+        TEST_CLASS_CLEANUP(Cleanup)
+        {
+            //insert additional code here
+            ::Test::Bootstrap::Cleanup();
+            return true;
+        }
+
+        TEST_METHOD(Foo)
+        {
+            //insert additional code here
+            PowerEventHandle handle;
+            auto token = winrt::Microsoft::ProjectReunion::PowerManager::BatteryStatusChanged(handle);
+            winrt::Microsoft::ProjectReunion::PowerManager::BatteryStatusChanged(token);
+            VERIFY_IS_NOT_NULL(handle);
+        }
+    };
+}
---
+++ PowerNotificationsTests/pch.h
@@ -0,0 +1,25 @@
+﻿// pch.h: This is a precompiled header file.
+// Files listed below are compiled only once, improving build performance for future builds.
+// This also affects IntelliSense performance, including code completion and many code browsing features.
+// However, files listed here are ALL re-compiled if any one of them is updated between builds.
+// Do not add files here that you will be updating frequently as this negates the performance advantage.
+
+#ifndef PCH_H
+#define PCH_H
+
+// add headers that you want to pre-compile here
+
+#ifndef INLINE_TEST_METHOD_MARKUP
+#define INLINE_TEST_METHOD_MARKUP
+#endif
+
+#include "framework.h"
+#include <WexTestClass.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.Management.Deployment.h>
+
+#include <winrt/Microsoft.ProjectReunion.h>
+#include <ProjectReunion.Test.Bootstrap.h>
+
+#endif //PCH_H
