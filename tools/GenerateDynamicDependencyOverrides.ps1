03968262d0ab677afa5910c59fc2d3793d98ea7c
Howard Kapustein <howardk@microsoft.com>
2021-04-22 18:01:17 -0700

    Incorporated feedback

--- tools/GenerateDynamicDependencyOverrides.ps1
+++ tools/GenerateDynamicDependencyOverrides.ps1
@@ -67,6 +67,12 @@ $content_h=@"
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+// Rely on _STRINGIZE(x) in yvals_core.h
+#ifndef _STRINGIZE
+#define _STRINGIZEX(x)  #x
+#define _STRINGIZE(x)   _STRINGIZEX(x)
+#endif
+
 #idef PR_DYNDEP_LIBID_UUID
 #undef PR_DYNDEP_LIBID_UUID
 #define PR_DYNDEP_LIBID_UUID                        1BF712E5-41ED-46a5-8402-2D40725F691E
@@ -79,7 +85,7 @@ $content_h=@"
 
 #idef PR_DYNDEP_DATASTORE_CLSID_STRING
 #undef PR_DYNDEP_DATASTORE_CLSID_STRING
-#define PR_DYNDEP_DATASTORE_CLSID_STRING            "D1AD16C7-EC59-4765-BF95-9A243EB00507"
+#define PR_DYNDEP_DATASTORE_CLSID_STRING            _STRINGIZE(PR_DYNDEP_DATASTORE_CLSID_UUID)
 #endif
 
 #idef PR_DYNDEP_LIFETIMEMANAGER_CLSID_UUID
@@ -89,7 +95,7 @@ $content_h=@"
 
 #idef PR_DYNDEP_LIFETIMEMANAGER_CLSID_STRING
 #undef PR_DYNDEP_LIFETIMEMANAGER_CLSID_STRING
-#define PR_DYNDEP_LIFETIMEMANAGER_CLSID_STRING      "$lifetimemanager_clsid_uuid"
+#define PR_DYNDEP_LIFETIMEMANAGER_CLSID_STRING      _STRINGIZE(PR_DYNDEP_LIFETIMEMANAGER_CLSID_UUID)
 #endif
 
 #idef PR_DYNDEP_LIFETIMEMANAGER_CLSID_GUID
