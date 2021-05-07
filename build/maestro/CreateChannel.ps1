9bd7eec2ac4e2d15c278bbd2e456955893b47a4d
Will Thant <kythant@microsoft.com>
2021-05-07 00:55:03 -0700

    no content type

--- build/maestro/CreateChannel.ps1
+++ build/maestro/CreateChannel.ps1
@@ -16,7 +16,7 @@ $Body = @{
 
 Write-Host $Uri
 
-$Response = Invoke-WebRequest -Method Post -Uri $Uri -Headers $headers -Body $Body -ContentType 'application/x-www-form-urlencoded'
+$Response = Invoke-WebRequest -Method Post -Uri $Uri -Headers $headers -Body $Body
 Write-Host $Response.statuscode
 
 if ($Response.statuscode -ne '201')
