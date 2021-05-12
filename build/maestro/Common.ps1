812a267e49412f9ea6c057649cfcc9b934f52ad4
Will Thant <kythant@microsoft.com>
2021-05-11 23:42:46 -0700

    try again

--- build/maestro/Common.ps1
+++ build/maestro/Common.ps1
@@ -5,18 +5,10 @@ Write-Host "Hello from Common.ps1"
 function GetOrgNameFromAzureDevOpsCollectionUri([string]$CollectionUri)
 {
     $test = "https://microsoft.visualstudio.com/"
-    $Split1 = $test.Split("//")
-    Write-Host "S1 " $Split1[1]
+    $Split1 = $test.Split(".")
+    Write-Host "S1 " $Split1[0]
     $temp1 = $Split1[1]
-    $Split2 = $temp1.Split(".")
-    Write-Host "S2 " $Split2[0]
-
-
-    $Inputstring ="Sonali Bhatt is Database Administrator"
-    $CharArray =$InputString.Split(" ")
-    $String1= $CharArray[0]
-    $String2= $CharArray[4] 
-    write-host $String1
-    write-host $String2
-    return $Split2[0]
+    $Split2 = $temp1.Substring(8) 
+    Write-Host "S2 " $Split2
+    return $Split2
 }
