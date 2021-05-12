d15d60570145759c7e900ae4b096ca177a588f8b
Will Thant <kythant@microsoft.com>
2021-05-11 22:51:07 -0700

    try agai

--- build/maestro/Common.ps1
+++ build/maestro/Common.ps1
@@ -4,8 +4,10 @@ Write-Host "Hello from Common.ps1"
 # https://dev.azure.com/fabrikamfiber/
 function GetOrgNameFromAzureDevOpsCollectionUri([string]$CollectionUri)
 {
-    $Split1 = $CollectionUri.Split("//")[1]
-    Write-Host "S1 " $Split1
-    $Split2 =  $Split1.Split(".")[0]
-    return $Split2
+    $Split1 = $CollectionUri.Split("//")
+    Write-Host "S1 " $Split1[1]
+    $temp1 = $Split1[1]
+    $Split2 = $temp1.Split(".")
+    Write-Host "S2 " $Split2[0]
+    return $Split2[0]
 }
