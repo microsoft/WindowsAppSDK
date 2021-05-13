f5ca3a6dbe3dfe1ba3dc8ba64672bc44a983014a
Paul Purifoy <purifoypaul@microsoft.com>
2021-05-13 14:05:47 -0700

    Push Notifications Components

---
+++ dev/PushNotifications/PushNotificationReceivedEventArgs.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include "Microsoft.Windows.PushNotifications.PushNotificationReceivedEventArgs.g.h"
+
+namespace winrt::Microsoft::Windows::PushNotifications::implementation
+{
+    struct PushNotificationReceivedEventArgs : PushNotificationReceivedEventArgsT<PushNotificationReceivedEventArgs>
+    {
+        PushNotificationReceivedEventArgs(winrt::Windows::ApplicationModel::Background::IBackgroundTaskInstance const& backgroundTask);
+        PushNotificationReceivedEventArgs(winrt::Windows::Networking::PushNotifications::PushNotificationReceivedEventArgs const& args);
+
+        static Microsoft::Windows::PushNotifications::PushNotificationReceivedEventArgs CreateFromBackgroundTaskInstance(winrt::Windows::ApplicationModel::Background::IBackgroundTaskInstance const& backgroundTask);
+        static Microsoft::Windows::PushNotifications::PushNotificationReceivedEventArgs CreateFromPushNotificationReceivedEventArgs(winrt::Windows::Networking::PushNotifications::PushNotificationReceivedEventArgs const& args);
+        winrt::com_array<uint8_t> Payload();
+        winrt::Windows::ApplicationModel::Background::BackgroundTaskDeferral GetDeferral();
+        winrt::event_token Canceled(winrt::Windows::ApplicationModel::Background::BackgroundTaskCanceledEventHandler const& handler);
+        void Canceled(winrt::event_token const& token) noexcept;
+        bool Handled();
+        void Handled(bool value);
+
+    private:
+        winrt::Windows::ApplicationModel::Background::IBackgroundTaskInstance m_backgroundTaskInstance = nullptr;
+        winrt::com_array<uint8_t> m_backgroundPayload{};
+        winrt::Windows::Networking::PushNotifications::PushNotificationReceivedEventArgs m_args = nullptr;
+        wil::srwlock m_lock;
+    };
+}
+namespace winrt::Microsoft::Windows::PushNotifications::factory_implementation
+{
+    struct PushNotificationReceivedEventArgs : PushNotificationReceivedEventArgsT<PushNotificationReceivedEventArgs, implementation::PushNotificationReceivedEventArgs>
+    {
+    };
+}
---
+++ dev/PushNotifications/PushNotificationRegistrationToken.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include "Microsoft.Windows.PushNotifications.PushNotificationRegistrationToken.g.h"
+
+namespace winrt::Microsoft::Windows::PushNotifications::implementation
+{
+    struct PushNotificationRegistrationToken : PushNotificationRegistrationTokenT<PushNotificationRegistrationToken>
+    {
+        PushNotificationRegistrationToken() = default;
+
+        PushNotificationRegistrationToken(uint64_t cookie, winrt::Windows::ApplicationModel::Background::BackgroundTaskRegistration const& taskRegistration);
+        uint64_t Cookie();
+        winrt::Windows::ApplicationModel::Background::BackgroundTaskRegistration TaskRegistration();
+
+    private:
+        uint64_t m_cookie;
+        winrt::Windows::ApplicationModel::Background::BackgroundTaskRegistration m_taskRegistration = nullptr;
+        wil::srwlock m_lock;
+    };
+}
+namespace winrt::Microsoft::Windows::PushNotifications::factory_implementation
+{
+    struct PushNotificationRegistrationToken : PushNotificationRegistrationTokenT<PushNotificationRegistrationToken, implementation::PushNotificationRegistrationToken>
+    {
+    };
+}
