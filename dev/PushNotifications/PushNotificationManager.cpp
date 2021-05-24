3042b42e58adfc3a945b5eca30e7f02e85422bb8
Sharath Manchala <10109130+sharath2727@users.noreply.github.com>
2021-05-24 10:19:55 -0700

    Address comments for createchannelasync

--- dev/PushNotifications/PushNotificationManager.cpp
+++ dev/PushNotifications/PushNotificationManager.cpp
@@ -33,9 +33,9 @@ namespace winrt::Microsoft::Windows::PushNotifications::implementation
     inline constexpr std::uint32_t c_maxBackoffSeconds{ 960 };
     inline constexpr std::uint32_t c_minBackoffSeconds{ 30 };
 
-    const HRESULT WNP_E_NOT_CONNECTED = MAKE_HRESULT(SEVERITY_ERROR, FACILITY_ITF, 0x880403E8L);
-    const HRESULT WNP_E_RECONNECTING = MAKE_HRESULT(SEVERITY_ERROR, FACILITY_ITF, 0x880403E9L);
-    const HRESULT WNP_E_BIND_USER_BUSY = MAKE_HRESULT(SEVERITY_ERROR, FACILITY_ITF, 0x880403FEL);
+    const HRESULT WNP_E_NOT_CONNECTED = (HRESULT)0x880403E8L;
+    const HRESULT WNP_E_RECONNECTING = (HRESULT)0x880403E9L;
+    const HRESULT WNP_E_BIND_USER_BUSY = (HRESULT)0x880403FEL;
 
     bool PushNotificationManager::IsChannelRequestRetryable(const hresult& hr)
     {
@@ -46,7 +46,7 @@ namespace winrt::Microsoft::Windows::PushNotifications::implementation
         case WPN_E_OUTSTANDING_CHANNEL_REQUEST:
         case WNP_E_RECONNECTING:
         case WNP_E_BIND_USER_BUSY:
-        case RPC_S_SERVER_UNAVAILABLE:
+        case HRESULT_FROM_WIN32(RPC_S_SERVER_UNAVAILABLE):
             return true;
         default:
             return false;
@@ -71,7 +71,7 @@ namespace winrt::Microsoft::Windows::PushNotifications::implementation
         return true;
     }
 
-    winrt::IAsyncOperationWithProgress<winrt::Microsoft::Windows::PushNotifications::PushNotificationCreateChannelResult, winrt::Microsoft::Windows::PushNotifications::PushNotificationCreateChannelStatus> PushNotificationManager::CreateChannelAsync(winrt::guid remoteId)
+    winrt::IAsyncOperationWithProgress<winrt::Microsoft::Windows::PushNotifications::PushNotificationCreateChannelResult, winrt::Microsoft::Windows::PushNotifications::PushNotificationCreateChannelStatus> PushNotificationManager::CreateChannelAsync(const winrt::guid &remoteId)
     {
         static bool s_remoteIdInProgress;
         static wil::critical_section s_lock;
@@ -79,9 +79,8 @@ namespace winrt::Microsoft::Windows::PushNotifications::implementation
         THROW_HR_IF(E_INVALIDARG, (remoteId == winrt::guid()));
 
         // API supports channel requests only for packaged applications for v0.8 version
-        THROW_HR_IF(E_NOTIMPL, (IsPackagedProcess() == false));
+        THROW_HR_IF(E_NOTIMPL, !IsPackagedProcess());
 
-        winrt::Microsoft::Windows::PushNotifications::PushNotificationCreateChannelResult channelResult{ nullptr };
         {
             auto lock = s_lock.lock();
             if (s_remoteIdInProgress == false)
@@ -90,9 +89,8 @@ namespace winrt::Microsoft::Windows::PushNotifications::implementation
             }
             else
             {
-                channelResult = winrt::make<winrt::Microsoft::Windows::PushNotifications::implementation::PushNotificationCreateChannelResult>(
+                co_return winrt::make<PushNotificationCreateChannelResult>(
                     nullptr, WPN_E_OUTSTANDING_CHANNEL_REQUEST, PushNotificationChannelStatus::CompletedFailure);
-                co_return channelResult;
             }
         }
 
@@ -120,16 +118,24 @@ namespace winrt::Microsoft::Windows::PushNotifications::implementation
 
         progress(channelStatus);
 
-        PushNotificationChannelManager channelManager{};
-        winrt::PushNotificationChannel pushChannelReceived{ nullptr };
-
         for (auto backOffTimeInSeconds = c_minBackoffSeconds; backOffTimeInSeconds <= c_maxBackoffSeconds * 2; backOffTimeInSeconds *= 2)
         {
             try
             {
+                PushNotificationChannelManager channelManager{};
+                winrt::PushNotificationChannel pushChannelReceived{ nullptr };
+
                 pushChannelReceived = co_await channelManager.CreatePushNotificationChannelForApplicationAsync();
-                channelRequestResult = S_OK;
-                status = PushNotificationChannelStatus::CompletedSuccess;
+
+                // Returns a com_ptr to the implementation type
+                auto pushChannel =
+                    winrt::make_self<PushNotificationChannel>(pushChannelReceived);
+
+                co_return winrt::make<PushNotificationCreateChannelResult>(
+                    winrt::make<PushNotificationChannel>(pushChannelReceived),
+                    S_OK,
+                    PushNotificationChannelStatus::CompletedSuccess);
+
                 break;
             }
             catch (...)
@@ -147,36 +153,17 @@ namespace winrt::Microsoft::Windows::PushNotifications::implementation
                 }
                 else
                 {
-                    channelRequestResult = channelRequestException.code();
-                    status = PushNotificationChannelStatus::CompletedFailure;
-                    break;
+                    co_return winrt::make<PushNotificationCreateChannelResult>(
+                        nullptr,
+                        channelRequestException.code(),
+                        PushNotificationChannelStatus::CompletedFailure);
                 }
             }
 
             co_await winrt::resume_after(std::chrono::seconds(backOffTimeInSeconds));
         }
 
-
-        if (status == PushNotificationChannelStatus::CompletedSuccess)
-        {
-            // Returns a com_ptr to the implementation type
-            auto pushChannel =
-                winrt::make_self<winrt::Microsoft::Windows::PushNotifications::implementation::PushNotificationChannel>(pushChannelReceived);
-
-            channelResult = winrt::make<winrt::Microsoft::Windows::PushNotifications::implementation::PushNotificationCreateChannelResult>(
-                *(pushChannel.get()),
-                channelRequestResult,
-                status);
-        }
-        else if (status == PushNotificationChannelStatus::CompletedFailure)
-        {
-            channelResult = winrt::make<winrt::Microsoft::Windows::PushNotifications::implementation::PushNotificationCreateChannelResult>(
-                nullptr,
-                channelRequestResult,
-                status);
-        }
-
-        co_return channelResult;
+        co_return nullptr;
     }
 
     PushNotificationRegistrationToken PushNotificationManager::RegisterActivator(PushNotificationActivationInfo const& details)
