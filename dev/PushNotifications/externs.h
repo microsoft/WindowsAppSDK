856cf9701fd82f16b4a72c4fe47464a6c4c91108
Daniel Ayala <joayalag@microsoft.com>
2021-05-26 09:04:56 -0700

    Removed global lock

--- dev/PushNotifications/externs.h
+++ dev/PushNotifications/externs.h
@@ -3,6 +3,5 @@
 #include "PushNotificationReceivedEventArgs.h"
 
 extern wil::unique_handle g_waitHandleForArgs;
-extern wil::critical_section g_lock;
 
 const winrt::hstring ACTIVATED_EVENT_ARGS_KEY = L"GlobalActivatedEventArgs";
