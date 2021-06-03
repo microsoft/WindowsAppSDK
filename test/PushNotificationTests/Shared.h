f6b562a03fafd83099f24b547bc04e5f8eb55990
Paul Purifoy <purifoypaul@microsoft.com>
2021-06-03 10:35:39 -0700

    Fixed Debug unit tests

--- test/PushNotificationTests/Shared.h
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information.
-#pragma once
-
-namespace Test::PushNotifications
-{
-    void InstallPackage(const std::wstring& packagePath);
-    void UninstallPackage(const std::wstring& packageFullName);
-}
