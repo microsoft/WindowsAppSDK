d5c189ede3f772a274a97e413b575c1eec6135e9
Jeff Genovy <29107334+jefgen@users.noreply.github.com>
2021-06-17 19:01:38 -0700

    Add Helix.TestCommon and Helix.Test.NetCore

---
+++ test/Helix.TestCommon/TestPass-PostRun.ps1
@@ -0,0 +1,6 @@
+Write-Host "TestPass-PostRun.ps1"
+
+# This script is run on the test machine for each work item that executes there.
+# It runs after executing the tests.
+
+# This script is called by TestPass-PostRunCore.ps1 which is a part of the WinUI.Helix package.
