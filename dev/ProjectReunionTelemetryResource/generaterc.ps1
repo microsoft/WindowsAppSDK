02cfe1255eb6a6ca41450f7ab0683ea635cccaec
Chandra Srinivasan <chsriniv@microsoft.com>
2021-07-02 11:56:49 -0700

    This change adds a ReunionTelemetryResourceDll project. This resource DLL will contain the resources (package version, experimentation level). This project also contains a script to generate the .rc file when provided with the package version and the experimentation level. The .rc file is intentionally missing from this check-in since it will be generated by the script. The script will be executed before invoking the build step on this project. ReunionTelemetry.h references this resource dll to populate the values in the PartB structure.

---
+++ dev/ProjectReunionTelemetryResource/generaterc.ps1
@@ -0,0 +1,5 @@
+﻿param($packagever, $experimentationLevel)
+$content = Get-Content -Path ".\resource.templ"
+$content = $content -replace "%REUNION_PACKAGE_VER%",$packagever
+$content = $content -replace "%REUNION_EXPERIMENTATION_LEVEL%", $experimentationLevel
+$content | Set-Content ".\resource.rc"
