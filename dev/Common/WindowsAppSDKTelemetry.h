c287f12a6a1ea3cec151dd89c3b0344b53f1a7fd
Chandra Srinivasan <chsriniv@microsoft.com>
2021-07-02 12:35:36 -0700

    This project add a resource dll for telemetry resources
    * Adding a script to auto generate the rc file for the resource dll that will contain the values needed to log PartB telemetry.
    * The .rc file for the resource project is generated from the resource.templ file checked in with the project.
    * The script must be executed before running the build step on this solution.
    * Adding ReunionTelemetry.h that will reference this resource dll to construct the PartB structure.

---
+++ dev/Common/WindowsAppSDKTelemetry.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#ifndef __WINDOWSAPPSDKTELEMETRY_INCLUDED
+#define __WINDOWSAPPSDKTELEMETRY_INCLUDED
+
+#ifdef __WIL_TRACELOGGING_H_INCLUDED
+#error "WIL Tracelogging.h must not be explicitly included when including this file"
+#endif
+
+char* WINDOWSAPPSDK_PACKAGE_VER();
+char* WINDOWSAPPSDK_EXPERIMENTATION_LEVEL();
+
+#define _GENERIC_PARTB_FIELDS_ENABLED \
+        TraceLoggingStruct(3, "PartB_WINDOWSAPPSDK_API"), \
+        TraceLoggingString(WINDOWSAPPSDK_PACKAGE_VER(), "Windows App SDK package version"), \
+        TraceLoggingString(WINDOWSAPPSDK_EXPERIMENTATION_LEVEL(), "Windows App SDK experimentation level"), \
+        TraceLoggingBool(wil::details::IsDebuggerPresent(), "IsDebugging")
+
+#include <wil/tracelogging.h>
+#endif // __WINDOWSAPPSDKTELEMETRY_INCLUDED
--- dev/ProjectReunionTelemetryResource/ProjectReunionTelemetryResource/generaterc.ps1
+++
@@ -1,5 +0,0 @@
-﻿param($packagever, $experimentationLevel)
-$content = Get-Content -Path ".\resource.templ"
-$content = $content -replace "%REUNION_PACKAGE_VER%",$packagever
-$content = $content -replace "%REUNION_EXPERIMENTATION_LEVEL%", $experimentationLevel
-$content | Set-Content ".\resource.rc"
