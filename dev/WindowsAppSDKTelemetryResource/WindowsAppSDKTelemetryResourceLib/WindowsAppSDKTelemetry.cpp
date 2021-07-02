659ea050123dcdd4ed85d262b0f5235e07da0bc7
Chandra Srinivasan <chsriniv@microsoft.com>
2021-07-02 12:35:36 -0700

    This project add a resource dll for telemetry resources
    * Adding a script to auto generate the rc file for the resource dll that will contain the values needed to log PartB telemetry.
    * The .rc file for the resource project is generated from the resource.templ file checked in with the project.
    * The script must be executed before running the build step on this solution.
    * Adding ReunionTelemetry.h that will reference this resource dll to construct the PartB structure.

--- dev/WindowsAppSDKTelemetryResource/WindowsAppSDKTelemetryResourceLib/WindowsAppSDKTelemetry.cpp
+++ dev/WindowsAppSDKTelemetryResource/WindowsAppSDKTelemetryResourceLib/WindowsAppSDKTelemetry.cpp
@@ -13,7 +13,7 @@ inline std::string load_resource_string(const char* dllName, uint32_t id)
     if (module)
     {
         char buffer[256];
-        if (0 <= LoadStringA(module.get(), id, buffer, ARRAYSIZE(buffer)))
+        if (0 < LoadStringA(module.get(), id, buffer, ARRAYSIZE(buffer)))
         {
             return buffer;
         }
