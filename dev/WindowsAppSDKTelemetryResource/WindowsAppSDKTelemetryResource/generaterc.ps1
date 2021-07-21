135225140b5ba3240a4eaf541d900c374b707c3c
Chandra Srinivasan <chandra.srinivasan@outlook.com>
2021-07-21 16:16:01 -0700

    Add WindowsAppSDKResource project
    * Adding a script to auto generate the rc file for the resource dll that will contain the values needed to log PartB telemetry. The PartB struct contains the SDK version, experimentation flag and other data
    * The .rc file for the resource project is generated from the resource.templ file checked in with the project.
    * The script must be executed before running the build step on this solution.
    * Adding WindowsAppSDKTelemetry.h that will reference this resource dll to construct the PartB structure.

---
+++ dev/WindowsAppSDKTelemetryResource/WindowsAppSDKTelemetryResource/generaterc.ps1
@@ -0,0 +1,5 @@
+﻿param($packagever, $experimentationLevel)
+$content = Get-Content -Path ".\resource.templ"
+$content = $content -replace "%WINDOWSAPPSDK_PACKAGE_VER%",$packagever
+$content = $content -replace "%WINDOWSAPPSDK_EXPERIMENTATION_LEVEL%", $experimentationLevel
+$content | Set-Content ".\resource.rc"
