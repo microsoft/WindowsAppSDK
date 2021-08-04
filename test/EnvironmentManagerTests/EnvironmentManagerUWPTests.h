c48b5e5b4a0aef4915d05eccdba86178ab918525
Darren Hoehna <dahoehna@microsoft.com>
2021-08-04 10:44:10 -0700

    Getting UWP tests to run.  All are green.

--- test/EnvironmentManagerTests/EnvironmentManagerUWPTests.h
+++ test/EnvironmentManagerTests/EnvironmentManagerUWPTests.h
@@ -9,11 +9,29 @@ namespace WindowsAppSDKEnvironmentManagerTests
     class EnvironmentManagerUWPTests {
         BEGIN_TEST_CLASS(EnvironmentManagerUWPTests)
             TEST_CLASS_PROPERTY(L"RunAs", L"UAP")
-            TEST_CLASS_PROPERTY(L"RunFixtureAs", L"ElevatedUser")
+            TEST_CLASS_PROPERTY(L"UAP:AppxManifest", L"AppxManifest.pkg.xml")
+            TEST_CLASS_PROPERTY(L"RunFixtureAs:Class", L"RestrictedUser")
+            TEST_CLASS_PROPERTY(L"RunFixtureAs:Method", L"ElevatedUser")
             END_TEST_CLASS()
+
+        TEST_CLASS_SETUP(ClassInit)
+        {
+            ::Test::Bootstrap::Setup();
+            return true;
+        }
+
+        TEST_CLASS_CLEANUP(ClassUninit)
+        {
+            ::Test::Bootstrap::Cleanup();
+            return true;
+        }
 
         TEST_METHOD_SETUP(UWPWriteEVs)
         {
+            VERIFY_IS_TRUE(TP::IsPackageRegistered_WindowsAppSDKFramework());
+            VERIFY_IS_TRUE(TP::IsPackageRegistered_DynamicDependencyDataStore());
+            VERIFY_IS_TRUE(TP::IsPackageRegistered_DynamicDependencyLifetimeManager());
+
             UserSetup();
             MachineSetup();
             return true;
