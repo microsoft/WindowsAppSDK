f8c856d348f29e10a1a5e2197bf4bdc50ac025ff
Rohan Palaniappan <rohanp@microsoft.com>
2021-08-08 12:07:09 -0700

    Tried debugging locally. Macro statement seeminly not in binary. Changed to static function call. Had to fix /MTd and /MDd mismatch between mrm and insights projects. Had to also add an include dir in mrm. Now the telemetry statement is executed. Nothing is logged though. I see a class not registered message in the output. Perhaps that's the reason.

--- dev/MRTCore/mrt/Core/src/MRM.cpp
+++ dev/MRTCore/mrt/Core/src/MRM.cpp
@@ -11,6 +11,8 @@
 #include "mrm/platform/WindowsCore.h"
 #include "mrm/readers/MrmManagers.h"
 
+#include "mrm/common/MrmTraceLogging.h"
+
 #include "MRM.h"
 
 #include <memory>
@@ -492,7 +494,7 @@ STDAPI MrmCreateResourceManager(_In_ PCWSTR priFileName, _Out_ MrmManagerHandle*
 
     RETURN_HR_IF(E_INVALIDARG, (priFileName == nullptr) || (*priFileName == L'\0'));
 
-    WRITE_MRMMIN_INIT_TRACE_INFO_CHECK(priFileName, S_OK);
+    MrtRuntimeProvider::TelemetryGenericEventParam1(L"MrmCreateResourceManager", priFileName, S_OK);
 
     std::unique_ptr<MrmObjects, decltype(&DestroyResourceManager)> resourceManagerObjects(
         new (std::nothrow) MrmObjects(), &DestroyResourceManager);
