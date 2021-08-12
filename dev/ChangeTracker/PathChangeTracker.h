21cfa1dadc1a665f6a82e959fd4e128b1afa2fcf
Darren Hoehna <dahoehna@microsoft.com>
2021-08-12 12:47:54 -0700

    Adding namespace

--- dev/ChangeTracker/PathChangeTracker.h
+++ dev/ChangeTracker/PathChangeTracker.h
@@ -2,7 +2,7 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 #pragma once
 #include "IChangeTracker.h"
-#include <EnvironmentManager.h>
+#include <Microsoft.Windows.System.EnvironmentManager.h>
 #include <wil/registry.h>
 
 namespace winrt::Microsoft::Windows::System::implementation
