d889c8b881f8e2b0f055729316b1af414670741b
zenbird <chandra.srinivasan@outlook.com>
2021-08-18 15:35:06 -0700

    Moving from the static library based solution for telemetry to a header based approach to reduce complexity.
    The difference (on disk) size between the lib based approach and header based approach is 1 KB (header > lib). In favor of reducing the complexity, the decision has been made to go with a header based solution

--- dev/WindowsAppSDK_Insights/WindowsAppSDKInsights.h
+++ dev/WindowsAppSDK_Insights/WindowsAppSDKInsights.h
@@ -31,25 +31,25 @@ inline std::string load_resource_string(const char* dllName, uint32_t id)
         }
         else
         {
-            LOG_LAST_ERROR_MSG("Unable to load resource string, id: %d, error: %d", id, GetLastError());
+            LOG_LAST_ERROR_MSG("Unable to load resource string, id: %d", id);
         }
         
     }
     else
     {
-        LOG_LAST_ERROR_IF_MSG(module == nullptr, "Unable to load dll: %s", dllName);
+        LOG_LAST_ERROR_MSG("Unable to load dll: %s", dllName);
     }
 
     return {};
 }
 
-__declspec(dllexport) char* WINDOWSAPPSDK_PACKAGE_VER()
+static char* WINDOWSAPPSDK_PACKAGE_VER()
 {
     static std::string packageVer = load_resource_string(INSIGHTSRESOURCEDLL, _WINDOWSAPPSDK_PACKAGE_VER_RESOURCEID);
     return const_cast<char*>(packageVer.c_str());
 }
 
-__declspec(dllexport) char* WINDOWSAPPSDK_EXPERIMENTATION_LEVEL()
+static char* WINDOWSAPPSDK_EXPERIMENTATION_LEVEL()
 {
     static std::string experimentationLevel = load_resource_string(INSIGHTSRESOURCEDLL, _WINDOWSAPPSDK_EXPERIMENTATION_FLAG_RESOURCEID);
     return const_cast<char*>(experimentationLevel.c_str());
