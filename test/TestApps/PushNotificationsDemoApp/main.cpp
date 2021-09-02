a6fc147e35614dcd7725d29aa61a31b23df78cfa
Sharath Manchala <10109130+sharath2727@users.noreply.github.com>
2021-09-02 07:07:22 -0700

    Fix minor nits

--- test/TestApps/PushNotificationsDemoApp/main.cpp
+++ test/TestApps/PushNotificationsDemoApp/main.cpp
@@ -4,7 +4,6 @@
 #include <wil/win32_helpers.h>
 #include <iostream>
 #include <winrt/Windows.ApplicationModel.Background.h>
-#include "Helpers.h"
 
 using namespace winrt::Microsoft::Windows::AppLifecycle;
 using namespace winrt::Microsoft::Windows::PushNotifications;
@@ -90,12 +89,11 @@ winrt::Microsoft::Windows::PushNotifications::PushNotificationChannel RequestCha
 
 int main()
 {
-    BootstrapInitialize();/*
     PushNotificationActivationInfo info(
         PushNotificationRegistrationOptions::PushTrigger | PushNotificationRegistrationOptions::ComActivator,
         winrt::guid("ccd2ae3f-764f-4ae3-be45-9804761b28b2")); // same clsid as app manifest
 
-    auto token = PushNotificationManager::RegisterActivator(info); */
+    auto token = PushNotificationManager::RegisterActivator(info);
 
     auto args = AppInstance::GetCurrent().GetActivatedEventArgs();
     auto kind = args.Kind();
@@ -132,7 +130,7 @@ int main()
     }
 
     // Don't unregister PushTrigger because we still want to receive push notifications from background infrastructure.
-   // PushNotificationManager::UnregisterActivator(token, PushNotificationRegistrationOptions::ComActivator);
+    PushNotificationManager::UnregisterActivator(token, PushNotificationRegistrationOptions::ComActivator);
 
     return 0;
 }
