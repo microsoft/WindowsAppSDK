d94d81a87c9193cf538ff5f13a98719ecee5aac9
Howard Kapustein <howardk@microsoft.com>
2021-09-07 14:13:26 -0700

    GenerationId has promoted to non-experimental. Remove experimental-ness from it :-)

--- test/DynamicDependency/Test_Win32/Test_GetCurrentPackageInfo.cpp
+++ test/DynamicDependency/Test_Win32/Test_GetCurrentPackageInfo.cpp
@@ -248,15 +248,7 @@ namespace Test::DynamicDependency
             VERIFY_ARE_EQUAL(expectedGenerationId, generationId);
 
             const auto actualGenerationId{ MddGetGenerationId() };
-            if (!::Microsoft::Windows::ApplicationModel::DynamicDependency::Feature_GenerationId::IsEnabled())
-            {
-                const auto expectedGeneratedIdIfDisabled{ static_cast<UINT32>(~0) };
-                VERIFY_ARE_EQUAL(expectedGeneratedIdIfDisabled, actualGenerationId);
-            }
-            else
-            {
-                VERIFY_ARE_EQUAL(expectedGenerationId, actualGenerationId);
-            }
+            VERIFY_ARE_EQUAL(expectedGenerationId, actualGenerationId);
         }
 
         void PrintGetCurrentPackageInfoHeader()
