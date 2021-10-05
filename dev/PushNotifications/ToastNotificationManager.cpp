d5db466fc3fd44e06e73edbc7913461d614a2add
Paul Purifoy <purifoypaul@microsoft.com>
2021-10-05 09:57:19 -0700

    Prototyping registry

---
+++ dev/PushNotifications/ToastNotificationManager.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+
+#include "ToastNotificationManager.h"
+#include "Microsoft.Windows.PushNotifications.ToastNotificationManager.g.cpp"
+#include "winrt/Windows.UI.h"
+#include <frameworkudk/pushnotifications.h>
+#include "NotificationsLongRunningProcess_h.h"
+#include "PushNotificationTelemetry.h"
+#include <TerminalVelocityFeatures-PushNotifications.h>
+
+namespace winrt
+{
+    using namespace winrt::Windows::UI;
+    using namespace Windows::Foundation;
+}
+
+namespace winrt::Microsoft::Windows::PushNotifications::implementation
+{
+    void ToastNotificationManager::RegisterActivator(winrt::hstring displayName, Uri iconUri, Color const& color)
+    {
+        auto notificationPlatform{ wil::CoCreateInstance<NotificationsLongRunningPlatform, INotificationsLongRunningPlatform>(CLSCTX_LOCAL_SERVER) };
+
+        wil::unique_cotaskmem_string processName;
+        THROW_IF_FAILED(GetCurrentProcessPath(processName));
+
+        THROW_IF_FAILED(notificationPlatform->RegisterFullTrustApplication(processName.get(), remoteId, &unpackagedAppUserModelId));
+
+        
+        return;
+    }    
+}
---
+++ dev/PushNotifications/ToastNotificationManager.h
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#pragma once
+#include "Microsoft.Windows.PushNotifications.ToastNotificationManager.g.h"
+#include <windows.foundation.h>
+#include "winrt/Windows.UI.h"
+
+namespace winrt::Microsoft::Windows::PushNotifications::implementation
+{
+
+    struct ToastNotificationManager
+    {
+        ToastNotificationManager() = delete;
+
+        static void RegisterActivator(winrt::hstring displayName, winrt::Windows::Foundation::Uri iconUri, winrt::Windows::UI::Color const& color);
+    };
+}
+namespace winrt::Microsoft::Windows::PushNotifications::factory_implementation
+{
+    struct ToastNotificationManager : ToastNotificationManagerT<ToastNotificationManager, implementation::ToastNotificationManager>
+    {
+    };
+}
