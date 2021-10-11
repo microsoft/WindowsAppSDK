277bd78af8b985ccc8bf561eaebb9f404768778e
Darren Hoehna <dahoehna@microsoft.com>
2021-10-11 13:21:24 -0700

    Adding first steps

---
+++ dev/CommandLineParser/CommandLineParser.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CommandLineParser.h"
+#include "Microsoft.Windows.System.CommandLineParser.g.cpp"
+
+namespace winrt::Microsoft::Windows::System::implementation
+{
+    CommandLineParser::CommandLineParser(hstring const& arguments)
+        : m_Arguments(arguments)
+    {}
+
+    void CommandLineParser::AddArguments(hstring const& arguments)
+    {
+        if ((m_Arguments.back() != L' ') ||
+            (arguments.front() != L' '))
+        {
+            m_Arguments.append(L" ");
+        }
+
+        m_Arguments.append(arguments);
+    }
+}
---
+++ dev/CommandLineParser/CommandLineParser.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include "Microsoft.Windows.System.CommandLineParser.g.h"
+#include <string>
+#include <map>
+
+using namespace winrt::Windows::Foundation::Collections;
+
+namespace winrt::Microsoft::Windows::System::implementation
+{
+    struct CommandLineParser : CommandLineParserT<CommandLineParser>
+    {
+        CommandLineParser() = default;
+
+        CommandLineParser(hstring const& arguments);
+        void AddArguments(hstring const& arguments);
+
+    private:
+        std::wstring m_Arguments{};
+        std::map<std::wstring, std::wstring> m_ParsedArguments{};
+
+    };
+}
+namespace winrt::Microsoft::Windows::System::factory_implementation
+{
+    struct CommandLineParser : CommandLineParserT<CommandLineParser, implementation::CommandLineParser>
+    {
+    };
+}
