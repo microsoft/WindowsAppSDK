a86c9ee77729949e3c4551c91dd26dfbd8b0ec25
Shashank Nayak <shasnayak@microsoft.com>
2021-10-13 14:57:53 -0400

    end of line white space + min PackageVersion constant change

--- dev/WindowsAppRuntime_BootstrapDLL/MddBootstrapAutoInitializer.cpp
+++ dev/WindowsAppRuntime_BootstrapDLL/MddBootstrapAutoInitializer.cpp
@@ -23,7 +23,7 @@ namespace Microsoft::Windows::ApplicationModel::DynamicDependency::Bootstrap
         {
             const UINT32 c_majorMinorVersion{ WINDOWSAPPSDK_RELEASE_MAJORMINOR };
             PCWSTR c_versionTag{ WINDOWSAPPSDK_RELEASE_VERSION_TAG_W };
-            const PACKAGE_VERSION c_minVersion{ WINDOWSAPPSDK_RELEASE_VERSION_UINT64 };
+            const PACKAGE_VERSION c_minVersion{ WINDOWSAPPSDK_RUNTIME_VERSION_UINT64 };
             const HRESULT hr{ ::MddBootstrapInitialize(c_majorMinorVersion, c_versionTag, c_minVersion) };
             if (FAILED(hr))
             {
