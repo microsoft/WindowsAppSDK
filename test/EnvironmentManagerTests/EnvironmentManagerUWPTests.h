f8898b30b55980079ad216c609fa396b8745e158
Darren Hoehna <dhoehna@yahoo.com>
2021-10-13 11:53:48 -0700

    Adding EM to Terminal Velocity (#1576)
    * Adding EM to Terminal Velocity
    
    * Marking all methods experimental
    
    * Adressing comments
    
    * Returning false for tests.
    
    * Fixing punction mistake.  Returning a better error for velocity.
    
    * Forgot to save
    
    * Removing absolute path.  Removing unnecessary IsEnabled checks
    
    * Addressing comments:
    
    * Adding change
    
    Co-authored-by: Darren Hoehna <dahoehna@microsoft.com>

--- test/EnvironmentManagerTests/EnvironmentManagerUWPTests.h
+++ test/EnvironmentManagerTests/EnvironmentManagerUWPTests.h
@@ -3,6 +3,7 @@
 
 #pragma once
 #include "TestSetupAndTeardownHelper.h"
+#include <TerminalVelocityFeatures-EnvironmentManager.h>
 
 namespace WindowsAppSDKEnvironmentManagerTests
 {
@@ -16,6 +17,12 @@ namespace WindowsAppSDKEnvironmentManagerTests
 
         TEST_CLASS_SETUP(ClassInit)
         {
+            if (!::Microsoft::Windows::System::Feature_EnvironmentManager::IsEnabled())
+            {
+                WEX::Logging::Log::Result(WEX::Logging::TestResults::Skipped, L"Environment Manager is experimental.");
+                return false;
+            }
+
             ::Test::Bootstrap::Setup();
             return true;
         }
