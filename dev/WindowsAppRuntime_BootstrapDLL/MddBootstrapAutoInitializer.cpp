1a7141c92611f1c02fced3129f58a668d825213b
Shashank Nayak <shasnayak@microsoft.com>
2021-11-05 13:08:20 -0400

    fail fast fix

--- dev/WindowsAppRuntime_BootstrapDLL/MddBootstrapAutoInitializer.cpp
+++ dev/WindowsAppRuntime_BootstrapDLL/MddBootstrapAutoInitializer.cpp
@@ -5,6 +5,7 @@
 #include <stdint.h>
 #include <MddBootstrap.h>
 #include <WindowsAppSDK-VersionInfo.h>
+#include <intrin.h>
 
 namespace Microsoft::Windows::ApplicationModel::DynamicDependency::Bootstrap
 {
@@ -22,13 +23,11 @@ namespace Microsoft::Windows::ApplicationModel::DynamicDependency::Bootstrap
 
         static void Initialize()
         {
-            const UINT32 c_majorMinorVersion{ WINDOWSAPPSDK_RELEASE_MAJORMINOR };
-            PCWSTR c_versionTag{ WINDOWSAPPSDK_RELEASE_VERSION_TAG_W };
-            const PACKAGE_VERSION c_minVersion{ WINDOWSAPPSDK_RUNTIME_VERSION_UINT64 };
-            const HRESULT hr{ ::MddBootstrapInitialize(c_majorMinorVersion, c_versionTag, c_minVersion) };
+            const PACKAGE_VERSION c_minVersion{WINDOWSAPPSDK_RUNTIME_VERSION_UINT64};
+            const HRESULT hr{::MddBootstrapInitialize(WINDOWSAPPSDK_RELEASE_MAJORMINOR, WINDOWSAPPSDK_RELEASE_VERSION_TAG_W, c_minVersion)};
             if (FAILED(hr))
             {
-                exit(hr);
+                __fastfail(hr);
             }
         }
     };
