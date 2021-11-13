cfd59e276f4e7a33f3b381f22da7835ba39fa459
Howard Kapustein <howardk@microsoft.com>
2021-11-13 00:43:59 -0800

    Add Licensing API and export from Bootstrap'r

---
+++ dev/Licensing/MsixLicensing.cpp
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+
+#include "pch.h"
+
+#include "msixlicensing.h"
+
+#include <WindowsAppRuntime-License.h>
+
+STDAPI MsixInstallLicenses() noexcept try
+{
+    //TODO
+    return S_OK;
+}
+CATCH_RETURN();
