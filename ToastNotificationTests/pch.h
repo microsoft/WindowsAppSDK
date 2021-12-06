ba7464ef375cfe0eb0756581ca865de72412d115
Paul Purifoy <purifoypaul@microsoft.com>
2021-12-06 11:50:25 -0800

    Making it blank

--- ToastNotificationTests/pch.h
+++ ToastNotificationTests/pch.h
@@ -1,4 +1,4 @@
-// pch.h: This is a precompiled header file.
+﻿// pch.h: This is a precompiled header file.
 // Files listed below are compiled only once, improving build performance for future builds.
 // This also affects IntelliSense performance, including code completion and many code browsing features.
 // However, files listed here are ALL re-compiled if any one of them is updated between builds.
@@ -6,8 +6,5 @@
 
 #ifndef PCH_H
 #define PCH_H
-
-// add headers that you want to pre-compile here
-#include "framework.h"
 
 #endif //PCH_H
