11723ccd8ec3812ab10cbb67f651e8fa7b4743f7
Steve Otteson <sotteson@microsoft.com>
2022-01-26 16:46:35 -0800

    Add cswin32 to C# projects

--- dev/VSIX/ProjectTemplates/Desktop/CSharp/PackagedApp/BlankApp/App.xaml.cs
+++ dev/VSIX/ProjectTemplates/Desktop/CSharp/PackagedApp/BlankApp/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
@@ -15,6 +16,10 @@ using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Microsoft.UI.Xaml.Shapes;
+using static Windows.Win32.PInvoke;
+using Windows.Win32.Foundation;
+using Windows.Win32.UI.WindowsAndMessaging;
+using WinRT;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,6 +31,9 @@ namespace $safeprojectname$
     /// </summary>
     public partial class App : Application
     {
+        private Window m_window;
+        private HWND m_hwnd;
+
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
@@ -43,9 +51,41 @@ namespace $safeprojectname$
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
             m_window = new MainWindow();
+
+            var windowNative = m_window.As<IWindowNative>();
+            m_hwnd = new HWND(windowNative.WindowHandle);
+
             m_window.Activate();
+
+            SetWindowSize(m_hwnd, 800, 600);
         }
 
-        private Window m_window;
+        /// <summary>
+        /// Uses Win32 interop to set the window size
+        /// </summary>
+        /// <param name="hwnd">Window handle</param>
+        /// <param name="width">Window width</param>
+        /// <param name="height">Window height</param>
+        private static void SetWindowSize(HWND hwnd, int width, int height)
+        {
+            var dpi = GetDpiForWindow(hwnd);
+            float scalingFactor = (float)dpi / 96f;
+            width = (int)(width * scalingFactor);
+            height = (int)(height * scalingFactor);
+
+            SetWindowPos(
+                hwnd, 
+                HWND_TOP,
+                0, 0, width, height,
+                SET_WINDOW_POS_FLAGS.SWP_NOMOVE);
+        }
+
+        [ComImport]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        [Guid("EECDBF0E-BAE9-4CB6-A68E-9598E1CB57BB")]
+        internal interface IWindowNative
+        {
+            IntPtr WindowHandle { get; }
+        }
     }
 }
