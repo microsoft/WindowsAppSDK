ff9e3bae3e3167920cdb5b3ea33e90a853fdc0e8
Venkata Sharath Chandra Manchala <vemancha@microsoft.com>
2022-02-01 05:37:11 -0800

    Complete implementing unittests

---
+++ dev/ToastNotifications/ToastProgressDataABI.cpp
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#include "pch.h"
+
+#include "ToastProgressData.h"
+#include "ToastProgressDataABI.h"
+#include "ToastNotificationUtility.h"
+
+namespace winrt
+{
+    using namespace winrt::Microsoft::Windows::ToastNotifications;
+}
+
+namespace ToastABI
+{
+    using namespace ::ABI::Microsoft::Internal::ToastNotifications;
+}
+
+ToastProgressDataABI::ToastProgressDataABI(winrt::ToastProgressData const& progressData)
+{
+    m_progressData = progressData;
+}
+
+STDMETHODIMP_(HRESULT __stdcall) ToastProgressDataABI::get_SequenceNumber(_Out_ unsigned int* value)
+{
+    *value = m_progressData.SequenceNumber();
+
+    return S_OK;
+}
+
+STDMETHODIMP_(HRESULT __stdcall) ToastProgressDataABI::get_Title(_Out_ HSTRING* value)
+{
+    *value = safe_make_unique_hstring(m_progressData.Title().c_str()).release();
+
+    return S_OK;
+}
+
+STDMETHODIMP_(HRESULT __stdcall) ToastProgressDataABI::get_Value(_Out_ double* value)
+{
+    *value = m_progressData.Value();
+
+    return S_OK;
+}
+
+STDMETHODIMP_(HRESULT __stdcall) ToastProgressDataABI::get_ValueStringOverride(_Out_ HSTRING* value)
+{
+    *value = safe_make_unique_hstring(m_progressData.ValueStringOverride().c_str()).release();
+
+    return S_OK;
+}
+
+STDMETHODIMP_(HRESULT __stdcall) ToastProgressDataABI::get_Status(_Out_ HSTRING* value)
+{
+    *value = safe_make_unique_hstring(m_progressData.Status().c_str()).release();
+
+    return S_OK;
+}
+
