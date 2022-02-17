d0c84b8d71dbbb50de1c515785bb2ec0c2eb3612
Paul Purifoy <purifoypaul@microsoft.com>
2022-02-17 14:08:16 -0800

    Move pushHandlers to PushNotificationManager

--- dev/PushNotifications/PushNotificationChannel.cpp
+++ dev/PushNotifications/PushNotificationChannel.cpp
@@ -6,7 +6,6 @@
 #include "PushNotificationChannel.h"
 #include "Microsoft.Windows.PushNotifications.PushNotificationChannel.g.cpp"
 #include <winrt\Windows.Foundation.h>
-#include "PushNotificationReceivedEventArgs.h"
 #include <FrameworkUdk/PushNotifications.h>
 #include "externs.h"
 #include "PushNotificationTelemetry.h"
@@ -53,96 +52,4 @@ namespace winrt::Microsoft::Windows::PushNotifications::implementation
             }
         }
     }
-
-    winrt::event_token PushNotificationChannel::PushReceived(TypedEventHandler<winrt::Microsoft::Windows::PushNotifications::PushNotificationChannel, winrt::Microsoft::Windows::PushNotifications::PushNotificationReceivedEventArgs> handler)
-    {
-        bool registeredEvent { false };
-        {
-            auto lock { m_lock.lock_shared() };
-            registeredEvent = bool(m_foregroundHandlers);
-        }
-
-        if(!registeredEvent)
-        {
-            if(PushNotificationHelpers::IsPackagedAppScenario())
-            {
-                auto appUserModelId{ PushNotificationHelpers::GetAppUserModelId() };
-
-                // Register a sink with platform which is initialized in the current process
-                THROW_IF_FAILED(PushNotifications_RegisterNotificationSinkForFullTrustApplication(appUserModelId.get(), this));
-            }
-            else
-            {
-                auto notificationsLongRunningPlatform{ PushNotificationHelpers::GetNotificationPlatform() };
-
-                wil::unique_cotaskmem_string processName;
-                THROW_IF_FAILED(GetCurrentProcessPath(processName));
-
-                // Register a sink with platform brokered through PushNotificationsLongRunningProcess
-                THROW_IF_FAILED(notificationsLongRunningPlatform->RegisterForegroundActivator(this, processName.get()));
-            }
-        }
-
-        auto lock { m_lock.lock_exclusive() };
-        return m_foregroundHandlers.add(handler);
-    }
-
-    void PushNotificationChannel::PushReceived(winrt::event_token const& token) noexcept
-    {
-        bool registeredEvent { false };
-        {
-            auto lock { m_lock.lock_exclusive() };
-            m_foregroundHandlers.remove(token);
-            registeredEvent = bool(m_foregroundHandlers);
-        }
-
-        if(!registeredEvent)
-        {
-            // Packaged apps with BI available will remove their handlers from the platform.
-            // Unpackaged apps / Packaged apps treated as unpackaged will unregister from Long Running process.
-            if (PushNotificationHelpers::IsPackagedAppScenario())
-            {
-                auto appUserModelId{ PushNotificationHelpers::GetAppUserModelId() };
-
-                THROW_IF_FAILED(PushNotifications_UnregisterNotificationSinkForFullTrustApplication(appUserModelId.get()));
-            }
-            else
-            {
-                auto notificationsLongRunningPlatform{ PushNotificationHelpers::GetNotificationPlatform() };
-
-                wil::unique_cotaskmem_string processName;
-                THROW_IF_FAILED(GetCurrentProcessPath(processName));
-
-                THROW_IF_FAILED(notificationsLongRunningPlatform->UnregisterForegroundActivator(processName.get()));
-            }
-        }
-    }
-
-    HRESULT __stdcall PushNotificationChannel::InvokeAll(_In_ ULONG length, _In_ byte* payload, _Out_ BOOL* foregroundHandled) noexcept try
-    {
-        auto args = winrt::make<winrt::Microsoft::Windows::PushNotifications::implementation::PushNotificationReceivedEventArgs>(payload, length);
-        m_foregroundHandlers(*this, args);
-        *foregroundHandled = args.Handled();
-        return S_OK;
-    }
-    CATCH_RETURN()
-
-    HRESULT __stdcall PushNotificationChannel::OnRawNotificationReceived(unsigned int payloadLength, _In_ byte* payload, _In_ HSTRING /*correlationVector */) noexcept try
-    {
-        BOOL foregroundHandled = true;
-        THROW_IF_FAILED(InvokeAll(payloadLength, payload, &foregroundHandled));
-
-        if (!foregroundHandled)
-        {
-            if (!AppModel::Identity::IsPackagedProcess())
-            {
-                wil::unique_cotaskmem_string processName;
-                THROW_IF_FAILED(GetCurrentProcessPath(processName));
-                THROW_IF_FAILED(PushNotificationHelpers::ProtocolLaunchHelper(processName.get(), payloadLength, payload));
-            }
-        }
-
-        return S_OK;
-    }
-    CATCH_RETURN();
 }
