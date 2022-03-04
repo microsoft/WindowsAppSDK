7e0cfa86503e67de037ce3bb9182b074ae6354ea
Paul Purifoy <purifoypaul@microsoft.com>
2022-03-04 14:15:19 -0800

    Working

---
+++ test/TestApps/PushNotifications-SelfContained/main.cpp
@@ -0,0 +1,11 @@
+﻿#include "pch.h"
+
+using namespace winrt;
+using namespace Windows::Foundation;
+using namespace winrt::Microsoft::Windows::AppLifecycle;
+
+int main()
+{
+    auto args = AppInstance::GetCurrent().GetActivatedEventArgs();
+    return 0;
+}
