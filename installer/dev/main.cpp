4c18e31d932adccf34ff63e4a5cdf84306d25674
sachintaMSFT <80828309+sachintaMSFT@users.noreply.github.com>
2022-03-10 10:38:09 -0800

    Add telemetry to Installer (#2046)
    * Add Telemetry to Installer
    
    * Add WindowsAppRuntime PartB info to Installer Telemetry
    
    * Added new --info option to display Privacy and License disclosures

--- installer/dev/main.cpp
+++ installer/dev/main.cpp
@@ -8,16 +8,23 @@
 
 using namespace winrt;
 
+using namespace WindowsAppRuntimeInstaller::Console;
+
 int wmain(int argc, wchar_t *argv[])
 {
     init_apartment();
 
+    wil::SetResultLoggingCallback(wilResultLoggingCallback);
+
     auto options{ WindowsAppRuntimeInstaller::Options::InstallPackages |
                   WindowsAppRuntimeInstaller::Options::InstallLicenses };
+
+    std::wstringstream args{};
 
     for (int i = 1; i < argc; ++i)
     {
         auto arg = std::wstring_view(argv[i]);
+
         if (arg == L"--dry-run")
         {
             WI_SetFlag(options, WindowsAppRuntimeInstaller::Options::DryRun);
@@ -48,16 +55,29 @@ int wmain(int argc, wchar_t *argv[])
         }
         else if ((arg == L"-?") || (arg == L"--help"))
         {
-            ShowHelp();
+            DisplayHelp();
+            return 1;
+        }
+        else if ((arg == L"--info"))
+        {
+            DisplayInfo();
             return 0;
         }
         else
         {
             std::wcerr << "Unknown argument: " << arg.data() << std::endl;
-            ShowHelp();
+            DisplayHelp();
             return ERROR_BAD_ARGUMENTS;
         }
+
+        // Capture valid arguments only
+        args << argv[i] << " ";
     }
+
+    auto& installActivityContext{ WindowsAppRuntimeInstaller::InstallActivity::Context::Get() };
+
+    installActivityContext.SetActivity(WindowsAppRuntimeInstaller_TraceLogger::Install::Start(args.str().c_str(), static_cast<UINT32>(options)));
+    args.clear();
 
     const HRESULT deployPackagesResult{ WindowsAppRuntimeInstaller::Deploy(options) };
     if (WI_IsFlagClear(options, WindowsAppRuntimeInstaller::Options::Quiet))
@@ -65,13 +85,25 @@ int wmain(int argc, wchar_t *argv[])
         if (SUCCEEDED(deployPackagesResult))
         {
             std::wcout << "All install operations successful." << std::endl;
+
+            if (!Security::IntegrityLevel::IsElevated())
+            {
+                std::wcout << "WARNING: Since WindowsAppRuntimeInstaller.exe is not run elevated, WindowsAppSDK packages are not provisioned for all users on your machine." << std::endl;
+            }
         }
         else
         {
+            installActivityContext.GetActivity().StopWithResult(
+                deployPackagesResult,
+                static_cast<UINT32>(installActivityContext.GetInstallStage()),
+                installActivityContext.GetCurrentResourceId().c_str(),
+                installActivityContext.GetDeploymentErrorExtendedHResult(),
+                installActivityContext.GetDeploymentErrorText().c_str(),
+                installActivityContext.GetDeploymentErrorActivityId());
+
             std::wcerr << "One or more install operations failed. Result: 0x" << std::hex << deployPackagesResult << std::endl;
         }
     }
 
     return deployPackagesResult;
 }
-
