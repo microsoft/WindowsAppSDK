98eb12775b2de496a21f41bb6510c819c5442cf6
Howard Kapustein <howardk@microsoft.com>
2022-03-28 20:58:13 -0700

    Let's try that. C# AutoInitializer options override

--- dev/WindowsAppRuntime_BootstrapDLL/MddBootstrapAutoInitializer.cs
+++ dev/WindowsAppRuntime_BootstrapDLL/MddBootstrapAutoInitializer.cs
@@ -11,13 +11,26 @@ namespace Microsoft.Windows.ApplicationModel.DynamicDependency.BootstrapCS
             uint majorMinorVersion = global::Microsoft.WindowsAppSDK.Release.MajorMinor;
             string versionTag = global::Microsoft.WindowsAppSDK.Release.VersionTag;
             var minVersion = new PackageVersion(global::Microsoft.WindowsAppSDK.Runtime.Version.UInt64);
+            var options = Options;
             try
             {
-                global::Microsoft.Windows.ApplicationModel.DynamicDependency.Bootstrap.Initialize(majorMinorVersion, versionTag, minVersion);
+                global::Microsoft.Windows.ApplicationModel.DynamicDependency.Bootstrap.Initialize(majorMinorVersion, versionTag, minVersion, options);
             }
             catch (global::System.Exception e)
             {
                 global::System.Environment.FailFast(e.Message);
+            }
+        }
+
+        internal static global::Microsoft.Windows.ApplicationModel.DynamicDependency.Bootstrap.InitializeOptions Options
+        {
+            get
+            {
+#ifdef MICROSOFT_WINDOWSAPPSDK_BOOTSTRAP_AUTOINITIALIZER_OPTIONS_OVERRIDE
+                return Microsoft.Windows.ApplicationModel.DynamicDependency.Bootstrap.AutoInitializer.InitializeOptions.Override.GetDefault();
+#else
+                return Microsoft.Windows.ApplicationModel.DynamicDependency.Bootstrap.InitializeOptions.OnNoMatch_ShowUI;
+#endif
             }
         }
     }
