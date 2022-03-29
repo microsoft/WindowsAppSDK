08fddd249cb9cfd19e7567516ddeaa0704408b03
Howard Kapustein <howardk@microsoft.com>
2022-03-28 17:49:21 -0700

    Update C++ AutoInitializer to use MddBootstrapInitialize2 with options override (for those who don't like the default)

--- dev/WindowsAppRuntime_BootstrapDLL/MddBootstrapAutoInitializer.cpp
+++ dev/WindowsAppRuntime_BootstrapDLL/MddBootstrapAutoInitializer.cpp
@@ -21,13 +21,24 @@ namespace Microsoft::Windows::ApplicationModel::DynamicDependency::Bootstrap
             ::MddBootstrapShutdown();
         }
 
+        constexpr static MddBootstrapInitializeOptions Options()
+        {
+#if defined(MICROSOFT_WINDOWSAPPSDK_BOOTSTRAP_INITIALIZE_OPTIONS)
+            // #define MICROSOFT_WINDOWSAPPSDK_BOOTSTRAP_INITIALIZE_OPTIONS=options (e.g. =0x0012 or =18)
+            return static_cast<MddBootstrapInitializeOptions>(MICROSOFT_WINDOWSAPPSDK_BOOTSTRAP_INITIALIZE_OPTIONS);
+#else
+            // Default options for the auto-initializer
+            return MddBootstrapInitializeOptions_OnNoMatch_ShowUI;
+#endif
+        }
+
         static void Initialize()
         {
             const UINT32 c_majorMinorVersion{ WINDOWSAPPSDK_RELEASE_MAJORMINOR };
             PCWSTR c_versionTag{ WINDOWSAPPSDK_RELEASE_VERSION_TAG_W };
             const PACKAGE_VERSION c_minVersion{ WINDOWSAPPSDK_RUNTIME_VERSION_UINT64 };
-            const auto c_options{ MddBootstrapInitializeOptions_OnNoMatch_ShowUI };
-            const HRESULT hr{ ::MddBootstrapInitialize2(c_majorMinorVersion, c_versionTag, c_minVersion, c_options) };
+            const auto c_options{ Options() };
+            const HRESULT hr{ ::MddBootstrapInitialize2(c_majorMinorVersion, c_versionTag, c_minVersion, c_options };
             if (FAILED(hr))
             {
                 exit(hr);
