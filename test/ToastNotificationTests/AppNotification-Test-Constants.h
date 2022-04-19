6d29c56e4c210f08be2f410e1c7eb4cd810bfed2
Paul Purifoy <pmpurifoy@gmail.com>
2022-04-19 16:17:16 -0700

    Update AppNotification-Test-Constants.h

--- test/ToastNotificationTests/AppNotification-Test-Constants.h
+++ test/ToastNotificationTests/AppNotification-Test-Constants.h
@@ -3,5 +3,5 @@
 #pragma once
 
 inline const winrt::hstring c_rawNotificationPayload = L"<toast></toast>";
-static const std::chrono::seconds c_timeout = std::chrono::seconds(300);
+inline const std::chrono::seconds c_timeout = std::chrono::seconds(300);
 inline IID c_toastComServerId = winrt::guid("1940dba9-0f64-4f0d-8a4b-5d207b812e61"); // Value from ToastNotificationsTestAppPackage ComActivator in appxmanifest.
