1b59fce44691b836af04f18fea0327f1fa2a583e
Paul Purifoy <33183370+pmpurifoy@users.noreply.github.com>
2022-04-19 17:07:21 -0700

    Refactoring PushNotification unit tests  (#2389)
    * Work in progress
    
    * Push tests working individually
    
    * Adding LRP to singleton, nits
    
    * Update PowerNotifications.vcxproj
    
    * Condensed packaged/unpackaged self contained tests
    
    * Used IsSupported flag instead
    
    * Working on it
    
    * BaseTestSuite working
    
    * Removed unused namespaces
    
    * Addressing nits
    
    * Use verify instead of throw
    
    * rename m_registered -> m_unregisteredFully
    
    * Ignored failing tests
    
    * Fix build issues
    
    * Update PushNotificationTests.vcxproj
    
    * Update AppNotification-Test-Constants.h

---
+++ test/PushNotificationTests/UnpackagedTests.cpp
@@ -0,0 +1,80 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+#include "pch.h"
+#include "UnpackagedTests.h"
+
+void UnpackagedTests::ChannelRequestUsingNullRemoteId()
+{
+    BaseTestSuite::ChannelRequestUsingNullRemoteId();
+}
+
+void UnpackagedTests::ChannelRequestUsingRemoteId()
+{
+    BaseTestSuite::ChannelRequestUsingRemoteId();
+}
+
+// Currently failing - https://github.com/microsoft/WindowsAppSDK/issues/2392
+void UnpackagedTests::MultipleChannelClose()
+{
+    BaseTestSuite::MultipleChannelClose();
+}
+
+void UnpackagedTests::VerifyRegisterAndUnregister()
+{
+    BaseTestSuite::VerifyRegisterAndUnregister();
+}
+
+void UnpackagedTests::VerifyUnregisterFails()
+{
+    BaseTestSuite::VerifyUnregisterFails();
+}
+
+void UnpackagedTests::VerifyUnregisterAllFails()
+{
+    BaseTestSuite::VerifyUnregisterAllFails();
+}
+
+void UnpackagedTests::VerifyRegisterAndUnregisterAll()
+{
+    BaseTestSuite::VerifyRegisterAndUnregisterAll();
+}
+
+void UnpackagedTests::MultipleRegister()
+{
+    BaseTestSuite::MultipleRegister();
+}
+
+void UnpackagedTests::VerifyMultipleRegisterAndUnregister()
+{
+    BaseTestSuite::VerifyMultipleRegisterAndUnregister();
+}
+
+void UnpackagedTests::VerifyMultipleRegisterAndUnregisterAll()
+{
+    BaseTestSuite::VerifyMultipleRegisterAndUnregisterAll();
+}
+
+void UnpackagedTests::VerifyUnregisterTwice()
+{
+    BaseTestSuite::VerifyUnregisterTwice();
+}
+
+void UnpackagedTests::VerifyUnregisterAllTwice()
+{
+    BaseTestSuite::VerifyUnregisterAllTwice();
+}
+
+void UnpackagedTests::VerifyUnregisterAndUnregisterAll()
+{
+    BaseTestSuite::VerifyUnregisterAndUnregisterAll();
+}
+
+void UnpackagedTests::VerifyForegroundHandlerSucceeds()
+{
+    BaseTestSuite::VerifyForegroundHandlerSucceeds();
+}
+
+void UnpackagedTests::VerifyForegroundHandlerFails()
+{
+    BaseTestSuite::VerifyForegroundHandlerFails();
+}
---
+++ test/ToastNotificationTests/AppNotification-Test-Constants.h
@@ -0,0 +1,7 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+#pragma once
+
+inline const winrt::hstring c_rawNotificationPayload = L"<toast></toast>";
+inline const std::chrono::seconds c_timeout = std::chrono::seconds(300);
+inline IID c_toastComServerId = winrt::guid("1940dba9-0f64-4f0d-8a4b-5d207b812e61"); // Value from ToastNotificationsTestAppPackage ComActivator in appxmanifest.
