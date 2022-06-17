63d2c9f07a3c354e518b0bd23990694e58fd85d6
Jeff Genovy <jefgen@microsoft.com>
2022-06-16 17:20:45 -0700

    Fix

--- dev/MRTCore/build/DownloadDotNetCoreSdk.ps1
+++ dev/MRTCore/build/DownloadDotNetCoreSdk.ps1
@@ -7,7 +7,7 @@ param(
 $dotnetInstallScript = "$env:TEMP\dotnet-install.ps1"
 
 $repoInstallDir  = [System.IO.Path]::GetFullPath("$PSScriptRoot\..\.dotnet")
-$versionPropsFilePropertyGroup = ([xml](Get-Content -Raw "$PSScriptRoot\..\..\..\eng\versions.props")).Project.PropertyGroup[0]
+$versionPropsFilePropertyGroup = ([xml](Get-Content -Raw "$PSScriptRoot\..\..\..\eng\versions.props")).Project.PropertyGroup
 $dotNetSdkVersion = $versionPropsFilePropertyGroup.CsWinRTDependencyDotNetCoreSdkPackageVersion
 $dotNetSdkVersionLkg =  if (-not $skipLKG) { $versionPropsFilePropertyGroup.CsWinRTDependencyDotNetCoreSdkLkgPackageVersion }
 
