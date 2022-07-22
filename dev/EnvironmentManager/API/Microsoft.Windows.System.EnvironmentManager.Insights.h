ca1597192d76a6478053a5fd90e12e241d680713
Darren Hoehna <dahoehna@microsoft.com>
2022-07-22 09:59:11 -0700

    FirstCommit

--- dev/EnvironmentManager/API/Microsoft.Windows.System.EnvironmentManager.Insights.h
+++ dev/EnvironmentManager/API/Microsoft.Windows.System.EnvironmentManager.Insights.h
@@ -2,16 +2,22 @@
 #include "Microsoft.Windows.System.EnvironmentManager.h"
 #include <WindowsAppRuntimeInsights.h>
 
+DECLARE_TRACELOGGING_CLASS(
+EnvironmentManagerTelemetryProvider,
+    "Microsoft.Windows.System.EnvironmentManager.Insights",
+    (0xfbce41fa, 0xf7bc, 0x538e, 0x11, 0xf2, 0xab, 0xe1, 0xb9, 0xd2, 0xab, 0x23));
+
 class EnvironmentManagerInsights : public wil::TraceLoggingProvider
-
 {
-    IMPLEMENT_TRACELOGGING_CLASS(
-        EnvironmentManagerInsights,
-        "Microsoft.Windows.System.EnvironmentManager.Insights",
-        (0xfbce41fa, 0xf7bc, 0x538e, 0x11, 0xf2, 0xab, 0xe1, 0xb9, 0xd2, 0xab, 0x23));
+    IMPLEMENT_TELEMETRY_CLASS(EnvironmentManagerInsights, EnvironmentManagerTelemetryProvider);
 
-    DEFINE_COMPLIANT_MEASURES_EVENT_STRING(LogMessage,
-        MICROSOFT_KEYWORD_MEASURES, stringvalue);
+    DEFINE_EVENT_METHOD(LogMessage)(std::wstring message) {
+        TraceLoggingClassWriteMeasure(
+            "CreateChannelAsync",
+            TelemetryPrivacyDataTag(PDT_ProductAndServicePerformance),
+            _GENERIC_PARTB_FIELDS_ENABLED,
+            TraceLoggingWideString(message.c_str(), "Message"));
+    }
 
     static void LogWithScopeAndMessage(winrt::Microsoft::Windows::System::implementation::EnvironmentManager::Scope const& scope,
         PCWSTR message)
