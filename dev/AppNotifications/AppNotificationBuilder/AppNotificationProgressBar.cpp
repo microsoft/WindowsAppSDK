24e7669ca91480cb26f96af034e6ea86c6e409f7
Eric Langlois <erlangl@microsoft.com>
2022-07-28 10:15:22 -0700

    Fixing build break

--- dev/AppNotifications/AppNotificationBuilder/AppNotificationProgressBar.cpp
+++ dev/AppNotifications/AppNotificationBuilder/AppNotificationProgressBar.cpp
@@ -1,4 +1,7 @@
-﻿#include "pch.h"
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#include "pch.h"
 #include "AppNotificationProgressBar.h"
 #include "Microsoft.Windows.AppNotifications.Builder.AppNotificationProgressBar.g.cpp"
 
--- dev/AppNotifications/AppNotificationBuilder/AppNotificationProgressBar.h
+++ dev/AppNotifications/AppNotificationBuilder/AppNotificationProgressBar.h
@@ -1,4 +1,7 @@
-﻿#pragma once
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#pragma once
 #include "Microsoft.Windows.AppNotifications.Builder.AppNotificationProgressBar.g.h"
 
 namespace winrt::Microsoft::Windows::AppNotifications::Builder::implementation
