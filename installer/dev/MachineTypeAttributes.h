846223563f97c6c552a8a85dfee7a616d195655a
Santosh Chintalapati <sachinta@ntdev.microsoft.com>
2022-08-11 14:04:04 -0700

    Fix aggregate pipeline build error by reverting to using v142 platform toolset.

--- installer/dev/MachineTypeAttributes.h
+++ installer/dev/MachineTypeAttributes.h
@@ -9,7 +9,7 @@ namespace MachineTypeAttributes
     // On systems running Windows 11, this inline function returns if given architecture is supported on the system in user mode.
     // On systems running down level Windows versions (ex: Windows 10), this inline function will return false, always.
     // NOTE: If in case, the GetMachineTypeAttributes API that is currently only available on Windows 11 get serviced to down level Windows versions as well,
-    // then on systems running down level Windows versions (ex: Windows 10), this inline function will function same as on Windows 11.
+    // then on systems running down level Windows versions (ex: Windows 10), this inline function will function the same as on Windows 11.
     inline bool IsWindows11_IsArchitectureSupportedInUserMode(USHORT architecture)
     {
         // GetMachineTypeAttributes() added to kernelbase.dll in NTDDI_WIN10_CO (aka Windows 11 21H2, the first market release version of Windows 11).
@@ -25,9 +25,7 @@ namespace MachineTypeAttributes
             if (!getMachineTypeAttributes)
             {
                 // For now, GetMachineTypeAttributes API is available only on Windows 11 (i.e. builds 22000+).
-                // TODO: If GetMachineTypeAttributes API gets serviced downlevel to Windows versions (ex: Windows 10), then we have to THROW_LAST_ERROR
-                // if GetProcAddressByFunctionDeclaration returned NULL, instead of logging last error and returning false here.
-                LOG_LAST_ERROR();
+                THROW_LAST_ERROR();
                 return false;
             }
             else
