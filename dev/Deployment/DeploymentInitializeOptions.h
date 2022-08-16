5372c81a509750989827dd41c684b95910e77f32
Eric Johnson <ericjohnson327@gmail.com>
2022-08-16 13:24:48 -0700

    Find/replace of copyright header strings with CELA guidance (#2857)
    * Find/replace of copyright header strings with CELA guidance
    
    * Update the copyright script
    
    Co-authored-by: Eric Johnson <ejohn@microsoft.com>

--- dev/Deployment/DeploymentInitializeOptions.h
+++ dev/Deployment/DeploymentInitializeOptions.h
@@ -1,8 +1,8 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information.
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
 
 // Copyright(c) Microsoft Corporation.All rights reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information.
+// Licensed under the MIT License.
 
 #pragma once
 
