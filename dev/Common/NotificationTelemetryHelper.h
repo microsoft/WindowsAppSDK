caae0e410b17452e94fec5324f62b4e3489c4e3c
Eric Langlois <erlangl@microsoft.com>
2022-08-16 06:38:29 -0700

    Copyright notice

--- dev/Common/NotificationTelemetryHelper.h
+++ dev/Common/NotificationTelemetryHelper.h
@@ -1,4 +1,7 @@
-﻿#pragma once
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE in the project root for license information.
+
+#pragma once
 
 class NotificationTelemetryHelper
 {
