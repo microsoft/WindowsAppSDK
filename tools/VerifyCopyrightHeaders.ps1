cfb5adc88113b33712c35b945123a5468d85fe6d
Howard Kapustein <howardk@microsoft.com>
2022-08-16 19:26:37 -0700

    Added VerifyCopyrightHeaders.cmd and tweaked error message about it

--- tools/VerifyCopyrightHeaders.ps1
+++ tools/VerifyCopyrightHeaders.ps1
@@ -39,7 +39,7 @@ if ($errorCount -gt 0) {
         Exit 0
     } else {
         Write-Host "Copyright missing from $errorCount files." -ForegroundColor red
-        Write-Host "Run 'VerifyCopyrightHeaders.ps1 -Fix' locally to update files."
+        Write-Host "Run 'tools\\VerifyCopyrightHeaders.cmd -Fix' locally to update files."
         Exit 1
     }
 }
