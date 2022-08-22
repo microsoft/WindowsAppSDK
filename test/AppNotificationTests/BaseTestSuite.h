b36ff7e994c792bad4995073e65a0c8e498a45ec
Paul Purifoy <33183370+pmpurifoy@users.noreply.github.com>
2022-08-22 10:14:19 -0700

    Refactoring app notification tests (#2444)
    * Base for unit tests setup
    
    * Working on refactoring
    
    * Fix testing bug
    
    * Need to fix for unpackaged
    
    * Include unpackaged support
    
    * Add icon/res file
    
    * Working on refactoring still
    
    * Added polling and payload checks
    
    * Update AppNotificationHelpers.h
    
    * Addressing some nits
    
    * Update AppNotifications.Test.h
    
    * Update AppNotifications.Test.h
    
    * Update AppNotifications.Test.h
    
    * Update AppNotifications.Test.h
    
    * Update AppNotifications.Test.h
    
    * Address sleep issues
    
    * Add copyright
    
    * Update resource.h
    
    * Fix testing issues
    
    * Update cppwinrt version

---
+++ test/AppNotificationTests/BaseTestSuite.h
@@ -0,0 +1,61 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#include "pch.h"
+
+class BaseTestSuite
+{
+    public:
+        // Unit test environment functions
+        void ClassSetup();
+        void ClassCleanup();
+        void MethodSetup();
+        void MethodCleanup();
+
+        // Base unit tests
+        void VerifyRegisterActivatorandUnregisterActivator();
+        void VerifyFailedMultipleRegister();
+        void VerifyUnregisterAll();
+        void VerifyUnregisterTwice();
+        void VerifyToastSettingEnabled();
+        void VerifyToastPayload();
+        void VerifyToastTag();
+        void VerifyToastGroup();
+        void VerifyToastProgressDataFromToast();
+        void VerifyToastExpirationTime();
+        void VerifyToastPriority();
+        void VerifyToastSuppressDisplay();
+        void VerifyToastExpiresOnReboot();
+        void VerifyShowToast();
+        void VerifyUpdateToastProgressDataUsingValidTagAndValidGroup();
+        void VerifyUpdateToastProgressDataUsingValidTagAndEmptyGroup();
+        void VerifyUpdateToastProgressDataUsingEmptyTagAndValidGroup();
+        void VerifyUpdateToastProgressDataUsingEmptyTagAndEmptyGroup();
+        void VerifyFailedUpdateNotificationDataWithNonExistentTagAndGroup();
+        void VerifyFailedUpdateNotificationDataWithoutPostToast();
+        void VerifyGetAllAsyncWithZeroActiveToast();
+        void VerifyGetAllAsyncWithOneActiveToast();
+        void VerifyGetAllAsyncWithMultipleActiveToasts();
+        void VerifyGetAllAsyncIgnoresUpdatesToProgressData();
+        void VerifyRemoveWithIdentifierAsyncUsingZeroedToastIdentifier();
+        void VerifyRemoveWithIdentifierAsyncUsingNonActiveToastIdentifierDoesNotThrow();
+        void VerifyRemoveWithIdentifierAsyncUsingActiveToastIdentifier();
+        void VerifyRemoveWithTagAsyncUsingEmptyTagThrows();
+        void VerifyRemoveWithTagAsyncUsingNonExistentTagDoesNotThrow();
+        void VerifyRemoveWithTagAsync();
+        void VerifyRemoveWithTagGroupAsyncUsingEmptyTagThrows();
+        void VerifyRemoveWithTagGroupAsyncUsingEmptyGroupThrows();
+        void VerifyRemoveWithTagGroupAsync();
+        void VerifyRemoveGroupAsyncUsingEmptyGroupThrows();
+        void VerifyRemoveGroupAsyncUsingNonExistentGroupDoesNotThrow();
+        void VerifyRemoveGroupAsync();
+        void VerifyRemoveAllAsyncWithNoActiveToastDoesNotThrow();
+        void VerifyRemoveAllAsync();
+        void VerifyToastProgressDataSequence0Fail();
+        void VerifyIconPathExists();
+        void VerifyExplicitAppId();
+    private:
+        void RegisterWithAppNotificationManager();
+        void UnregisterAllWithAppNotificationManager();
+        bool m_unregisteredFully{ true };
+};
