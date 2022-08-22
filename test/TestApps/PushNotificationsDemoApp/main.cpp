447ba5d1346986155403ec9280c8bf2fbb4ba76f
Eric Langlois <erlangl@microsoft.com>
2022-08-22 07:20:47 -0700

    Updating copyright texts

--- test/TestApps/PushNotificationsDemoApp/main.cpp
+++ test/TestApps/PushNotificationsDemoApp/main.cpp
@@ -1,5 +1,6 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information.
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
 #include "pch.h"
 #include <wil/win32_helpers.h>
 #include <iostream>
--- test/TestApps/PushNotificationsTestApp/pch.cpp
+++ test/TestApps/PushNotificationsTestApp/pch.cpp
@@ -1,3 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information.
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
 #include "pch.h"
