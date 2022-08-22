92bbae747cda86333344fb86024ce6ec69e19b7f
Eric Langlois <erlangl@microsoft.com>
2022-08-22 07:11:09 -0700

    Updating copyright texts

--- dev/AppNotifications/AppNotificationManager.cpp
+++ dev/AppNotifications/AppNotificationManager.cpp
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information.
-=======
-// Copyright (c) Microsoft Corporation.
+﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
->>>>>>> develop
 
 #include "pch.h"
 #include "AppNotificationManager.h"
