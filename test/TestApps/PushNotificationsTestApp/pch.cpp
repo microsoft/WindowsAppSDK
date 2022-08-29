45e73cb26c0f17fbf991940f76eeff5d11b77e81
Eric Langlois <erlangl@microsoft.com>
2022-08-29 09:20:02 -0700

    Code cleanup

--- test/TestApps/PushNotificationsTestApp/pch.cpp
+++ test/TestApps/PushNotificationsTestApp/pch.cpp
@@ -1,3 +1,3 @@
-// Copyright (c) Microsoft Corporation and Contributors. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation and Contributors. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 #include "pch.h"
