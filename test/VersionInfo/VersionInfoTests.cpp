0616493ad5c4170dba48f52746546f7f7de749fe
Howard Kapustein <howardk@microsoft.com>
2022-09-07 12:47:19 -0700

    Runtime query'able version information (#2889)
    * VerisonInfo API
    
    * Compiles!
    
    * Slight simplification
    
    * Added C# projection. Added new bits to the Transport package
    
    * Tweak projection project name to avoid obj\... exceeding MAX_PATH (Still no LFN support? Sigh)
    
    * Added tests. Fixed package manifest definitions
    
    * Fix typo
    
    * Incorporated feedback
    
    * Incorporated API Review feedback
    
    * Drop the M.W.AM.WAR.VersionInfo projection and artifacts per the namespace change to M.W.AM.WAR (which already exists)
    
    * Changed ToString to AsString to avoid C#/WinRT warnings and language conflicts
    
    * Updated new VersionInfo test project to VS2022

---
+++ test/VersionInfo/VersionInfoTests.cpp
@@ -0,0 +1,57 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#include "pch.h"
+
+namespace TB = ::Test::Bootstrap;
+namespace TP = ::Test::Packages;
+
+namespace Test::VersionInfo
+{
+    class VersionInfoTests
+    {
+    public:
+        BEGIN_TEST_CLASS(VersionInfoTests)
+            TEST_CLASS_PROPERTY(L"ThreadingModel", L"MTA")
+            //TEST_CLASS_PROPERTY(L"RunFixtureAs:Class", L"RestrictedUser")
+        END_TEST_CLASS()
+
+        TEST_CLASS_SETUP(ClassSetup)
+        {
+            ::TB::Setup();
+            return true;
+        }
+
+        TEST_CLASS_CLEANUP(ClassCleanup)
+        {
+            ::TB::Cleanup();
+            return true;
+        }
+
+        TEST_METHOD(VersionInfo_Release)
+        {
+            try
+            {
+                auto release{ winrt::Microsoft::Windows::ApplicationModel::WindowsAppRuntime::ReleaseInfo::AsString() };
+                VERIFY_FAIL(L"Success is not expected without Microsoft.WindowsAppRuntime.Insights.Resource.dll");
+            }
+            catch (winrt::hresult_error& e)
+            {
+                VERIFY_ARE_EQUAL(HRESULT_FROM_WIN32(ERROR_MOD_NOT_FOUND), e.code());
+            }
+        }
+
+        TEST_METHOD(VersionInfo_Runtime)
+        {
+            try
+            {
+                auto runtime{ winrt::Microsoft::Windows::ApplicationModel::WindowsAppRuntime::RuntimeInfo::AsString() };
+                VERIFY_FAIL(L"Success is not expected without Microsoft.WindowsAppRuntime.Insights.Resource.dll");
+            }
+            catch (winrt::hresult_error& e)
+            {
+                VERIFY_ARE_EQUAL(HRESULT_FROM_WIN32(ERROR_MOD_NOT_FOUND), e.code());
+            }
+        }
+    };
+}
