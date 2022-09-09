3076f9cb229a697e2cd2f448dbad54f44b9149bb
Kyaw Thant <48363984+kythant@users.noreply.github.com>
2022-09-08 17:17:27 -0700

    Manual FI Main to Develop  (#2949)

---
+++ dev/VersionInfo/VersionInfo.RuntimeInfo.h
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include <Microsoft.Windows.ApplicationModel.WindowsAppRuntime.RuntimeInfo.g.h>
+
+namespace winrt::Microsoft::Windows::ApplicationModel::WindowsAppRuntime::implementation
+{
+    struct RuntimeInfo
+    {
+        RuntimeInfo() = default;
+
+        static winrt::Windows::ApplicationModel::PackageVersion Version();
+        static hstring AsString();
+
+    private:
+        static const ::Microsoft::WindowsAppSDK::VersionInfo& GetVersionInfo();
+    };
+}
+namespace winrt::Microsoft::Windows::ApplicationModel::WindowsAppRuntime::factory_implementation
+{
+    struct RuntimeInfo : RuntimeInfoT<RuntimeInfo, implementation::RuntimeInfo>
+    {
+    };
+}
