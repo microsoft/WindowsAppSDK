5e21dae131ac206529e5c2b47468764241641229
Daniel Ayala <14967941+danielayala94@users.noreply.github.com>
2022-09-23 15:37:53 -0700

    IDL and stubs up and compiling!

---
+++ dev/AppNotifications/Badges/BadgeNotificationManager.cpp
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+#include <pch.h>
+#include "BadgeNotificationManager.h"
+
+#include <Microsoft.Windows.AppNotifications.Badges.BadgeNotificationManager.g.cpp>
+
+namespace winrt::Microsoft::Windows::AppNotifications::Badges::implementation
+{
+    void BadgeNotificationManager::PostValue(uint32_t) {};
+    void BadgeNotificationManager::PostGlyph(winrt::Microsoft::Windows::AppNotifications::Badges::BadgeNotificationGlyph) {};
+    void BadgeNotificationManager::Clear() {};
+}
---
+++ dev/AppNotifications/Badges/BadgeNotificationManager.h
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+#pragma once
+
+#include <Microsoft.Windows.AppNotifications.Badges.BadgeNotificationManager.g.h>
+
+namespace winrt::Microsoft::Windows::AppNotifications::Badges::implementation
+{
+    struct BadgeNotificationManager
+    {
+        struct BadgeNotificationManager() = default;
+
+        static void PostValue(uint32_t value);
+        static void PostGlyph(winrt::Microsoft::Windows::AppNotifications::Badges::BadgeNotificationGlyph glyph);
+        static void Clear();
+    };
+}
+
+namespace winrt::Microsoft::Windows::AppNotifications::Badges::factory_implementation
+{
+    struct BadgeNotificationManager : BadgeNotificationManagerT<BadgeNotificationManager,
+        implementation::BadgeNotificationManager>
+    {
+    };
+}
