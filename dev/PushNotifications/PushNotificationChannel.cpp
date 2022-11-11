7f38193afb5bbecfb744e61f5b06d96a1e720232
Daniel Ayala <14967941+danielayala94@users.noreply.github.com>
2022-11-10 16:23:33 -0800

    Changes specific to Push Notification activities

--- dev/PushNotifications/PushNotificationChannel.cpp
+++ dev/PushNotifications/PushNotificationChannel.cpp
@@ -41,13 +41,12 @@ namespace winrt::Microsoft::Windows::PushNotifications::implementation
     {
         HRESULT hr{ S_OK };
 
-        auto logTelemetry{ wil::scope_exit([&]() {
-            PushNotificationTelemetry::LogCloseChannel(hr);
-        }) };
+        auto logTelemetry{ PushNotificationTelemetry::CloseChannel::Start(g_telemetryHelper) };
 
         try
         {
             THROW_IF_FAILED(PushNotifications_CloseChannel(m_channelInfo.appId.c_str(), m_channelInfo.channelId.c_str()));
+            logTelemetry.Stop();
         }
 
         catch (...)
@@ -55,6 +54,8 @@ namespace winrt::Microsoft::Windows::PushNotifications::implementation
             auto channelCloseException { hresult_error(to_hresult()) };
 
             hr = channelCloseException.code();
+
+            logTelemetry.StopWithResult(hr);
 
             if (channelCloseException.code() != HRESULT_FROM_WIN32(ERROR_NOT_FOUND))
             {
