6d9d12928ed86d27aa5dfcef83ab682579e0ec70
Kyaw Thant <kythant@microsoft.com>
2022-11-16 11:57:15 -0800

    update strip symbols

--- build/StripSymbols.ps1
+++ build/StripSymbols.ps1
@@ -1,9 +1,19 @@
-﻿param(
+﻿# Use pdbcopy to strip private information from all
+# pdbs in the $InputDirectory and place the new pdbs
+# in the $OutputDirectory
+
+param(
     [parameter(mandatory)][string] $InputDirectory,
-    [parameter(mandatory)][string] $OutputDirectory
+    [parameter(mandatory)][string] $OutputDirectory,
+    [string] $WindowsBuildNumber = [string] "19041",
+    [string] $Verbosity = "quiet",
+    [string] $LogsDirectory
 )
 
 $ErrorActionPreference = "Stop"
+
+Import-Module -Name $PSScriptRoot\WindowsSdkInstallerFunctions.psm1 -DisableNameChecking -Force
+
 function Get-Is64Bit
 {
     return $env:PROCESSOR_ARCHITECTURE -eq "AMD64"
@@ -25,7 +35,63 @@ function Get-DebuggingToolsRoot
     }
 }
 
-function Remove-PrivateSymbolInformation(
+function Install-DebuggingTools(
+    [parameter(mandatory)][ValidateNotNullOrEmpty()] [string] $buildNumber)
+{
+    $installerPath = Ensure-WindowsSDKInstaller $buildNumber
+
+    try
+    {
+        Write-Host -NoNewLine "Installing Debugging Tools for Windows..."
+
+        $windowsSDKOptions = @("OptionId.WindowsDesktopDebuggers")
+
+        $options = "/features $($windowsSDKOptions -join " ")"
+        if ($Verbosity -eq "quiet") { $options += " /q" }
+        if ($LogsDirectory) { $options += " /log $LogsDirectory\DebuggingToolsInstall.log" }
+
+        Start-Process -Wait $installerPath $options
+
+        Write-Host -ForegroundColor Green "OK"
+    }
+    catch
+    {
+        Write-Host -ForegroundColor Red "FAILED"
+        Write-Host -ForegroundColor Red "Error: $PSItem"
+        throw
+    }
+
+    return $installerPath
+}
+
+function Uninstall-DebuggingTools(
+    [parameter(mandatory)][ValidateNotNullOrEmpty()] [string] $installerPath)
+{
+    try
+    {
+        Write-Host -NoNewLine "Uninstalling Debugging Tools for Windows..."
+
+        $windowsSDKOptions = @("OptionId.WindowsDesktopDebuggers")
+
+        $options = "/uninstall /features $($windowsSDKOptions -join " ")"
+        if ($Verbosity -eq "quiet") { $options += " /q" }
+        if ($LogsDirectory) { $options += " /log $LogsDirectory\DebuggingToolsUninstall.log" }
+
+        Start-Process -Wait $installerPath $options
+
+        Write-Host -ForegroundColor Green "OK"
+    }
+    catch
+    {
+        Write-Host -ForegroundColor Red "FAILED"
+        Write-Host -ForegroundColor Red "Error: $PSItem"
+        throw
+    }
+
+    Cleanup-WindowsSDKInstaller $installerPath
+}
+
+function Strip-PrivateInformation(
     [parameter(mandatory)][ValidateNotNullOrEmpty()] [string] $inputPdbPath,
     [parameter(mandatory)][ValidateNotNullOrEmpty()] [string] $outputPdbPath)
 {
@@ -43,47 +109,48 @@ function Remove-PrivateSymbolInformation(
 
     Write-Host "$inputPdbPath => $outputPdbPath"
     Start-Process -Wait -NoNewWindow $pdbCopy $arguments
-} 
-
-Write-Host -NoNewline "Checking for installed Debugging Tools for Windows..."
-
-$debuggingToolsRoot = Get-DebuggingToolsRoot
-if ($debuggingToolsRoot)
-{
-    Write-Host -ForegroundColor Green "FOUND"
 }
-else
-{
-    Write-Host -ForegroundColor Yellow "MISSING"
-}
-Write-Host "OuTPUTDIR"
-Write-Host $OutputDirectory
-if (Test-Path $OutputDirectory)
-{
-    Remove-Item -Recurse $OutputDirectory
-}
-New-Item -ItemType Directory $OutputDirectory | Out-Null
 
-Write-Host "Stripping private information from symbols..."
-
-foreach ($inputPdb in (Get-ChildItem -Recurse -Filter "*.pdb" $InputDirectory))
+$installerPath = $null
+try
 {
-    Write-Host "input dir name " $inputPdb.DirectoryName
-    $outputPdbDir = $inputPdb.DirectoryName -replace [regex]::Escape($InputDirectory), $OutputDirectory
-    Write-Host "output dir name" $outputPdbDir
-    
-    if (!(Test-Path $outputPdbDir))
+    Write-Host -NoNewLine "Checking for installed Debugging Tools for Windows..."
+
+    $debuggingToolsRoot = Get-DebuggingToolsRoot
+    if (!$debuggingToolsRoot)
     {
-        New-Item -ItemType Directory $outputPdbDir | Out-Null
+        Write-Host -ForegroundColor Yellow "MISSING"
+
+        $installerPath = Install-DebuggingTools $WindowsBuildNumber
+        $debuggingToolsRoot = Get-DebuggingToolsRoot
+    }
+    else
+    {
+        Write-Host -ForegroundColor Green "FOUND"
     }
 
-    $inputPdbPath = $inputPdb.FullName
-    $outputPdbPath = Join-Path $outputPdbDir $inputPdb.name
+    Write-Host "Stripping private information from symbols..."
 
-    Write-Host $inputPdbPath
-    Write-Host $outputPdbPath
+    if (Test-Path $OutputDirectory)
+    {
+        Remove-Item -Recurse $OutputDirectory
+    }
+    New-Item -ItemType Directory $OutputDirectory | Out-Null
 
-    Remove-PrivateSymbolInformation $inputPdbPath $outputPdbPath
+    foreach ($pdb in (Get-ChildItem -Recurse -Filter "*.pdb" $InputDirectory))
+    {
+        $inputPdbPath = $pdb.FullName
+        $outputPdbPath = Join-Path $OutputDirectory $pdb.Name
+        Strip-PrivateInformation $inputPdbPath $outputPdbPath
+    }
 
-    Write-Host
+    Write-Host -ForegroundColor Green "Finished."
+}
+finally
+{
+    # If we installed the debugging tools, uninstall them as part of cleanup
+    if ($installerPath)
+    {
+        Uninstall-DebuggingTools $installerPath
+    }
 }
