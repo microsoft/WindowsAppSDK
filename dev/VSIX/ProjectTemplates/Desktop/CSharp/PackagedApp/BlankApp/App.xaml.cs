0e5176202e7ef508b761d14ce07ed3b97f969d28
bpulliam@microsoft.com <bpulliam@microsoft.com>
2023-01-10 13:35:29 -0500

    Remove copyright notice and license. Set xaml files to utf-8.

--- dev/VSIX/ProjectTemplates/Desktop/CSharp/PackagedApp/BlankApp/App.xaml.cs
+++ dev/VSIX/ProjectTemplates/Desktop/CSharp/PackagedApp/BlankApp/App.xaml.cs
@@ -1,7 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation and Contributors.
-// Licensed under the MIT License.
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
