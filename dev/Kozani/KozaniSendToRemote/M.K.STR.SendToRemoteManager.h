ad9abc46fdb648ff0e560ee9732503420eef138a
Howard Kapustein <howardk@microsoft.com>
2023-01-15 17:37:38 -0800

    Added missing files I forgot to include in previous commit

---
+++ dev/Kozani/KozaniSendToRemote/M.K.STR.SendToRemoteManager.h
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#pragma once
+
+#include "Microsoft.Kozani.SendToRemote.SendToRemoteManager.g.h"
+
+namespace winrt::Microsoft::Kozani::SendToRemote::implementation
+{
+    struct SendToRemoteManager : SendToRemoteManagerT<SendToRemoteManager>
+    {
+        SendToRemoteManager() = default;
+
+        static winrt::Microsoft::Kozani::SendToRemote::SendToRemoteManager Open();
+        void TODO_ReplaceMeWithRealContent();
+    };
+}
+namespace winrt::Microsoft::Kozani::SendToRemote::factory_implementation
+{
+    struct SendToRemoteManager : SendToRemoteManagerT<SendToRemoteManager, implementation::SendToRemoteManager>
+    {
+    };
+}
