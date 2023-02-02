9f4ccfcf43c1cc50dde2c1dd3eef8181a328d650
rhuang-msft <79358837+rhuang-msft@users.noreply.github.com>
2023-02-02 15:21:48 -0800

    KozaniManager in-proc WinRT APIs and OOP classic COM APIs to activate remote apps (#3383)
    * KozaniManager in-proc WinRT APIs and OOP classic COM APIs to activate remote apps.
    
    Created KozaniManager in-proc WinRT APIs and OOP classic COM APIs to activate remote apps. Added tests to validate the changes.
    
    * Fixed WindowsAppRuntime.sln issue
    
    * Addressed CR feedbacks
    
    * Fixed build issue with x86 and ARM64

--- dev/Kozani/KozaniManagerRuntime/M.K.MR.ManagerRuntimeManager.h
+++ dev/Kozani/KozaniManagerRuntime/M.K.MR.ManagerRuntimeManager.h
@@ -3,16 +3,27 @@
 
 #pragma once
 
-#include "Microsoft.Kozani.ManagerRuntime.ManagerRuntimeManager.g.h"
+#include <KozaniManager_h.h>
+#include "ManagerRuntimeManager.g.h"
 
 namespace winrt::Microsoft::Kozani::ManagerRuntime::implementation
 {
     struct ManagerRuntimeManager : ManagerRuntimeManagerT<ManagerRuntimeManager>
     {
-        ManagerRuntimeManager() = default;
+        ManagerRuntimeManager();
 
-        static winrt::Microsoft::Kozani::ManagerRuntime::ManagerRuntimeManager Open();
-        void TODO_ReplaceMeWithRealContent();
+        static winrt::Microsoft::Kozani::ManagerRuntime::ManagerRuntimeManager Create();
+
+        void ActivateRemoteApplication(
+            Windows::ApplicationModel::Activation::ActivationKind activationKind,
+            winrt::hstring appUserModelId,
+            winrt::hstring connectionRdpFilePath,
+            winrt::hstring additionalSettingsFilePath,
+            Windows::ApplicationModel::Activation::IActivatedEventArgs args,
+            IInspectable statusCallback);
+
+    private:
+        wil::com_ptr<IKozaniManager> m_kozaniManager;
     };
 }
 namespace winrt::Microsoft::Kozani::ManagerRuntime::factory_implementation
--- test/Kozani/KozaniManagerRuntimeTests/KozaniManagerRuntimeTests.cpp
+++ test/Kozani/KozaniManagerRuntimeTests/KozaniManagerRuntimeTests.cpp
@@ -2,12 +2,70 @@
 // Licensed under the MIT License.
 
 #include "pch.h"
+#include <KozaniManager_h.h>
+#include "winrt/Microsoft.Kozani.ManagerRuntime.h"
 
 namespace TB = ::Test::Bootstrap;
 namespace TP = ::Test::Packages;
 
+namespace KozaniManagerTestPackage
+{
+    constexpr PCWSTR c_PackageDirName = L"KozaniManager";
+    constexpr PCWSTR c_PackageFullName = L"KozaniManager_1.0.0.0_neutral__8wekyb3d8bbwe";
+}
+
 namespace Test::KozaniManagerRuntimeTests
 {
+    struct MyKozaniStatusCallback : winrt::implements<MyKozaniStatusCallback, IKozaniStatusCallback, winrt::Windows::Foundation::IInspectable>
+    {
+#pragma region IKozaniStatusCallback_methods
+        STDMETHODIMP OnActivated(DWORD pid)
+        {
+            WEX::Logging::Log::Comment(WEX::Common::String().Format(L"IKozaniStausCallback::OnActivated is called. pid = %u", pid));
+            m_isActivated = true;
+            return S_OK;
+        }
+
+        STDMETHODIMP OnActivationFailed(
+            DWORD errorCode,
+            PCWSTR errorMessage)
+        {
+            WEX::Logging::Log::Comment(
+                WEX::Common::String().Format(L"IKozaniStausCallback::OnActivationFailed is called. errorCode = 0x%x, errorMessage: %s",
+                    errorCode, errorMessage));
+            m_isActivationFailed = true;
+            return S_OK;
+        }
+
+        STDMETHODIMP OnClosed()
+        {
+            WEX::Logging::Log::Comment(L"IKozaniStausCallback::OnClosed is called.");
+            m_isClosed = true;
+            return S_OK;
+        }
+#pragma endregion IKozaniStatusCallback_methods
+
+        bool IsActivated()
+        {
+            return m_isActivated;
+        }
+
+        bool IsActivationFailed()
+        {
+            return m_isActivationFailed;
+        }
+
+        bool IsClosed()
+        {
+            return m_isClosed;
+        }
+
+    private:
+        bool m_isActivated{};
+        bool m_isActivationFailed{};
+        bool m_isClosed{};
+    };
+
     class KozaniManagerRuntimeTests
     {
     public:
@@ -18,18 +76,28 @@ namespace Test::KozaniManagerRuntimeTests
         TEST_CLASS_SETUP(ClassSetup)
         {
             ::TB::Setup();
+            ::TP::AddPackage(KozaniManagerTestPackage::c_PackageDirName, KozaniManagerTestPackage::c_PackageFullName);
             return true;
         }
 
         TEST_CLASS_CLEANUP(ClassCleanup)
         {
+            ::TP::RemovePackageIfNecessary(KozaniManagerTestPackage::c_PackageFullName);
             ::TB::Cleanup();
             return true;
         }
 
-        // TODO Replace this with your own tests
-        TEST_METHOD(JustDoIt)
+        TEST_METHOD(ActivateRemoteApplication_Launch)
         {
+            auto runtimeManager{ winrt::Microsoft::Kozani::ManagerRuntime::ManagerRuntimeManager::Create() };
+            auto statusCallback{ winrt::make_self<MyKozaniStatusCallback>() };
+
+            runtimeManager.ActivateRemoteApplication(winrt::Windows::ApplicationModel::Activation::ActivationKind::Launch,
+                L"Microsoft.WindowsCalculator_8wekyb3d8bbwe!App", L"c:\\data\\connection.rdp", L"c:\\data\\additionalSettings.txt",
+                nullptr,    // IActivatedEventArgs
+                statusCallback.as<winrt::Windows::Foundation::IInspectable>());
+
+            VERIFY_IS_TRUE(statusCallback->IsActivated(), L"IKozaniStausCallback::OnActivated() should have been called.");
         }
     };
 }
