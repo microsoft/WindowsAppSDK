4769872260436e3e32dc28fde8c0de155435936a
Paul Purifoy <33183370+pmpurifoy@users.noreply.github.com>
2023-02-06 17:32:33 -0800

    Enable installers for arm64

--- build/scripts/DownloadDotNetRuntimeInstaller.ps1
+++ build/scripts/DownloadDotNetRuntimeInstaller.ps1
@@ -18,6 +18,7 @@ if(-not(Test-Path $OutputDirectory))
 # Find direct download links at https://dotnet.microsoft.com/download/dotnet/5.0
 $downloadurlx86 = "https://download.visualstudio.microsoft.com/download/pr/c089205d-4f58-4f8d-ad84-c92eaf2f3411/5cd3f9b3bd089c09df14dbbfb64124a4/windowsdesktop-runtime-5.0.5-win-x86.exe"
 $downloadurlx64 = "https://download.visualstudio.microsoft.com/download/pr/c1ef0b3f-9663-4fc5-85eb-4a9cadacdb87/52b890f91e6bd4350d29d2482038df1c/windowsdesktop-runtime-5.0.5-win-x64.exe"
+$downloadurlArm64 = "https://download.visualstudio.microsoft.com/download/pr/1201ea91-3344-4745-9143-ad4f7eb0a88d/f04108de4f95817aa1b832061a467be0/dotnet-runtime-5.0.5-win-arm64.exe"
 
 if($Platform -eq "x86")
 {
@@ -26,6 +27,10 @@ if($Platform -eq "x86")
 elseif($Platform -eq "x64")
 {
     $downloadurl = $downloadurlx64
+}
+elseif($Platform -eq "arm64")
+{
+    $downloadurl = $downloadurlArm64
 }
 else
 {
--- build/scripts/DownloadVCRedistInstaller.ps1
+++ build/scripts/DownloadVCRedistInstaller.ps1
@@ -15,7 +15,7 @@ if(-not(Test-Path $OutputDirectory))
     $null = New-Item -ItemType Directory -Path $OutputDirectory
 }
 
-# Find direct download links at https://dotnet.microsoft.com/download/dotnet/5.0
+# Find direct download links at https://learn.microsoft.com/en-US/cpp/windows/latest-supported-vc-redist?view=msvc-170
 $downloadurl = "https://aka.ms/vs/17/release/vc_redist.$Platform.exe"
 
 if(-not(Test-Path $outputPath))
