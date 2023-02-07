b4c3bb2cde3faf3dff8069fbec9a1187fa1d67b6
rhuang-msft <79358837+rhuang-msft@users.noreply.github.com>
2023-02-07 14:52:30 -0800

    Initial DVC client impl

---
+++ dev/Kozani/Common/Logging.cpp
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+#include <pch.h>
+#include "Logging.h"
+
+void LogDebugMessage(PCSTR formatString, ...)
+{
+    if (IsDebuggerPresent())
+    {
+        char debugMsg[1000] = {};
+
+        va_list args;
+        va_start(args, formatString);
+
+        HRESULT hr = StringCchVPrintfA(
+            debugMsg,
+            ARRAYSIZE(debugMsg),
+            formatString,
+            args);
+        if (FAILED(hr) && (hr != STRSAFE_E_INSUFFICIENT_BUFFER))
+        {
+            OutputDebugStringA("Cannot construct debug message. StringCchVPrintfA() failed. Input format string: ");
+            OutputDebugStringA(formatString);
+        }
+        else
+        {
+            OutputDebugStringA(debugMsg);
+        }
+    }
+}
