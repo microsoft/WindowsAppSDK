e5e28dd9112bae8c71c53b76ceccd8ca0d6c1101
rhuang-msft <79358837+rhuang-msft@users.noreply.github.com>
2023-05-03 16:53:21 -0700

    not sure the state of things (#3612)
    Co-authored-by: William Cheng <wcheng@ntdev.microsoft.com>

---
+++ test/Kozani/KozaniHostRuntimeTestPackage/pch.h
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include "targetver.h"
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <windows.h>
