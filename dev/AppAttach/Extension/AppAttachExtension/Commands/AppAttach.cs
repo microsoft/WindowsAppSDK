0488c6619c5854b20865fc72191cab21c601f17e
Abhishek <abgho@microsoft.com>
2023-06-07 17:04:42 +0530

    1. Created a new visual studio extension. 2. Attached the extension to the project context menu as command. 3. Extension enabled for WinUI3 project. 4. Extension only enabled after the solution build is over.

---
+++ dev/AppAttach/Extension/AppAttachExtension/Commands/AppAttach.cs
@@ -0,0 +1,90 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell.Interop;
+using static Microsoft.VisualStudio.VSConstants;
+using System.Windows;
+using System.Linq;
+using Microsoft.VisualStudio.PlatformUI;
+using System.IO;
+using Community.VisualStudio.Toolkit;
+using System.ComponentModel.Design;
+using System.Management.Instrumentation;
+using System.Windows.Input;
+using System.Xml.Linq;
+using System.Data.Common;
+using System;
+
+namespace AppAttachExtension
+{
+    [Command(PackageIds.AppAttach)]
+    internal sealed class AppAttach
+    {
+        private readonly AsyncPackage package;
+        public const int CommandId = 0x0100;
+        public const string commandGuid = "ac8bcc47-f5f3-4afa-8cc2-f0d697e04bb7";
+
+        public static AppAttach Instance
+        {
+            get;
+            private set;
+        }
+
+        public static readonly Guid CommandSet = new Guid(commandGuid);
+
+        public static void Initialize(AsyncPackage package, IMenuCommandService commandService)
+        {
+            Instance = new AppAttach(package, commandService);
+        }
+
+        private AppAttach(AsyncPackage package, IMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new OleMenuCommand(this.Execute, menuCommandID);
+            menuItem.BeforeQueryStatus += new EventHandler(OnBeforeQueryStatusAsync);
+            commandService.AddCommand(menuItem);
+        }
+
+        private async void OnBeforeQueryStatusAsync(object sender, EventArgs e)
+        {
+            var appAttachCommand = sender as OleMenuCommand;
+            appAttachCommand.Visible = false;
+            appAttachCommand.Enabled = false;
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+            var _dte2 = await ServiceProvider.GetGlobalServiceAsync(typeof(SDTE)) as DTE2;
+            var selectedItem = _dte2.SelectedItems.Item(1);
+            var project = selectedItem.Project;
+            if (project != null)
+            {
+                string projectPath = project.FullName;
+                XDocument projectDoc = XDocument.Load(projectPath);
+                XNamespace ns = projectDoc.Root.Name.Namespace;
+
+                XElement useWinUIElement = projectDoc.Descendants(ns + "UseWinUI").FirstOrDefault();
+                if (appAttachCommand != null)
+                {
+                    appAttachCommand.Visible = useWinUIElement.Value == "true";
+                    appAttachCommand.Enabled = false;
+                    if (_dte2.Solution.IsOpen && _dte2.Solution.SolutionBuild.BuildState != vsBuildState.vsBuildStateInProgress)
+                    {
+                        appAttachCommand.Enabled = true;
+                    }
+                }
+            }
+
+        }
+
+        private void Execute(object sender, EventArgs e)
+        {
+            AppAttachWindow appAttachWindow = new AppAttachWindow();
+        }
+
+        public AsyncPackage Packageval()
+        {
+            return package;
+        }
+
+    }
+}
---
+++ dev/AppAttach/Extension/AppAttachExtension/Windows/AppAttachWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.PlatformUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppAttachExtension
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class AppAttachWindow  : Window
+    {
+        
+
+        public AppAttachWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
