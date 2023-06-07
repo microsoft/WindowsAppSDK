453999e64d4060deda77d4da429e72a9857307a5
Arnab Saha <arnabsaha@microsoft.com>
2023-06-07 11:50:20 +0530

    Added IAttribute implementations and utility classes

---
+++ dev/AppAttach/AppAttachFramework/AppAttachAPI/Response/Implementations/AppAttachOutput.cs
@@ -0,0 +1,28 @@
+﻿using AppAttachAPI.Response.Interfaces;
+
+namespace AppAttachAPI.Response.Implementations
+{
+    public class AppAttachOutput : IAppAttachOutput
+    {
+        private string _message;
+        private bool _error;
+
+        public AppAttachOutput() { }
+
+        public AppAttachOutput(string message, bool isError)
+        {
+            this._message = message;
+            this._error = isError;
+        }
+
+        public string getMessage()
+        {
+            return _message;
+        }
+
+        public bool isError()
+        {
+            return _error;
+        }
+    }
+}
