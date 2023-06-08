f6dc31dece5202f43e03d4fb4cc8e0000b69f714
Abhishek <abgho@microsoft.com>
2023-06-08 15:18:54 +0530

    1. Create publish to azure page 2. Authentication using vs credentials store to azure 3. Auto population of azure resource details 3. Integration to Publish to azure

---
+++ dev/AppAttach/Extension/AppAttachExtension/Models/IdentityViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppAttachExtension.Models
+{
+    public class IdentityViewModel
+    {
+        private string _jwtToken;
+        private string _userEmail;
+        private string _upn;
+        private string _userName;
+        private string _storageAccountConnectionString;
+
+        public IdentityViewModel()
+        {
+            _jwtToken = null;
+            _userEmail = null;
+            _upn = null;
+            _userName = null;
+            _storageAccountConnectionString = null;
+        }
+
+        public string JwtToken { get => _jwtToken; set => _jwtToken = value; }
+        public string UserEmail { get => _userEmail; set => _userEmail = value; }
+        public string Upn { get => _upn; set => _upn = value; }
+        public string UserName { get => _userName; set => _userName = value; }
+        public string StorageAccountConnectionString { get => _storageAccountConnectionString; set => _storageAccountConnectionString = value; }
+    }
+}
