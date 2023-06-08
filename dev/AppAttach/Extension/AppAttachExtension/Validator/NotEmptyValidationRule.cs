3aaf81327b7adfea03a21276f51e1331cfec8c42
Snigdha LNU <snigdhalnu@microsoft.com>
2023-06-08 11:45:55 +0530

    Added validations for the fields such as output location (should not be empty), version. Ensured that the verify button for certificate should be enabled only after certificate is verified. Also, next button will be enable once all fields are verified successfully.

---
+++ dev/AppAttach/Extension/AppAttachExtension/Validator/NotEmptyValidationRule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace AppAttachExtension.Validator
+{
+    public class NotEmptyValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (string.IsNullOrEmpty((string)value))
+            {
+                return new ValidationResult(false, "This field is required.");
+            }
+            return ValidationResult.ValidResult;
+        }
+    }
+}
