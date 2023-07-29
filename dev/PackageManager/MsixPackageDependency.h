1b6877510d48901cdaf52b76c73a5105c21c2edc
Howard Kapustein <howardk@microsoft.com>
2023-07-29 12:20:50 -0700

    Start of Package*Manager

---
+++ dev/PackageManager/MsixPackageDependency.h
@@ -0,0 +1,7 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+#if !defined(MSIXPACKAGEMANAGER_H)
+#define MSIXPACKAGEMANAGER_H
+
+#endif // MSIXPACKAGEMANAGER_H
---
+++ dev/PackageManager/pch.h
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include <Windows.h>
+
+#include <unknwn.h>
+#include <appmodel.h>
+#include <MsixDynamicDependency.h>
+
+#include <filesystem>
+#include <thread>
+#include <mutex>
+
+#include <wil/cppwinrt.h>
+#include <wil/resource.h>
+#include <wil/result_macros.h>
+
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+
+#include <winrt/Windows.ApplicationModel.h>
+//TODO REMOVE #include <winrt/Windows.Data.Json.h>
+#include <winrt/Windows.Management.Core.h>
+#include <winrt/Windows.Management.Deployment.h>
+//TODO REMOVE #include <winrt/Windows.Storage.h>
+#include <winrt/Windows.System.h>
+
+#include <appmodel.identity.h>
+#include <appmodel.package.h>
