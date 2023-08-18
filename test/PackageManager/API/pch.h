cab1589300a6f5bbd238be0e85312c1f490b9558
Howard Kapustein <howardk@microsoft.com>
2023-08-18 16:49:51 -0700

    Added PackageManagerTests skeleton

---
+++ test/PackageManager/API/pch.h
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+#ifndef PCH_H
+#define PCH_H
+
+#include <unknwn.h>
+
+#include <wil/cppwinrt.h>
+
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+
+#include <WexTestClass.h>
+
+#include <WindowsAppRuntime.Test.AppModel.h>
+#include <WindowsAppRuntime.Test.Package.h>
+#include <WindowsAppRuntime.Test.Bootstrap.h>
+
+#endif //PCH_H
