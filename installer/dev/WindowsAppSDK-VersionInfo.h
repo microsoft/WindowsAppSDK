2a3e60ee8a35118d4f568f51fa2695629de43cf7
Michael Crider <mcrider@microsoft.com>
2023-10-02 10:08:31 -0700

    Add copyright in the placeholder file.

--- installer/dev/WindowsAppSDK-VersionInfo.h
+++ installer/dev/WindowsAppSDK-VersionInfo.h
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+//
 // This file is a PLACEHOLDER which will be replaced in the CreateInstaller
 // build pipeline with a file which defines WINDOWSAPPSDK_RELEASE_MAJOR
 // and WINDOWSAPPSDK_RELEASE_MINOR. For the dev inner loop, these will remain
