8cd3e2a7393a2d0d19be21322ba7ffdfffbc0b57
Michael Crider <mcrider@microsoft.com>
2023-10-02 09:52:27 -0700

    Additional changes to bring in the version.

---
+++ installer/dev/WindowsAppSDK-VersionInfo.h
@@ -0,0 +1,4 @@
+// This file is a PLACEHOLDER which will be replaced in the CreateInstaller
+// build pipeline with a file which defines WINDOWSAPPSDK_RELEASE_MAJOR
+// and WINDOWSAPPSDK_RELEASE_MINOR. For the dev inner loop, these will remain
+// undefined and just version the installer as 0.0.
