fbd5d592b9ed25fa596a387fb9968149bc5be001
Howard Kapustein <howardk@microsoft.com>
2023-10-19 11:08:08 -0700

    Added new directory

---
+++ dev/ApplicationData/pch.h
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include <Windows.h>
+
+#include <unknwn.h>
+#include <appmodel.h>
+
+#include <filesystem>
+#include <thread>
+#include <mutex>
+
+#include <wil/cppwinrt.h>
+#include <wil/resource.h>
+#include <wil/result_macros.h>
+
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+
+#include <winrt/Windows.ApplicationModel.h>
+#include <winrt/Windows.Storage.h>
+#include <winrt/Windows.System.h>
