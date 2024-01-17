3195440d89a476f0e6fcca2642787223d63bcde9
alexlamtest <68841560+alexlamtest@users.noreply.github.com>
2024-01-17 09:38:43 -0800

    48525090 - Skipping a test case to temporarily avoid hitting this bug (#4083)
    * Skipping test case: FullLifecycle_FilePathLifetime_Frameworks_WindowsAppRuntime_MathAdd()
    
    * Changed to be more nuanced about disabling behavior. Currently Skipping unless a TAEF parameter is specified (/p:DynamicDependency.23H1=1). We can disable only on 23H1 by default once a recent public API gets to rs_prerelease (so...next month? Uncomment the TODO comment)
    
    * Moved the Skip-23H1 logic to the parent Setup() to cover elevated and non-elevated behavior
    
    ---------
    
    Co-authored-by: Howard Kapustein <howardk@microsoft.com>

--- test/DynamicDependency/Test_Win32/Test_Win32.cpp
+++ test/DynamicDependency/Test_Win32/Test_Win32.cpp
@@ -19,6 +19,12 @@ wil::unique_hmodule Test::DynamicDependency::Test_Win32::m_bootstrapDll;
 
 bool Test::DynamicDependency::Test_Win32::Setup()
 {
+    if (/*TODO ::WindowsVersion::IsWindows11_23H1OrGreater() &&*/ !::Test::TAEF::IsEnabled(L"DynamicDependency.23H1"))
+    {
+        WEX::Logging::Log::Result(WEX::Logging::TestResults::Skipped, L"Skipping tests to avoid https://task.ms/48525090");
+        return true;
+    }
+
     // Remove our packages in case they were previously installed and incompletely removed
     TP::RemovePackage_DynamicDependencyLifetimeManager();
     TP::RemovePackage_DynamicDependencyDataStore();
