a3f1b0be24f0e2141d774f8aa25ba8832d961485
Kyaw Thant <kythant@microsoft.com>
2024-01-25 17:00:23 -0800

    Revert "Revert "DynamicDependencies: Use Win11 APIs if available" #3704 #4083 (#4108)"
    This reverts commit 775edb1e851b18dc98d9759d1ae676a5fb2cc17b.

--- dev/WindowsAppRuntime_BootstrapDLL/dllmain.cpp
+++ dev/WindowsAppRuntime_BootstrapDLL/dllmain.cpp
@@ -3,6 +3,8 @@
 
 #include "pch.h"
 
+#include "MddWin11.h"
+
 // Including this file once per binary will automatically opt WIL error handling macros into calling RoOriginateError when they
 // begin logging a new error.  This greatly improves the debuggability of errors that propagate before a failfast.
 #include <wil/result_originate.h>
@@ -12,10 +14,19 @@ BOOL APIENTRY DllMain(HMODULE hmodule, DWORD  reason, LPVOID reserved)
     switch (reason)
     {
     case DLL_PROCESS_ATTACH:
+    {
         DisableThreadLibraryCalls(hmodule);
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
+        FAIL_FAST_IF_FAILED(MddWin11Initialize());
+        break;
+    }
     case DLL_PROCESS_DETACH:
+    {
+        MddWin11Shutdown();
+        break;
+    }
+    case DLL_THREAD_ATTACH:
+        break;
+    case DLL_THREAD_DETACH:
         break;
     }
 
