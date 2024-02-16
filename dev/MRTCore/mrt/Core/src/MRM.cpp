7e3a9990fda2c367b2c2b4c0649c1fbca90fbd11
Scott Jones <sjones@microsoft.com>
2024-02-16 13:25:39 -0800

    missing space for null terminator on baseDir (#4209) (#4211)
    * missing space for null terminator on baseDir
    
    * PR feedback

--- dev/MRTCore/mrt/Core/src/MRM.cpp
+++ dev/MRTCore/mrt/Core/src/MRM.cpp
@@ -993,7 +993,8 @@ STDAPI MrmGetFilePathFromName(_In_opt_ PCWSTR filename, _Outptr_ PWSTR* filePath
         if (SUCCEEDED(wil::TryGetEnvironmentVariableW(L"MICROSOFT_WINDOWSAPPRUNTIME_BASE_DIRECTORY", baseDir)) && baseDir)
         {
             path.swap(baseDir);
-            RETURN_IF_FAILED(StringCchLengthW(path.get(), STRSAFE_MAX_CCH, &bufferCount));
+            RETURN_IF_FAILED(StringCchLengthW(path.get(), STRSAFE_MAX_CCH, &length));
+            RETURN_IF_FAILED(SizeTAdd(length, 1, &bufferCount));
         }
         filenameToUse = c_defaultPriFilename;
     }
