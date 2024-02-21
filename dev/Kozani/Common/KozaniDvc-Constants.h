4482bd70f0140347ee0c30d4bb8cd54106248ded
Howard Kapustein <howardk@microsoft.com>
2024-02-20 19:23:56 -0800

    Remove Kozani. Change of direction making the code obsolete and if oneday it's needed it's just a get revert away (#4219)

--- dev/Kozani/Common/KozaniDvc-Constants.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation and Contributors.
-// Licensed under the MIT License.
-
-#pragma once
-
-namespace Microsoft::Kozani::DVC::Constants
-{
-    const char ConnectionIdSwitch[] = "-connectionId";
-}
