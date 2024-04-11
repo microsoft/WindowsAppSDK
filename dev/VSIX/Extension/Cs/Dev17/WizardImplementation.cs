c0f531c70fb13afc0c65c109ee267927e50511a0
Shashank Nayak <shasnayak@microsoft.com>
2024-04-11 15:59:03 -0400

    fixing bug

--- dev/VSIX/Extension/Cs/Dev17/WizardImplementation.cs
+++ dev/VSIX/Extension/Cs/Dev17/WizardImplementation.cs
@@ -13,7 +13,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WindowsAppSDK.TemplateUtilities.Cpp
+namespace WindowsAppSDK.TemplateUtilities
 {
     public class NuGetPackageInstaller : IWizard
     {
