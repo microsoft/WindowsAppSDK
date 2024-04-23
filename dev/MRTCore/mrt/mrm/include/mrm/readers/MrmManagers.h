e2b6ae655774f4059f7c6812444596cdef41af2c
Kyaw Thant <kythant@microsoft.com>
2024-04-23 10:54:11 -0700

    More

--- dev/MRTCore/mrt/mrm/include/mrm/readers/MrmManagers.h
+++ dev/MRTCore/mrt/mrm/include/mrm/readers/MrmManagers.h
@@ -664,8 +664,8 @@ public:
 private:
     ManagedSchema(_In_ const ManagedFile* pInitialFile, _In_ const IHierarchicalSchema* pInitialMap);
 
-    UINT64 m_generation;
-    const IHierarchicalSchema* m_pCurrentSchema;
+    UINT64 m_generation = 0;
+    const IHierarchicalSchema* m_pCurrentSchema = nullptr;
     const ManagedFile* m_pCurrentFile = nullptr;
 
     typedef struct _SchemaPerFileInfo
@@ -758,14 +758,14 @@ private:
 
     HRESULT UpdateCurrent() const;
 
-    mutable UINT64 m_generation;
+    mutable UINT64 m_generation = 0;
 
     mutable const ManagedFile* m_pCurrentFile = nullptr;
-    mutable const IResourceMapBase* m_pCurrentMap;
+    mutable const IResourceMapBase* m_pCurrentMap = nullptr;
 
-    const ManagedSchema* m_pSchema;
-    UnifiedDecisionInfo* m_pDecisions;
-    UnifiedResourceView* m_pUnifiedResourceView;
+    const ManagedSchema* m_pSchema = nullptr;
+    UnifiedDecisionInfo* m_pDecisions = nullptr;
+    UnifiedResourceView* m_pUnifiedResourceView = nullptr;
 
     typedef struct _MapPerFileInfo
     {
@@ -775,8 +775,8 @@ private:
         StringResult* pPackageRootPath;
     } MapPerFileInfo;
 
-    const PriFileManager* m_pFileManager;
-    mutable DynamicArray<MapPerFileInfo>* m_pFiles;
+    const PriFileManager* m_pFileManager = nullptr;
+    mutable DynamicArray<MapPerFileInfo>* m_pFiles = nullptr;
 
     mutable StringResult m_packageRootPath;
 };
@@ -906,20 +906,20 @@ public:
     bool TryGetReverseFileMap(_Outptr_opt_ const ReverseFileMap** ppMapOut) const;
 
 protected:
-    CoreProfile* m_pProfile;
+    CoreProfile* m_pProfile = nullptr;
 
-    PriFileManager* m_pFileManager;
-    AtomPoolGroup* m_pAtoms;
-    UnifiedEnvironment* m_pEnvironment;
-    UnifiedDecisionInfo* m_pDecisions;
-    ProviderResolver* m_pResolver;
+    PriFileManager* m_pFileManager = nullptr;
+    AtomPoolGroup* m_pAtoms = nullptr;
+    UnifiedEnvironment* m_pEnvironment = nullptr;
+    UnifiedDecisionInfo* m_pDecisions = nullptr;
+    ProviderResolver* m_pResolver = nullptr;
 
-    DynamicArray<UnifiedViewFileInfo*>* m_pReferencedFiles;
+    DynamicArray<UnifiedViewFileInfo*>* m_pReferencedFiles = nullptr;
 
-    DynamicArray<ManagedSchema*>* m_pSchemas;
-    DynamicArray<ManagedResourceMap*>* m_pMaps;
+    DynamicArray<ManagedSchema*>* m_pSchemas = nullptr;
+    DynamicArray<ManagedResourceMap*>* m_pMaps = nullptr;
 
-    UnifiedViewFileInfo* m_pAppFile;
+    UnifiedViewFileInfo* m_pAppFile = nullptr;
 
     UnifiedResourceView(_In_ CoreProfile* pProfile);
 
@@ -1042,14 +1042,14 @@ protected:
 
     class DecisionInfoCache;
 
-    const UnifiedEnvironment* m_pEnvironment;
-    const IDecisionInfo* m_pDecisions;
-    UINT64 m_generation;
+    const UnifiedEnvironment* m_pEnvironment = nullptr;
+    const IDecisionInfo* m_pDecisions = nullptr;
+    UINT64 m_generation = 0;
 
-    mutable DecisionInfoCache* m_pCache;
-    mutable SRWLOCK m_srwLock;
-    mutable SRWLOCK m_srwQualifierSetLock;
-    mutable SRWLOCK m_srwQualifierLock;
+    mutable DecisionInfoCache* m_pCache = nullptr;
+    mutable SRWLOCK m_srwLock = { nullptr };
+    mutable SRWLOCK m_srwQualifierSetLock = { nullptr };
+    mutable SRWLOCK m_srwQualifierLock = { nullptr };
 };
 
 class ProviderResolver : public ResolverBase
@@ -1092,8 +1092,8 @@ protected:
 
     class PerQualifierPoolInfo;
 
-    mutable IProviderDataSources* m_pDataSources;
-    mutable PerQualifierPoolInfo* m_pQualifiers;
+    mutable IProviderDataSources* m_pDataSources = nullptr;
+    mutable PerQualifierPoolInfo* m_pQualifiers = nullptr;
 };
 
 class PerThreadQualifier;
@@ -1162,13 +1162,13 @@ public:
 protected:
     class PerQualifierPoolInfo;
 
-    const IResolver* m_pParent;
-    mutable PerQualifierPoolInfo* m_pQualifiers;
-    bool m_bHasScoreCache;
-    bool m_bIsDifferentQualifierValueFromParent;
-    PerThreadQualifier* m_pPerThreadQualifier;
-    bool m_bClonedResolver;
-    mutable SRWLOCK m_srwQualifierValuesLock;
+    const IResolver* m_pParent = nullptr;
+    mutable PerQualifierPoolInfo* m_pQualifiers = nullptr;
+    bool m_bHasScoreCache = false;
+    bool m_bIsDifferentQualifierValueFromParent = false;
+    PerThreadQualifier* m_pPerThreadQualifier = nullptr;
+    bool m_bClonedResolver = false;
+    mutable SRWLOCK m_srwQualifierValuesLock = { nullptr };
 
     OverrideResolver(_In_ const IResolver* pParent);
 
