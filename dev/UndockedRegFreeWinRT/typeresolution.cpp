53c5a6f143af9d0efe41991642ab57879540a34a
Kyaw Thant <kythant@microsoft.com>
2024-04-29 23:46:51 -0700

    Fix all C6101

--- dev/UndockedRegFreeWinRT/typeresolution.cpp
+++ dev/UndockedRegFreeWinRT/typeresolution.cpp
@@ -64,6 +64,14 @@ namespace UndockedRegFreeWinRT
         _COM_Outptr_opt_result_maybenull_ IMetaDataImport2** ppMetaDataImport,
         _Out_opt_ mdTypeDef* pmdTypeDef)
     {
+        if (ppMetaDataImport != nullptr)
+        {
+            *ppMetaDataImport = nullptr;
+        }
+        if (pmdTypeDef != nullptr)
+        {
+            *pmdTypeDef = 0;
+        }
         HRESULT hr = S_OK;
         Microsoft::WRL::ComPtr<IMetaDataImport2> spMetaDataImport;
         MetaDataImportersLRUCache* pMetaDataImporterCache = MetaDataImportersLRUCache::GetMetaDataImportersLRUCacheInstance();
@@ -213,8 +221,19 @@ namespace UndockedRegFreeWinRT
         _COM_Outptr_opt_result_maybenull_ IMetaDataImport2** ppMetaDataImport,
         _Out_opt_ mdTypeDef* pmdTypeDef)
     {
+        if (ppMetaDataImport != nullptr)
+        {
+            *ppMetaDataImport = nullptr;
+        }
+        if (pmdTypeDef != nullptr)
+        {
+            *pmdTypeDef = 0;
+        }
+
         wchar_t szCandidateFileName[MAX_PATH + 1]{};
         HRESULT hr{ StringCchCopy(szCandidateFileName, ARRAYSIZE(szCandidateFileName), pszFullName) };
+        hr = WindowsCreateString(L"", 0, phstrMetaDataFilePath);
+
         if (SUCCEEDED(hr))
         {
             // To resolve type SomeNamespace.B.C, first check if SomeNamespace.B.C is a type or
