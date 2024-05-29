dd3d9547d40c31dc1d548aecae0984fd99c41b9a
Saharsh Jaiswal <sahjaiswal@microsoft.com>
2024-05-29 13:21:56 +0530

    Ut's running successfully

--- CameraCaptureUITest/CameraCaptureUITests.cpp
+++ CameraCaptureUITest/CameraCaptureUITests.cpp
@@ -2,22 +2,23 @@
 // Licensed under the MIT License.
 
 #include "pch.h"
-#include <winrt/Microsoft.Windows.Media.Capture.h>
 
 using namespace WEX::Common;
 using namespace WEX::Logging;
 using namespace WEX::TestExecution;
-using namespace winrt;
-using namespace Windows::Foundation;
-using namespace Windows::Storage;
-using namespace Windows::Media::Capture;
-using namespace Microsoft::Windows::Media::Capture;
-using namespace winrt::Windows::ApplicationModel::Activation;
-using namespace winrt::Windows::ApplicationModel::Background;
-using namespace winrt::Windows::Foundation::Collections;
-using namespace winrt::Windows::Management::Deployment;
-using namespace winrt::Windows::System;
+//using namespace winrt;
+//using namespace Windows::Foundation;
+//using namespace Windows::Storage;
+//using namespace Windows::Media::Capture;
+//using namespace Microsoft::Windows::Media::Capture;
+//using namespace winrt::Windows::ApplicationModel::Activation;
+//using namespace winrt::Windows::Foundation::Collections;
+//using namespace winrt::Windows::Management::Deployment;
+//using namespace winrt::Windows::System;
 
+
+namespace TB = ::Test::Bootstrap;
+namespace TP = ::Test::Packages;
 
 
 namespace Test::CameraCaptureUI
@@ -31,37 +32,47 @@ namespace Test::CameraCaptureUI
     {
     public:
         BEGIN_TEST_CLASS(CameraCaptureUITests)
-            TEST_CLASS_PROPERTY(L"IsolationLevel", L"Method")
             TEST_CLASS_PROPERTY(L"ThreadingModel", L"MTA")
-            TEST_METHOD_PROPERTY(L"RunAs", L"ElevatedUser")
+            TEST_METHOD_PROPERTY(L"RunAs", L"UAP")
+            TEST_CLASS_PROPERTY(L"UAP:AppxManifest", L"CameraCaptureUI-AppxManifest.xml")
             END_TEST_CLASS()
 
-            TEST_CLASS_SETUP(ClassInit)
+        TEST_CLASS_SETUP(ClassInit)
         {
+			//::Test::Bootstrap::SetupPackages();
             return true;
         }
 
         TEST_CLASS_CLEANUP(ClassUninit)
         {
+			//::Test::Bootstrap::CleanupPackages();
             return true;
         }
 
+		TEST_METHOD_SETUP(MethodInit)
+		{
+			VERIFY_IS_TRUE(TP::IsPackageRegistered_WindowsAppRuntimeFramework());
+			return true;
+		}
+
+		TEST_METHOD_CLEANUP(MethodUninit)
+		{
+			VERIFY_IS_TRUE(TP::IsPackageRegistered_WindowsAppRuntimeFramework());
+			return true;
+		}
+
         TEST_METHOD(CapturePhoto_ShouldReturnFile)
         {
-            BEGIN_TEST_METHOD_PROPERTIES()
-                TEST_METHOD_PROPERTY(L"RunAs", L"UAP")
-                END_TEST_METHOD_PROPERTIES();
-
             try
             {
                 // Arrange
                 //MockWindowId mockWindowId{ 12345 };
                 //com_ptr<Microsoft::Windows::Media::Capture::CameraCaptureUI> cameraUI = make_self<Microsoft::Windows::Media::Capture::CameraCaptureUI>(reinterpret_cast<WindowId&>(mockWindowId));
                 //auto cameraUI{ Microsoft::Windows::Media::Capture::CameraCaptureUI(reinterpret_cast<WindowId&>(mockWindowId)) };
-                Microsoft::Windows::Media::Capture::CameraCaptureUI cameraUI({12345});
+                winrt::Microsoft::Windows::Media::Capture::CameraCaptureUI cameraUI({12345});
                 
                 // Act
-                auto photoOperation = cameraUI.CaptureFileAsync(CameraCaptureUIMode::Photo);
+                auto photoOperation = cameraUI.CaptureFileAsync(winrt::Windows::Media::Capture::CameraCaptureUIMode::Photo);
                 auto photo = photoOperation.get();
 
                 // Assert
@@ -76,7 +87,7 @@ namespace Test::CameraCaptureUI
                     VERIFY_FAIL(L"Photo capture returned null.");
                 }
             }
-            catch (const hresult_error& ex)
+            catch (const winrt::hresult_error& ex)
             {
                 Log::Error((std::wstring(L"Exception thrown: ") + ex.message().c_str()).c_str());
                 VERIFY_FAIL(L"Exception occurred during photo capture.");
@@ -121,55 +132,51 @@ namespace Test::CameraCaptureUI
         //    }
         //}
 
-        //TEST_METHOD(CapturePhoto_ShouldReturnNullOnCancel)
-        //{
-        //    BEGIN_TEST_METHOD_PROPERTIES()
-        //        TEST_METHOD_PROPERTY(L"RunAs", L"UAP")
-        //        END_TEST_METHOD_PROPERTIES();
+        TEST_METHOD(CapturePhoto_ShouldReturnNullOnCancel)
+        {
+            BEGIN_TEST_METHOD_PROPERTIES()
+                TEST_METHOD_PROPERTY(L"RunAs", L"UAP")
+                END_TEST_METHOD_PROPERTIES();
 
-        //    // Arrange
-        //    MockWindowId mockWindowId{ 12345 };
-        //    CameraCaptureUI cameraUI{ mockWindowId };
+            // Arrange
+            winrt::Microsoft::Windows::Media::Capture::CameraCaptureUI cameraUI({ 12345 });
+            winrt::Windows::Storage::StorageFile photo = nullptr;
+            // Act
+            try
+            {
+                auto photoOperation = cameraUI.CaptureFileAsync(winrt::Windows::Media::Capture::CameraCaptureUIMode::Photo);
+                photo = photoOperation.get();
+            }
+            catch (winrt::hresult_canceled const&)
+            {
+                Log::Comment(L"Photo capture was canceled.");
+            }
 
-        //    // Act
-        //    StorageFile photo = nullptr;
-        //    try
-        //    {
-        //        auto photoOperation = cameraUI.CaptureFileAsync(CameraCaptureUIMode::Photo);
-        //        photo = photoOperation.get();
-        //    }
-        //    catch (hresult_canceled const&)
-        //    {
-        //        Log::Comment(L"Photo capture was canceled.");
-        //    }
+            // Assert
+            VERIFY_IS_NULL(photo);
+        }
 
-        //    // Assert
-        //    VERIFY_IS_NULL(photo);
-        //}
+        TEST_METHOD(CaptureVideo_ShouldReturnNullOnCancel)
+        {
+            BEGIN_TEST_METHOD_PROPERTIES()
+                TEST_METHOD_PROPERTY(L"RunAs", L"UAP")
+                END_TEST_METHOD_PROPERTIES();
 
-        //TEST_METHOD(CaptureVideo_ShouldReturnNullOnCancel)
-        //{
-        //    BEGIN_TEST_METHOD_PROPERTIES()
-        //        TEST_METHOD_PROPERTY(L"RunAs", L"UAP")
-        //        END_TEST_METHOD_PROPERTIES();
+            // Arrange
+            winrt::Microsoft::Windows::Media::Capture::CameraCaptureUI cameraUI({ 12345 });
+            winrt::Windows::Storage::StorageFile video = nullptr;
+            // Act
+            try
+            {
+                video = cameraUI.CaptureFileAsync(winrt::Windows::Media::Capture::CameraCaptureUIMode::Video).get();
+            }
+            catch (winrt::hresult_canceled const&)
+            {
+                Log::Comment(L"Video capture was canceled.");
+            }
 
-        //    // Arrange
-        //    MockWindowId mockWindowId{ 12345 };
-        //    CameraCaptureUI cameraUI{ mockWindowId };
-
-        //    // Simulate cancellation by the user
-        //    StorageFile video = nullptr;
-        //    try
-        //    {
-        //        video = cameraUI.CaptureFileAsync(CameraCaptureUIMode::Video).get();
-        //    }
-        //    catch (hresult_canceled const&)
-        //    {
-        //        Log::Comment(L"Video capture was canceled.");
-        //    }
-
-        //    // Assert
-        //    VERIFY_IS_NULL(video);
-        //}
+            // Assert
+            VERIFY_IS_NULL(video);
+        }
     };
 }
--- CameraCaptureUITest/pch.h
+++ CameraCaptureUITest/pch.h
@@ -8,20 +8,29 @@
 // However, files listed here are ALL re-compiled if any one of them is updated between builds.
 // Do not add files here that you will be updating frequently as this negates the performance advantage.
 
+
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
 #ifndef PCH_H
 #define PCH_H
 
-// Standard headers
-#include <windows.h>
-#include "WexTestClass.h"
-#include <winrt/Windows.Foundation.h>
-#include <winrt/Windows.Storage.h>
-#include <winrt/Windows.Media.Capture.h>
-#include <winrt/Windows.ApplicationModel.Activation.h>
-#include <winrt/Windows.ApplicationModel.Background.h>
-#include <winrt/Windows.Foundation.Collections.h>
-#include <winrt/Windows.Management.Deployment.h>
-#include <winrt/Windows.System.h>
+#include <unknwn.h>
 
-#include <filesystem>
+#include <wil/cppwinrt.h>
+
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.Storage.h>
+
+
+#include <winrt/Microsoft.Windows.Media.Capture.h>
+
+#include <WexTestClass.h>
+
+#include <WindowsAppRuntime.Test.AppModel.h>
+#include <WindowsAppRuntime.Test.Package.h>
+#include <WindowsAppRuntime.Test.Bootstrap.h>
+
+
 #endif //PCH_H
