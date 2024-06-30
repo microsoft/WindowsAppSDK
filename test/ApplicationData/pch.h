f753170eaddb4c8516a53d6ad6b476cffd5c0f64
Howard Kapustein <howardk@microsoft.com>
2024-06-29 17:10:22 -0700

    Microsoft.Windows.Storage.ApplicationData (#4057)
    * Added AddPackageSetOptions. Added tests for AddPackageSetAsync. Fixed bugs
    
    * Fixed typos. Added missing AddPackageAsync in .idl
    
    * Remove dead code
    
    * Update Transport package's manifested inproc servers
    
    * Added new directory
    
    * Added ApplicationData project. Expanded docs. Added missing ApplicationDataCreateDisposition. Added TerminalVelocity support
    
    * Added stub implementations
    
    * Initial implementation
    
    * Implemented ApplicationData
    
    * Tweaked to be smarter about Windows.Storage.ApplicationData's weird limitations
    
    * Fixed ApplicationData.MachinePath to use the right path. Bumped WIL to 1.0.231216.1 (to get access to wil::reg APIs)
    
    * Implemented IsMachinePathSupported()
    
    * Added tests (mostly)
    
    * Fixed machine path test infra (AppDataPaths doesn't work in our scenario)
    
    * Removed method overload per feedback. Added more doc-comments
    
    * More fixes and optimizations
    
    * More test coverage
    
    * Updated MachinePath/etc to only be accessible if a package in the family is registered to the user OR running as SYSTEM
    
    * Removed ClearAllAsync() and ClearMachineAsync(). Use ClearAsync(locality)
    
    * Updated per API Review
    
    * Fixed build error
    
    * Fixed bad merge. Added ApplicationData Projection

---
+++ test/ApplicationData/pch.h
@@ -0,0 +1,33 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+#ifndef PCH_H
+#define PCH_H
+
+#include <unknwn.h>
+
+#include <shlobj_core.h>
+#include <shlobj.h>
+
+#include <wil/cppwinrt.h>
+#include <wil/filesystem.h>
+
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+
+#include <winrt/Windows.ApplicationModel.h>
+#include <winrt/Windows.Management.Core.h>
+#include <winrt/Windows.Management.Deployment.h>
+#include <winrt/Windows.Storage.h>
+
+#include <winrt/Microsoft.Windows.Storage.h>
+
+#include <WexTestClass.h>
+
+#include <AppModel.Package.h>
+
+#include <WindowsAppRuntime.Test.AppModel.h>
+#include <WindowsAppRuntime.Test.Package.h>
+#include <WindowsAppRuntime.Test.Bootstrap.h>
+
+#endif //PCH_H
