ca908a156bed225c1263d76bdde830fe8f33148a
Saharsh Jaiswal <sahjaiswal@microsoft.com>
2024-09-26 14:02:03 +0530

    Minor fix

--- test/CameraCaptureUITests/APITests.cpp
+++ test/CameraCaptureUITests/APITests.cpp
@@ -2,7 +2,6 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 #include "pch.h"
-#include "pch.h"
 #include "winrt\Microsoft.Windows.Media.Capture.h"
 #include <thread>
 #include <future>
