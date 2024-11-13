c01327a4cc5714810cc756b85155cde5f68999f9
Howard Kapustein <howardk@microsoft.com>
2024-11-13 13:47:27 -0800

    Updated WindowsAppRuntime_UniversalBGTaskDLL - ordering of some logic in .vcxproj, fixed explicit versions to use central versioning (eng\Version.*), removed unnecessary dependencies, removed compiler/linker settings set project wide (see Directory.Build.props), adding missing Copyright, etc. Same output (and hopefully build pipeline won't choke now)

--- dev/WindowsAppRuntime_UniversalBGTaskDLL/Task.cpp
+++ dev/WindowsAppRuntime_UniversalBGTaskDLL/Task.cpp
@@ -3,9 +3,7 @@
 
 #include "pch.h"
 #include "Task.h"
-#if __has_include("Task.g.cpp")
 #include "Task.g.cpp"
-#endif
 
 using namespace winrt;
 using namespace winrt::Windows::ApplicationModel::Background;
@@ -13,17 +11,18 @@ using namespace winrt::Windows::Storage;
 
 namespace winrt::Microsoft::Windows::ApplicationModel::Background::UniversalBGTask::implementation
 {
-    inline constexpr CLSID IID_IBackgroundTask = { 2098451764, 64786, 17358, 140, 34, 234, 31, 241, 60, 6, 223 };
+    inline constexpr CLSID IID_IBackgroundTask{ 2098451764, 64786, 17358, 140, 34, 234, 31, 241, 60, 6, 223 };
+
     void Task::Run(winrt::Windows::ApplicationModel::Background::IBackgroundTaskInstance taskInstance)
     {
-        winrt::hstring lookupStr = winrt::to_hstring(taskInstance.Task().TaskId());
-        ApplicationDataContainer localSettings = ApplicationData::Current().LocalSettings();
-        auto values = localSettings.Values();
-        auto lookupobj = values.Lookup(lookupStr);
-        winrt::guid comClsId = winrt::unbox_value<winrt::guid>(lookupobj);
+        winrt::hstring lookupKey{ winrt::to_hstring(taskInstance.Task().TaskId()) };
+        ApplicationDataContainer localSettings{ ApplicationData::Current().LocalSettings() };
+        auto values{ localSettings.Values() };
+        auto lookupValue{ values.Lookup(lookupKey) };
+        winrt::guid comClsId{ winrt::unbox_value<winrt::guid>(lookupValue) };
 
-        winrt::Windows::ApplicationModel::Background::IBackgroundTask bgTask = nullptr;
-        winrt::hresult hr = CoCreateInstance(comClsId, nullptr, CLSCTX_LOCAL_SERVER, IID_IBackgroundTask, reinterpret_cast<void**>(&bgTask));
+        winrt::Windows::ApplicationModel::Background::IBackgroundTask bgTask;
+        winrt::hresult hr{ CoCreateInstance(comClsId, nullptr, CLSCTX_LOCAL_SERVER, IID_IBackgroundTask, reinterpret_cast<void**>(&bgTask)) };
         bgTask.Run(taskInstance);
     }
 }
