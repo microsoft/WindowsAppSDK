6839e9592e2ab85450245a6716ca8e795803ce8e
Howard Kapustein <howardk@microsoft.com>
2024-11-22 22:46:26 -0800

    Delegate to OS RegFreeWinRT when available (>=24H1) (#4728)
    * Delegate to OS RegFreeWinRT when available (>=19H1)
    
    * Changed DevCheck to emit expected TAEF version found (not just -Verbose). Added -CheckTAEFService -CheckVisualStudio -CheckDeveloperMode to 'DevCheck: Setup/Verify developer environment' to cover the missing checks
    
    * Removed redundant checks done later in pipelines
    
    * Added diagnostics for troubleshooting. Fixed compiler warnings
    
    * More debuggery
    
    * Added IsWindows11_24H2OrGreater(). Changed URFW to only delegate to OS RFW on >=24H1 (where DynDep will delegate to OS DynDep). Fixed some bad sprintfs. Changed Is*Supported(PackageDeploymentFeature::PackageUriScheme_ms_uup) to use the Is*Supported function
    
    * More diagnostics
    
    * Bumped TAEF to latest version 10.95.240918004
    
    * Moar debugging
    
    * Fixes
    
    * Minor fix
    
    * Removed previous interim block disabling Dynamic Dependencies delegation to OS Dynamic Dependencies. Let's take it for a spin and see how it handles...
    
    * Fixed DevCheck not getting VS' path thus not checking VS stuff
    
    * Split IsReady tests into RestrictedUser vs ElevatedUser
    
    * Fix mis-tagging of TAEF properties (though according to TAEF folks TEST_CLASS_PROPERTY is treated same as TEST_METHOD_PROPERTY if inside a BEGIN/END_METHOD_PROPERTIES() block because it's been a common mistake, so TAEF now treats it effectively as TEST_PROPERTY() scoped to the surrounding BEGIN/END_*_PROPERTY()
    
    * DevCheck -SyncDependencies update
    
    * Removed FrameworkUdk references in packages.config. Added Build Dependencies in WindowsAppRuntime.sln for anything using FrUdk on the Microsoft.FrameworkUdk.PackageReferences.csproj as th SexyNewfangledWay(TM) to restore FrameworkUdk (when necessary)
    
    * PackageManager test tweaks
    
    * Expanded enablement for further test investigation
    
    * Fixed test to account for OS vs WinAppSDK error detection
    
    * Fixed RegisterNewerIfAvailable
    
    * Removed 22H2 testing
    
    * Fixed h/cpp mismatch
    
    * Removed non-existent GUID/reference (bad merge?)
    
    * Fixed test bug not handling all behavior coming out of OS API
    
    * Fixed tests
    
    * Updated WinRT tests akin to Win32

--- test/PackageManager/API/PackageDeploymentManagerTests_IsReadyOrNewerAvailable.cpp
+++ test/PackageManager/API/PackageDeploymentManagerTests_IsReadyOrNewerAvailable.cpp
@@ -52,7 +52,7 @@ namespace Test::PackageManager::Tests
             return true;
         }
 
-        TEST_METHOD(IsPackageReadyOrNewerAvailable_PackageFullName_InvalidParameter)
+        TEST_METHOD(IsPackageReadyOrNewerAvailable_InvalidParameter)
         {
             if (TPMT::SkipIfFeatureNotSupported_IsPackageReadyOrNewerAvailable())
             {
@@ -184,8 +184,8 @@ namespace Test::PackageManager::Tests
 
             try
             {
-                PCWSTR packageFamilyName{ L"Not a valid Package Family Name" };
-                packageDeploymentManager.IsPackageReadyOrNewerAvailable(packageFamilyName);
+                PCWSTR c_packageFamilyName{ L"Does.Not.Exist_1234567890abc" };
+                packageDeploymentManager.IsPackageReadyOrNewerAvailable(c_packageFamilyName);
                 VERIFY_FAIL(L"Success is not expected");
             }
             catch (winrt::hresult_error& e)
