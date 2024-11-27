6b4e1a2a731de072cf7dfefb7bb773349eb0aa6a
Satwik Kumar Sharma <satwiksharma@microsoft.com>
2024-11-27 14:46:29 +0530

    badge notfication support code changes

--- dev/AppNotifications/NotificationProperties.h
+++ dev/AppNotifications/NotificationProperties.h
@@ -6,7 +6,7 @@
 #include <frameworkUdk/ToastNotificationsRT.h>
 #include "BaseNotification.h"
 
-struct NotificationProperties : winrt::implements<NotificationProperties, ::ABI::Microsoft::Internal::ToastNotifications::INotificationProperties>
+struct NotificationProperties : winrt::implements<NotificationProperties, ::ABI::Microsoft::Internal::ToastNotifications::INotificationProperties, ::ABI::Microsoft::Internal::ToastNotifications::INotificationProperties3>
 {
     NotificationProperties(winrt::Microsoft::Windows::AppNotifications::AppNotification const& toastNotification);
 
