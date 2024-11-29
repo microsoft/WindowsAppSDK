7e0bec60ff9c3c885f55067e36874e5cb76b0ad2
Satwik Kumar Sharma <satwiksharma@microsoft.com>
2024-11-29 15:24:06 +0530

    Unpackaged app CTA compliance code changes

--- dev/PushNotifications/PushNotificationsLongRunningTask/NotificationListener.cpp
+++ dev/PushNotifications/PushNotificationsLongRunningTask/NotificationListener.cpp
@@ -80,3 +80,15 @@ STDMETHODIMP_(HRESULT __stdcall) NotificationListener::OnToastNotificationReceiv
     return S_OK;
 }
 CATCH_RETURN()
+
+STDMETHODIMP_(HRESULT __stdcall) NotificationListener::GetAppProcessName(
+    _Out_ HSTRING* appProcessName) noexcept try
+{
+    auto lock{ m_lock.lock_shared() };
+    *appProcessName = ((m_processName.c_str() == nullptr || wcslen(m_processName.c_str()) == 0) ?
+        wil::unique_hstring() :
+        wil::make_unique_string<wil::unique_hstring>(m_processName.c_str())).release();
+    
+    return S_OK;
+}
+CATCH_RETURN()
