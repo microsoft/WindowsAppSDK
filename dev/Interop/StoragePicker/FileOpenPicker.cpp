e6b3e8348b8846dc15ea3b7c334794c146dfd2ab
Dinah Gao <x.kic8462852@gmail.com>
2024-12-06 21:58:27 +0800

    pack

--- dev/Interop/StoragePicker/FileOpenPicker.cpp
+++ dev/Interop/StoragePicker/FileOpenPicker.cpp
@@ -2,25 +2,36 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 #include "pch.h"
 #include "FileOpenPicker.h"
-#include "Microsoft.Storage.Pickers.FileOpenPicker.g.cpp"
+#include "Microsoft.Windows.Storage.Pickers.FileOpenPicker.g.cpp"
 #include <windows.h>
 #include <shobjidl.h> 
 #include <wil/cppwinrt.h>
 #include <wil/com.h>
 #include <wil/resource.h>
 
-namespace winrt::Microsoft::Storage::Pickers::implementation
+namespace winrt::Microsoft::Windows::Storage::Pickers::implementation
 {
 	void FileOpenPicker::MyProperty(int32_t value)
 	{
 		throw hresult_not_implemented();
 	}
 
+    hstring FileOpenPicker::MyString()
+    {
+        throw hresult_not_implemented();
+    }
+    void FileOpenPicker::MyString(hstring const& value)
+    {
+        throw hresult_not_implemented();
+    }
+
 	winrt::Windows::Foundation::IAsyncOperation<winrt::Windows::Storage::StorageFile> FileOpenPicker::PickSingleFileAsync()
 	{
 		winrt::apartment_context ui_thread;
 
 		co_await winrt::resume_background();
+
+        MyString();
 
 		// Handle cancellation.
 		auto cancellationToken = co_await winrt::get_cancellation_token();
--- dev/Interop/StoragePicker/FileOpenPicker.h
+++ dev/Interop/StoragePicker/FileOpenPicker.h
@@ -1,20 +1,23 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 #pragma once
-#include "Microsoft.Storage.Pickers.FileOpenPicker.g.h"
+#include "Microsoft.Windows.Storage.Pickers.FileOpenPicker.g.h"
 
-namespace winrt::Microsoft::Storage::Pickers::implementation
+namespace winrt::Microsoft::Windows::Storage::Pickers::implementation
 {
     struct FileOpenPicker : FileOpenPickerT<FileOpenPicker>
     {
         FileOpenPicker() = default;
+
+        hstring MyString();
+        void MyString(hstring const& value);
 
         int32_t MyProperty();
         void MyProperty(int32_t value);
         winrt::Windows::Foundation::IAsyncOperation<winrt::Windows::Storage::StorageFile> PickSingleFileAsync();
     };
 }
-namespace winrt::Microsoft::Storage::Pickers::factory_implementation
+namespace winrt::Microsoft::Windows::Storage::Pickers::factory_implementation
 {
     struct FileOpenPicker : FileOpenPickerT<FileOpenPicker, implementation::FileOpenPicker>
     {
