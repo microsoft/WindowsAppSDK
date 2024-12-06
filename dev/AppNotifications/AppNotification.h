cacca8fd40fd245ba8640f820d9a39da3a9467c1
Satwik Kumar Sharma <satwiksharma@microsoft.com>
2024-12-06 16:08:26 +0530

    AppNotification Refactoring

--- dev/AppNotifications/AppNotification.h
+++ dev/AppNotifications/AppNotification.h
@@ -18,22 +18,8 @@ namespace winrt::Microsoft::Windows::AppNotifications::implementation
         AppNotification();
 
         AppNotification(hstring const& payload);
-       /* hstring Tag();
-        void Tag(hstring const& value);
-        hstring Group();
-        void Group(hstring const& value);
-        uint32_t Id();
-        hstring Payload(); */
         winrt::Microsoft::Windows::AppNotifications::AppNotificationProgressData Progress();
         void Progress(winrt::Microsoft::Windows::AppNotifications::AppNotificationProgressData const& value);
-        /*winrt::Windows::Foundation::DateTime Expiration();
-        void Expiration(winrt::Windows::Foundation::DateTime const& value);
-        bool ExpiresOnReboot();
-        void ExpiresOnReboot(bool value);
-        winrt::Microsoft::Windows::AppNotifications::AppNotificationPriority Priority();
-        void Priority(winrt::Microsoft::Windows::AppNotifications::AppNotificationPriority const& value);
-        bool SuppressDisplay();
-        void SuppressDisplay(bool value);*/
 
         // IAppNotificationInternal
         void SetNotificationId(uint32_t id);
@@ -42,18 +28,7 @@ namespace winrt::Microsoft::Windows::AppNotifications::implementation
         void ConferencingConfig(winrt::Microsoft::Windows::AppNotifications::AppNotificationConferencingConfig const& value);
 
     private:
-       /* winrt::hstring m_tag{};
-        winrt::hstring m_group{};
-        uint32_t m_notificationId{ 0 };
-        winrt::hstring m_payload{};*/
         winrt::Microsoft::Windows::AppNotifications::AppNotificationProgressData m_progressData{ nullptr };
-        /*winrt::Windows::Foundation::DateTime m_expirationTime{};
-        bool m_expiresOnReboot{ false };
-        winrt::Microsoft::Windows::AppNotifications::AppNotificationPriority m_priority
-        { winrt::Microsoft::Windows::AppNotifications::AppNotificationPriority::Default };
-        bool m_suppressDisplay{ false };
-        wil::srwlock m_lock;*/
-
         winrt::Microsoft::Windows::AppNotifications::AppNotificationConferencingConfig m_conferencingConfig{ nullptr };
     };
 }
