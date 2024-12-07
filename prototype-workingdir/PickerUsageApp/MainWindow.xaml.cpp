fc9a03af48cf4522a67a2cce717548270cfa8942
Xiang Hong <hx.hongxiang@gmail.com>
2024-12-07 12:06:47 +0800

    feature: add folder functionalities to demo test app

--- prototype-workingdir/PickerUsageApp/MainWindow.xaml.cpp
+++ prototype-workingdir/PickerUsageApp/MainWindow.xaml.cpp
@@ -19,168 +19,260 @@ using namespace Microsoft::UI::Xaml;
 
 namespace winrt::PickerUsageApp::implementation
 {
+    Windows::Storage::Pickers::PickerViewMode Convert(Microsoft::Storage::Pickers::PickerViewMode viewMode)
+    {
+        switch (viewMode)
+        {
+        case winrt::Microsoft::Storage::Pickers::PickerViewMode::List:
+            return Windows::Storage::Pickers::PickerViewMode::List;
+        case winrt::Microsoft::Storage::Pickers::PickerViewMode::Thumbnail:
+            return Windows::Storage::Pickers::PickerViewMode::Thumbnail;
+        default:
+            return Windows::Storage::Pickers::PickerViewMode::List;
+        }
+    }
 
-	Windows::Storage::Pickers::PickerViewMode Convert(Microsoft::Storage::Pickers::PickerViewMode viewMode) {
-		switch (viewMode)
-		{
-		case winrt::Microsoft::Storage::Pickers::PickerViewMode::List:
-			return Windows::Storage::Pickers::PickerViewMode::List;
-		case winrt::Microsoft::Storage::Pickers::PickerViewMode::Thumbnail:
-			return Windows::Storage::Pickers::PickerViewMode::Thumbnail;
-		default:
-			return Windows::Storage::Pickers::PickerViewMode::List;
-		}
-	}
+    winrt::Windows::Foundation::IAsyncAction MainWindow::SDKClick(IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args)
+    {
+        labelBlock().Text(L"");
+        hstring message{};
+        try
+        {
+            switch (m_PickerTypeIndex)
+            {
+            case 0:
+                message = co_await  OpenFileSDKClick(sender, args);
+                break;
+            case 2:
+                message = co_await  OpenFolderSDKClick();
+                break;
+            default:
+                message = L"Unsupported Picker Type";
+                break;
+            }
+        }
+        catch (winrt::hresult_error const& ex_)
+        {
+            message = ex_.message();
+        }
+        labelBlock().Text(message);
+    }
 
-	Windows::Foundation::IAsyncAction MainWindow::OpenFileUWPClick(IInspectable const&, RoutedEventArgs const&)
-	{
-		hstring error_mesasge{};
-		try {
-			labelBlock().Text(L"");
-			auto windowNative = this->m_inner.as<IWindowNative>();
-			HWND hWnd = nullptr;
-			check_hresult(windowNative->get_WindowHandle(&hWnd));
+    winrt::Windows::Foundation::IAsyncAction MainWindow::UWPClick(IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args)
+    {
+        labelBlock().Text(L"");
+        hstring message{};
+        try
+        {
+            switch (m_PickerTypeIndex)
+            {
+            case 0:
+                message = co_await OpenFileUWPClick(sender, args);
+                break;
+            case 2:
+                message = co_await OpenFolderUWPClick();
+                break;
 
-			winrt::Windows::Storage::Pickers::FileOpenPicker picker{};
+            default:
+                message = L"Unsupported Picker Type";
+                break;
+            }
+        }
+        catch (winrt::hresult_error const& ex_)
+        {
+            message = ex_.message();
+        }
+        labelBlock().Text(message);
+    }
 
-			picker.as<::IInitializeWithWindow>()->Initialize(hWnd);
-			SetPickerOptions<winrt::Windows::Storage::Pickers::FileOpenPicker, winrt::Windows::Storage::Pickers::PickerLocationId>(picker);
-			//SetPickerOptions(picker);
-			picker.ViewMode(Convert(m_ViewMode));
 
-			if (!m_MultipleSelect) {
-				auto& file = co_await picker.PickSingleFileAsync();
-				if (file != nullptr) {
-					labelBlock().Text(file.Path());
-				}
-			}
-			else {
-				auto& files = co_await picker.PickMultipleFilesAsync();
-				winrt::hstring names = L"";
-				for (auto& file : files)
-				{
-					names = names + file.Path() + L"\n";
-				}
-				labelBlock().Text(names);
-			}
+    Windows::Foundation::IAsyncOperation<hstring> MainWindow::OpenFileUWPClick(IInspectable const&, RoutedEventArgs const&)
+    {
+        auto windowNative = this->m_inner.as<IWindowNative>();
+        HWND hWnd = nullptr;
+        check_hresult(windowNative->get_WindowHandle(&hWnd));
 
-			co_return;
-		}
-		catch (winrt::hresult_error const& ex_)
-		{
-			error_mesasge = ex_.message();
-		}
-		if (!error_mesasge.empty())
-		{
-			winrt::Windows::UI::Popups::MessageDialog dialog(error_mesasge);
-			co_await dialog.ShowAsync();
-		}
-	}
+        winrt::Windows::Storage::Pickers::FileOpenPicker picker{};
 
-	Windows::Foundation::IAsyncAction MainWindow::OpenFileSDKClick(IInspectable const&, RoutedEventArgs const&)
-	{
-		hstring error_mesasge{};
-		try {
-			labelBlock().Text(L"");
-			auto id = AppWindow().Id();
-			winrt::Microsoft::Storage::Pickers::FileOpenPicker picker{ id };
+        picker.as<::IInitializeWithWindow>()->Initialize(hWnd);
+        SetPickerOptions<winrt::Windows::Storage::Pickers::FileOpenPicker, winrt::Windows::Storage::Pickers::PickerLocationId>(picker);
+        picker.ViewMode(Convert(m_ViewMode));
 
-			SetPickerOptions<winrt::Microsoft::Storage::Pickers::FileOpenPicker, winrt::Microsoft::Storage::Pickers::PickerLocationId>(picker);
-			//SetPickerOptions(picker);
-			picker.ViewMode(m_ViewMode);
-			if (!m_MultipleSelect) {
-				auto& file = co_await picker.PickSingleFileAsync();
-				if (file != nullptr) {
-					labelBlock().Text(file.Path());
-				}
-			}
-			else {
-				auto& files = co_await picker.PickMultipleFilesAsync();
-				winrt::hstring names = L"";
-				for (auto& file : files)
-				{
-					names = names + file.Path() + L"\n";
-				}
-				labelBlock().Text(names);
-			}
-			co_return;
-		}
-		catch (winrt::hresult_error const& ex_)
-		{
-			error_mesasge = ex_.message();
-		}
-		if (!error_mesasge.empty())
-		{
-			winrt::Windows::UI::Popups::MessageDialog dialog(error_mesasge);
-			co_await dialog.ShowAsync();
-		}
-	}
+        if (!m_MultipleSelect)
+        {
+            auto& file = co_await picker.PickSingleFileAsync();
+            if (file != nullptr)
+            {
+                co_return file.Path();
+            }
+        }
+        else
+        {
+            auto& files = co_await picker.PickMultipleFilesAsync();
+            winrt::hstring names = L"";
+            for (auto& file : files)
+            {
+                names = names + file.Path() + L"\n";
+            }
+            co_return names;
+        }
+        co_return L"no selection";
+    }
+
+    winrt::Windows::Foundation::IAsyncOperation<hstring> MainWindow::OpenFolderUWPClick()
+    {
+        auto windowNative = this->m_inner.as<IWindowNative>();
+        HWND hWnd = nullptr;
+        check_hresult(windowNative->get_WindowHandle(&hWnd));
+
+        winrt::Windows::Storage::Pickers::FolderPicker picker{};
+
+        picker.as<::IInitializeWithWindow>()->Initialize(hWnd);
+        SetPickerOptions<winrt::Windows::Storage::Pickers::FolderPicker, winrt::Windows::Storage::Pickers::PickerLocationId>(picker);
+        picker.ViewMode(Convert(m_ViewMode));
+
+        if (!m_MultipleSelect)
+        {
+            auto& folder = co_await picker.PickSingleFolderAsync();
+            if (folder != nullptr)
+            {
+                co_return folder.Path();
+            }
+        }
+        else
+        {
+            co_return L"Folder multi selection is not support";
+        }
+        co_return L"no selection";
+
+        throw hresult_not_implemented();
+    }
+
+    Windows::Foundation::IAsyncOperation<hstring> MainWindow::OpenFileSDKClick(IInspectable const&, RoutedEventArgs const&)
+    {
+        auto id = AppWindow().Id();
+        winrt::Microsoft::Storage::Pickers::FileOpenPicker picker{ id };
+
+        SetPickerOptions<winrt::Microsoft::Storage::Pickers::FileOpenPicker, winrt::Microsoft::Storage::Pickers::PickerLocationId>(picker);
+        //SetPickerOptions(picker);
+        picker.ViewMode(m_ViewMode);
+        if (!m_MultipleSelect)
+        {
+            auto& file = co_await picker.PickSingleFileAsync();
+            if (file != nullptr)
+            {
+                co_return file.Path();
+            }
+        }
+        else
+        {
+            auto& files = co_await picker.PickMultipleFilesAsync();
+            winrt::hstring names = L"";
+            for (auto& file : files)
+            {
+                names = names + file.Path() + L"\n";
+            }
+            co_return names;
+        }
+        co_return L"no selection";
+    }
+
+    winrt::Windows::Foundation::IAsyncOperation<hstring> MainWindow::OpenFolderSDKClick()
+    {
+        auto id = AppWindow().Id();
+        winrt::Microsoft::Storage::Pickers::FolderPicker picker{ id };
+
+        SetPickerOptions<winrt::Microsoft::Storage::Pickers::FolderPicker, winrt::Microsoft::Storage::Pickers::PickerLocationId>(picker);
+        picker.ViewMode(m_ViewMode);
+        if (!m_MultipleSelect)
+        {
+            auto& folder = co_await picker.PickSingleFolderAsync();
+            if (folder != nullptr)
+            {
+                co_return folder.Path();
+            }
+        }
+        else
+        {
+            co_return L"Folder multiple selection is not supported";
+        }
+        co_return L"no selection";
+    }
 }
 
 
 void winrt::PickerUsageApp::implementation::MainWindow::UIFronzenTestClick(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
 {
-	counterBlock().Text(std::to_wstring(count));
-	count++;
+    counterBlock().Text(std::to_wstring(count));
+    count++;
 }
 
 
 void winrt::PickerUsageApp::implementation::MainWindow::toggleCustomLableClick(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
 {
-	m_isUseCustomLabel = !m_isUseCustomLabel;
-	if (m_isUseCustomLabel)
-	{
-		useCustomLabelToggle().Content(box_value(L"Disable"));
-		customLabelBox().Visibility(Visibility::Visible);
-	}
-	else
-	{
-		useCustomLabelToggle().Content(box_value(L"Enable"));
-		customLabelBox().Visibility(Visibility::Collapsed);
-	}
+    m_isUseCustomLabel = !m_isUseCustomLabel;
+    if (m_isUseCustomLabel)
+    {
+        useCustomLabelToggle().Content(box_value(L"Disable"));
+        customLabelBox().Visibility(Visibility::Visible);
+    }
+    else
+    {
+        useCustomLabelToggle().Content(box_value(L"Enable"));
+        customLabelBox().Visibility(Visibility::Collapsed);
+    }
 }
 
 
 void winrt::PickerUsageApp::implementation::MainWindow::ViewModeSelectionChanged(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Controls::SelectionChangedEventArgs const& e)
 {
-	auto radioButtons = sender.as<Microsoft::UI::Xaml::Controls::RadioButtons>();
-	auto selectedIndex = radioButtons.SelectedIndex();
-	switch (selectedIndex)
-	{
-	case 0:
-		m_ViewMode = Microsoft::Storage::Pickers::PickerViewMode::List;
-		break;
-	case 1:
-		m_ViewMode = Microsoft::Storage::Pickers::PickerViewMode::Thumbnail;
-		break;
-	default:
-		break;
-	}
+    auto radioButtons = sender.as<Microsoft::UI::Xaml::Controls::RadioButtons>();
+    auto selectedIndex = radioButtons.SelectedIndex();
+    switch (selectedIndex)
+    {
+    case 0:
+        m_ViewMode = Microsoft::Storage::Pickers::PickerViewMode::List;
+        break;
+    case 1:
+        m_ViewMode = Microsoft::Storage::Pickers::PickerViewMode::Thumbnail;
+        break;
+    default:
+        break;
+    }
+
+
 }
 
 
 void winrt::PickerUsageApp::implementation::MainWindow::FilterTypeSelectionChanged(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Controls::SelectionChangedEventArgs const& e)
 {
-	auto radioButtons = sender.as<Microsoft::UI::Xaml::Controls::RadioButtons>();
+    auto radioButtons = sender.as<Microsoft::UI::Xaml::Controls::RadioButtons>();
 
-	m_FilterTypeIndex = radioButtons.SelectedIndex();
+    m_FilterTypeIndex = radioButtons.SelectedIndex();
 }
 
 
 void winrt::PickerUsageApp::implementation::MainWindow::SettingsIdentifierChanged(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Controls::SelectionChangedEventArgs const& e)
 {
-	auto radioButtons = sender.as<Microsoft::UI::Xaml::Controls::RadioButtons>();
-	m_SettingsIdentifierIndex = radioButtons.SelectedIndex();
+    auto radioButtons = sender.as<Microsoft::UI::Xaml::Controls::RadioButtons>();
+    m_SettingsIdentifierIndex = radioButtons.SelectedIndex();
 }
 
 void winrt::PickerUsageApp::implementation::MainWindow::PickerLocationIdChanged(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Controls::SelectionChangedEventArgs const& e)
 {
-	auto radioButtons = sender.as<Microsoft::UI::Xaml::Controls::RadioButtons>();
-	m_PickerLocationIdIndex = radioButtons.SelectedIndex();
+    auto radioButtons = sender.as<Microsoft::UI::Xaml::Controls::RadioButtons>();
+    m_PickerLocationIdIndex = radioButtons.SelectedIndex();
 }
 
 
 void winrt::PickerUsageApp::implementation::MainWindow::MultiSelectToggled(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
 {
-	m_MultipleSelect = sender.as<Microsoft::UI::Xaml::Controls::ToggleSwitch>().IsOn();
+    m_MultipleSelect = sender.as<Microsoft::UI::Xaml::Controls::ToggleSwitch>().IsOn();
 }
+
+void winrt::PickerUsageApp::implementation::MainWindow::PickerTypeChanged(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+{
+    m_PickerTypeIndex = sender.as<Microsoft::UI::Xaml::Controls::RadioButtons>().SelectedIndex();
+}
+
+
