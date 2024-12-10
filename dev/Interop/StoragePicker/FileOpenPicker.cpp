bbf72f8edecc9a4b74bf61bfb5ab5f8c72b0956f
Dinah Gao <116714259+DinahK-2SO@users.noreply.github.com>
2024-12-10 11:01:23 +0800

    Microsoft.Windows.Storage.Pickers.FileOpenPicker.PickSingleFileAsync (#4945)
    Creating new API Microsoft.Windows.Storage.Pickers.FileOpenPicker.PickSingleFileAsync

---
+++ dev/Interop/StoragePicker/FileOpenPicker.cpp
@@ -0,0 +1,58 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+#include "pch.h"
+#include "FileOpenPicker.h"
+#include "Microsoft.Windows.Storage.Pickers.FileOpenPicker.g.cpp"
+#include <windows.h>
+#include <shobjidl.h> 
+#include <wil/cppwinrt.h>
+#include <wil/com.h>
+#include <wil/resource.h>
+
+namespace winrt::Microsoft::Windows::Storage::Pickers::implementation
+{
+	winrt::Windows::Foundation::IAsyncOperation<winrt::Windows::Storage::StorageFile> FileOpenPicker::PickSingleFileAsync()
+	{
+		winrt::apartment_context ui_thread;
+
+		co_await winrt::resume_background();
+
+		// Handle cancellation.
+		auto cancellationToken = co_await winrt::get_cancellation_token();
+		if (cancellationToken())
+		{
+			co_return nullptr;
+		}
+
+		// TODO: should we initialize COM?
+		//wil::com_initialize_ex initializeCom{ COINIT_APARTMENTTHREADED };
+
+		wil::com_ptr<IFileOpenDialog> fileOpenDialog;
+		THROW_IF_FAILED(CoCreateInstance(CLSID_FileOpenDialog, nullptr, CLSCTX_ALL, IID_PPV_ARGS(&fileOpenDialog)));
+
+		HRESULT hr = fileOpenDialog->Show(nullptr);
+		if (FAILED(hr) || cancellationToken())
+		{
+			co_return nullptr;
+		}
+
+		wil::com_ptr<IShellItem> shellItem;
+		THROW_IF_FAILED(fileOpenDialog->GetResult(shellItem.put()));
+
+		wil::unique_cotaskmem_string filePath;
+		THROW_IF_FAILED(shellItem->GetDisplayName(SIGDN_FILESYSPATH, &filePath));
+
+		// Convert the file path to a StorageFile.
+		auto const& file = co_await winrt::Windows::Storage::StorageFile::GetFileFromPathAsync(filePath.get());
+		if (cancellationToken()) {
+			co_return nullptr;
+		}
+
+		co_await ui_thread;
+		if (cancellationToken()) {
+			co_return nullptr;
+		}
+
+		co_return file;
+	}
+}
