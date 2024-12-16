ebf1f2fd283600a2f86a9fa35ecaa6acd3e0e515
Ayush Jaiswal <ayushjaiswal@microsoft.com>
2024-12-16 16:43:39 +0530

    Unit tests for OAuth2.0 API

---
+++ test/TestApps/OAuthTestApp/main.cpp
@@ -0,0 +1,36 @@
+#include "pch.h"
+#include <cstdio>
+#include <windows.h>
+#include <winrt/Microsoft.Windows.AppLifecycle.h>
+#include <winrt/Microsoft.Security.Authentication.OAuth.h>
+#include <winrt/Windows.ApplicationModel.Activation.h>
+#include <fstream> 
+using namespace winrt::Microsoft::Windows::AppLifecycle;
+using namespace winrt::Microsoft::Security::Authentication::OAuth;
+using namespace winrt::Windows::ApplicationModel::Activation;
+
+int main()
+{
+    try
+    {
+        auto args = AppInstance::GetCurrent().GetActivatedEventArgs();
+        auto kind = args.Kind();
+        if (kind == ExtendedActivationKind::Protocol)
+        {
+            auto uri = args.Data().as<IProtocolActivatedEventArgs>().Uri();
+            if (!OAuth2Manager::CompleteAuthRequest(uri))
+            {
+                std::printf("WARNING: Failed to complete auth request with uri '%ls'.\n", uri.RawUri().c_str());
+                std::printf("WARNING: This may or may not be expected depending on which test is running.\n");
+            }
+        }
+        else
+        {
+            std::printf("WARNING: Application was launched with something other than protocol activation!\n");
+        };
+    }
+    catch (const std::exception& ex)
+    {
+        std::printf("Standard exception: %s\n", ex.what());
+    }
+}
