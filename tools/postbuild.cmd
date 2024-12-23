963de7fd14431f3fe92822d9b93e603cb427e823
Dinah Gao <x.kic8462852@gmail.com>
2024-12-23 15:57:31 +0800

    update script - add cleanup

--- tools/postbuild.cmd
+++ tools/postbuild.cmd
@@ -2,8 +2,13 @@ REM execute this script from the root directory of WindowAppSDK repository
 
 @echo off
 
-REM Step 1: Rename the .nupkg file to .zip
-ren "BuildOutput\Microsoft.WindowsAppSDK.Foundation.TransportPackage.1.1.1.1.nupkg" "Microsoft.WindowsAppSDK.Foundation.TransportPackage.1.1.1.1.zip"
+REM if exist, delete "BuildOutput\Microsoft.WindowsAppSDK.Foundation.TransportPackage.1.1.1.1.zip"
+if exist "BuildOutput\Microsoft.WindowsAppSDK.Foundation.TransportPackage.1.1.1.1.zip" (
+    del "BuildOutput\Microsoft.WindowsAppSDK.Foundation.TransportPackage.1.1.1.1.zip"
+)
+
+REM Step 1: duplicate a .zip file from .nupkg
+copy "BuildOutput\Microsoft.WindowsAppSDK.Foundation.TransportPackage.1.1.1.1.nupkg" "BuildOutput\Microsoft.WindowsAppSDK.Foundation.TransportPackage.1.1.1.1.zip"
 
 REM Step 2: Clear content in the target directory
 rd /s /q "..\WindowsAppSDKAggregator\build\packages\Microsoft.WindowsAppSDK.Foundation.TransportPackage.1.1.1.1"
