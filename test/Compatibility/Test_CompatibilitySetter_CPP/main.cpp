1137c89f998c1687a01d6deb92eea65f89dd88a5
Mike Crider <45052236+codendone@users.noreply.github.com>
2025-01-31 08:58:10 -0800

    Add comment.

--- test/Compatibility/Test_CompatibilitySetter_CPP/main.cpp
+++ test/Compatibility/Test_CompatibilitySetter_CPP/main.cpp
@@ -13,6 +13,9 @@ int WINAPI WinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/, PSTR /*
 {
     // Note: This test app currently only tests that the compatibility properties specified in
     // the project file generates the Initialize() function and compiles successfully.
+    // ** THE CODE WILL NOT RUN SUCCESSFULLY ** since this test project does not have access to the real
+    //                                          WindowsAppSDK package and is also not using the test
+    //                                          Microsoft.WindowsAppRuntime.Framework package.
     Microsoft::Windows::ApplicationModel::WindowsAppRuntime::Compatibility::AutoInitialize::Initialize();
     // Apps using the <WindowsAppSDKRuntimePatchLevel*> or <WindowsAppSDKDisabledChanges> project properties should
     // NOT directly call the above function. This test calls it just to verify that the function is generated.
--- test/Compatibility/Test_CompatibilitySetter_CS/Program.cs
+++ test/Compatibility/Test_CompatibilitySetter_CS/Program.cs
@@ -9,6 +9,9 @@ class Program
     {
         // Note: This test app currently only tests that the compatibility properties specified in
         // the project file generates the ConfigureCompatibility() function and compiles successfully.
+        // ** THE CODE WILL NOT RUN SUCCESSFULLY ** since this test project does not have access to the real
+        //                                          WindowsAppSDK package and is also not using the test
+        //                                          Microsoft.WindowsAppRuntime.Framework package.
         Microsoft.Windows.ApplicationModel.WindowsAppRuntime.Compatibility.AutoInitialize.ConfigureRuntimeCompatibility();
         // Apps using the <WindowsAppSDKRuntimePatchLevel*> or <WindowsAppSDKDisabledChanges> project properties should
         // NOT directly call the above function. This test calls it just to verify that the function is generated.
