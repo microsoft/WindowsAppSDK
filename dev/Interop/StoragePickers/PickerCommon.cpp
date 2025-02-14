522a85a367905a7775d8c8e30c80e88c4afcdc03
Dinah Gao <x.kic8462852@gmail.com>
2025-02-14 16:40:33 +0800

    filesavepicker-autoapply-filetypechoices when extension's missing

--- dev/Interop/StoragePickers/PickerCommon.cpp
+++ dev/Interop/StoragePickers/PickerCommon.cpp
@@ -123,21 +123,11 @@ namespace PickerCommon {
     }
 
     // TODO: better way to convert ShellItem a StorageFile without relying on path?.
-    winrt::Windows::Foundation::IAsyncOperation<winrt::Windows::Storage::StorageFile> CreateStorageFileFromShellItem(winrt::com_ptr<IShellItem> shellItem, bool createFile)
+    winrt::Windows::Foundation::IAsyncOperation<winrt::Windows::Storage::StorageFile> CreateStorageFileFromShellItem(winrt::com_ptr<IShellItem> shellItem)
     {
         wil::unique_cotaskmem_string filePath;
         check_hresult(shellItem->GetDisplayName(SIGDN_FILESYSPATH, &filePath));
         auto pathStr = filePath.get();
-
-        if (createFile)
-        {
-            HANDLE hFile = CreateFile(pathStr, GENERIC_WRITE, 0, nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
-            if (hFile == INVALID_HANDLE_VALUE)
-            {
-                co_return nullptr;
-            }
-            CloseHandle(hFile);
-        }
 
         co_return co_await winrt::Windows::Storage::StorageFile::GetFileFromPathAsync(pathStr);
     }
