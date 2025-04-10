266016807d8067f3e3a347e756ce0ef1a1439d56
Chris Wall (WIN SDE) <chriwall@microsoft.com>
2025-04-10 11:25:51 -0600

    Add ABForward Tests

---
+++ test/ABForward/pch.h
@@ -0,0 +1,41 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+#ifndef PCH_H
+#define PCH_H
+#endif //PCH_H
+
+#include <unknwn.h>
+
+#include <windows.h>
+#include <sddl.h>
+#include <appmodel.h>
+
+#include <wil/result.h>
+#include <wil/cppwinrt.h>
+#include <wil/token_helpers.h>
+#include <wil/resource.h>
+#include <wil/filesystem.h>
+
+#include <WexTestClass.h>
+
+#include <string>
+
+#include <wil/result_macros.h>
+
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+
+#include <winrt/Windows.ApplicationModel.Activation.h>
+#include <winrt/Windows.Management.Deployment.h>
+#include <winrt/Windows.Storage.h>
+#include <winrt/Windows.System.h>
+#include <winrt/Microsoft.Windows.ApplicationModel.WindowsAppRuntime.h>
+#include <winrt/Microsoft.Windows.AppNotifications.h>
+#include <winrt/Microsoft.Windows.Management.Deployment.h>
+#include <winrt/Microsoft.Windows.Storage.h>
+#include <winrt/Microsoft.Windows.System.h>
+#include <winrt/Microsoft.Windows.System.Power.h>
+
+#include <WindowsAppRuntime.Test.Bootstrap.h>
+namespace TP = ::Test::Packages;
