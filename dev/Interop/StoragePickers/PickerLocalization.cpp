9e1af62f9e1e6a49adbe7fcc3e62e24dda8fa0db
Xiang Hong (from Dev Box) <xianghong@microsoft.com>
2025-07-11 11:28:05 +0800

    use raii for buffer and proper string length

--- dev/Interop/StoragePickers/PickerLocalization.cpp
+++ dev/Interop/StoragePickers/PickerLocalization.cpp
@@ -9,30 +9,41 @@
 #include "PickerLocalization.h"
 #include <winrt\base.h>
 #include <winrt\Microsoft.Windows.ApplicationModel.Resources.h>
+#include <vector>
 
 
 const winrt::hstring priFileName = L"Microsoft.WindowsAppRuntime.pri";
 winrt::hstring GetPriFilePath()
 {
     HMODULE hModule = nullptr;
+    // use GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT, the module handle does not need to be released
+    // GetPriFilePath should only be called once to initialize the static priFilePath variable, thus thread safe
     if (GetModuleHandleExW(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS | GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT,
         reinterpret_cast<LPCWSTR>(&GetPriFilePath), &hModule))
     {
-        wchar_t path[MAX_PATH];
-        if (GetModuleFileNameW(hModule, path, MAX_PATH) > 0)
+        auto buffer = std::make_unique<wchar_t[]>(MAX_PATH + 1);
+        auto pathLength = GetModuleFileName(hModule, buffer.get(), MAX_PATH + 1);
+        if (pathLength > 0)
         {
-            std::filesystem::path dllPath(path);
+            auto dllPathString = std::wstring(buffer.get(), pathLength);
+            std::filesystem::path dllPath(dllPathString);
+            if (!std::filesystem::is_regular_file(dllPath))
+            {
+                return priFileName;
+            }
             std::filesystem::path parentDir = dllPath.parent_path();
             auto priFilePath = parentDir / std::wstring{ priFileName };
-            auto priFilePathHstring = winrt::hstring{ priFilePath.wstring() };
-            return priFilePathHstring;
+            if (std::filesystem::exists(priFilePath))
+            {
+                return winrt::hstring{ priFilePath.wstring() };
+            }
         }
     }
     return priFileName;
 }
 
 namespace PickerLocalization {
-    
+
     winrt::hstring GetStoragePickersLocalizationText(winrt::hstring key, winrt::hstring fallback)
     {
         static winrt::hstring priPath = GetPriFilePath();
