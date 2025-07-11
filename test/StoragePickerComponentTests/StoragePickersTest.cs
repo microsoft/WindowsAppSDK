5ca9e362c2db825c5355724ee15aa7c67244c79d
Xiang Hong <hx.hongxiang@gmail.com>
2025-07-11 18:43:01 +0800

    try adding component package based test

---
+++ test/StoragePickerComponentTests/StoragePickersTest.cs
@@ -0,0 +1,30 @@
+using WEX.Logging.Interop;
+using WEX.TestExecution;
+using WEX.TestExecution.Markup;
+using Microsoft.Windows.Storage.Pickers;
+
+namespace StoragePickerComponentTests;
+
+[TestClass]
+public class StoragePickersTest
+{
+    [AssemblyInitialize]
+    // This attribute is used by GenerateHelixWorkItems.ps1 to control how it creates the work items.
+    // The default is CreateWorkItemPerModule
+    //[TestProperty("HelixWorkItemCreation", "CreateWorkItemPerTestClass")]
+    //[TestProperty("CoreClrProfile", "net5")]
+    //[TestProperty("CoreClrProfile", "TestNetv8.0")]
+
+    [TestProperty("IsolationLevel", "Class")]
+    [TestProperty("CoreClrProfile", "localDotNet")]
+    public static void AssemblyInitialize(TestContext testContext)
+    {
+        Log.Comment("AssemblyInitialize");
+    }
+
+    [TestMethod]
+    public void SimpleAPlusBTest()
+    {
+        Verify.AreEqual(2, 1 + 1);
+    }
+}
