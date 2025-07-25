c6252481b827bb628e8b85dca540aabafad71bd3
Howard Kapustein <howardk@microsoft.com>
2025-07-24 23:36:16 -0700

    Add PackageGraph APIs

--- dev/Common/AppModel.PackageGraph.h
+++ dev/Common/AppModel.PackageGraph.h
@@ -31,6 +31,44 @@ inline HRESULT GetCurrentPackageGraph(
     packageInfo = reinterpret_cast<PACKAGE_INFO*>(buffer.get());
     return S_OK;
 }
+
+/// Return the package information for the first package in a package family in the current process' package graph (if any)
+/// @param packageFamilyName the package family.
+/// @param packageGraphCount number of packages in packageGraph.
+/// @param packageGraph current packageGraph.
+/// @return pointer to the first package in the package family in the package graph, or NULL if not found.
+inline PACKAGE_INFO* GetPackageInFamily(PCWSTR packageFamilyName, uint32_t& packageGraphCount, wil::unique_cotaskmem_ptr<PACKAGE_INFO>& packageGraph)
+{
+    const UINT32 flags{ PACKAGE_FILTER_HEAD | PACKAGE_FILTER_DIRECT | PACKAGE_FILTER_STATIC | PACKAGE_FILTER_DYNAMIC | PACKAGE_INFORMATION_BASIC };
+    uint32_t packageGraphCount{};
+    const PACKAGE_INFO* packageInfo{};
+    RETURN_IF_FAILED(::AppModel::PackageGraph::GetCurrentPackageGraph(flags, packageGraphCount, packageInfo, packageGraph));
+    const auto frameworkPackageFamilyName{ ::WindowsAppRuntime::VersionInfo::Framework::GetPackageFamilyName() };
+    for (uint32_t index=0; index < packageGraphCount; ++index)
+    {
+        if (CompareStringOrdinal(packageInfo[index].packageFamilyName, -1, packageFamilyName, -1, TRUE) == CSTR_EQUAL)
+        {
+            // Found the Windows App SDK framework package in the package graph
+            return packageInfo + index;
+        }
+    }
+
+    // Didn't find the Windows App SDK framework package in the package graph
+    packageGraphCount = 0;
+    packageGraph.reset();
+    return nullptr;
+}
+
+/// Return the package information for the first package in a package family in the current process' package graph (if any)
+/// @param packageFamilyName the package family.
+/// @param buffer memory for the package graph information.
+/// @return pointer to the first package in the package family in the package graph, or NULL if not found.
+inline PACKAGE_INFO* GetPackageInFamily(PCSTR packageFamilyName, wil::unique_cotaskmem_ptr<PACKAGE_INFO>& buffer)
+{
+    uint32_t packageGraphCount{};
+    return GetFramework(packageFamilyName, packageGraphCount, buffer);
+}
+}
 }
 
 #endif // __APPMODEL_PACKAGEGRAPH_H
---
+++ dev/Common/WindowsAppRuntime.Package.h
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+#ifndef __MICROSFT_WINDOWSAPPRUNTIME_PACKAGE_H
+#define __MICROSFT_WINDOWSAPPRUNTIME_PACKAGE_H
+
+#if defined(__cplusplus)
+#include "AppModel.PackageGraph.h"
+
+namespace WindowsAppRuntime::Package
+{
+/// Return the package information for the Windows App Runtime runtime (framework) package in the current process' package graph (if any)
+/// @note This is a variant of WindowsAppRuntime_IsSelfContained() but returns package details instead of mere existence.
+///       You can think of WindowsAppRuntime_IsSelfContained() as a misnomer for `bool WindowsAppRuntime::PackageGraph::IsFrameworkPresent()`.
+/// @param packageGraphCount number of packages in packageGraph.
+/// @param packageGraph current packageGraph.
+/// @return pointer to the Windows App SDK runtime (framework) package in the package graph, or NULL if not found.
+inline PACKAGE_INFO* GetFrameworkPackageInfo(uint32_t& packageGraphCount, wil::unique_cotaskmem_ptr<PACKAGE_INFO>& packageGraph)
+{
+    const auto frameworkPackageFamilyName{ ::WindowsAppRuntime::VersionInfo::Framework::GetPackageFamilyName() };
+    return ::AppModel::PackageGraph::GetPackageInFamily(frameworkPackageFamilyName.c_str(), packageGraphCount, packageGraph);
+}
+
+/// Return the package information for the Windows App Runtime runtime (framework) package in the current process' package graph (if any)
+/// @note This is a variant of WindowsAppRuntime_IsSelfContained() but returns package details instead of mere existence.
+///       You can think of WindowsAppRuntime_IsSelfContained() as a misnomer for `bool WindowsAppRuntime::PackageGraph::IsFrameworkPresent()`.
+/// @param buffer memory for the package graph information.
+/// @return pointer to the Windows App SDK runtime (framework) package in the package graph, or NULL if not found.
+inline PACKAGE_INFO* GetFrameworkPackageInfo(wil::unique_cotaskmem_ptr<PACKAGE_INFO>& buffer)
+{
+    uint32_t packageGraphCount{};
+    return GetFrameworkPackageInfo(packageGraphCount, buffer);
+}
+}
+#endif // defined(__cplusplus)
+
+#endif // __MICROSFT_WINDOWSAPPRUNTIME_PACKAGE_H
