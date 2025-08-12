b046f76d15eac58a0db12abd0cae9652f5a8e0c2
alexlamtest <68841560+alexlamtest@users.noreply.github.com>
2025-08-11 17:44:04 -0700

    Update windows-sdk.ps1

--- build/scripts/windows-sdk.ps1
+++ build/scripts/windows-sdk.ps1
@@ -3,7 +3,7 @@
 #
 
 param(
-    # TODO: Use version, current hardwire to only support 2 specific versions.
+    # TODO: Use version, currently hardwire to only support 2 specific versions.
     [string]$SdkVersion = $null
 )
 
@@ -81,7 +81,7 @@ function PrintMessageAndExit($Message, $ReturnCode)
 # Main execution sequence
 #
 
-if ($SdkVersion -eq "10.0.17763")
+if ($SdkVersion -eq "10.1.17763")
 {
     # Requires Windows SDK with the same version number as the WDK
     $winSdkUrl = "https://go.microsoft.com/fwlink/p/?LinkID=2023014"
