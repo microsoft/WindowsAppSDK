bd32ba1aed48e142b3dd5d390c323a191084e27d
guimafelipe <guima.felipec@gmail.com>
2025-08-25 08:53:03 -0700

    Enabling Storage Picker tests

--- test/StoragePickersTests/PickerCommonTests.cpp
+++ test/StoragePickersTests/PickerCommonTests.cpp
@@ -339,8 +339,15 @@ namespace Test::PickerCommonTests
                     folderItem->GetDisplayName(SIGDN_FILESYSPATH, resultFolderPath.put());
                     winrt::hstring resultFolderPathString(resultFolderPath.get());
 
-                    message = L"Verify folder path of '" + folder + L"', expect: '" + folder + L"', actual: '" + resultFolderPathString + L"'";
-                    VERIFY_ARE_EQUAL(resultFolderPathString, expectConfig, message.c_str());
+                     // Do a case insensitive comparison for the folder path.
+                    std::wstring expectConfigLower{ expectConfig };
+                    std::wstring resultFolderPathLower{ resultFolderPathString };
+
+                    std::transform(expectConfigLower.begin(), expectConfigLower.end(), expectConfigLower.begin(), ::towlower);
+                    std::transform(resultFolderPathLower.begin(), resultFolderPathLower.end(), resultFolderPathLower.begin(), ::towlower);
+
+                    message = L"Verify folder path of '" + folder + L"', expect: '" + expectConfigLower + L"', actual: '" + resultFolderPathLower + L"'";
+                    VERIFY_ARE_EQUAL(resultFolderPathLower, expectConfigLower, message.c_str());
                 }
                 else
                 {
