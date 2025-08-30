9ed0f4608efacb13bb60dba1df418d2f1d639cba
guimafelipe <guima.felipec@gmail.com>
2025-08-30 11:07:23 -0700

    Revert "Increasing timeout"
    This reverts commit 2a5ec5bc2d352fe0efc865cc74400d6c1153da23.

--- test/AppNotificationTests/AppNotification-Test-Constants.h
+++ test/AppNotificationTests/AppNotification-Test-Constants.h
@@ -3,7 +3,7 @@
 #pragma once
 
 inline const winrt::hstring c_rawNotificationPayload = L"<toast></toast>";
-inline const std::chrono::milliseconds c_timeout{ 1500 };
+inline const std::chrono::milliseconds c_timeout{ 500 };
 inline IID c_toastComServerId = winrt::guid("1940dba9-0f64-4f0d-8a4b-5d207b812e61"); // Value from ToastNotificationsTestAppPackage ComActivator in appxmanifest.
 inline const std::wstring c_localWindowsAppSDKFolder{ LR"(\Microsoft\WindowsAppSDK\)" };
 inline const std::wstring c_pngExtension{ LR"(.png)" };
