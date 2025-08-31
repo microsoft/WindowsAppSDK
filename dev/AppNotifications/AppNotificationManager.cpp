40446f0905b3b0eaefb19ad740ab4a2d616497ee
guimafelipe <guima.felipec@gmail.com>
2025-08-31 01:01:09 -0700

    Revert "Testing supported thing"
    This reverts commit c2eda3a8ab27c9f7347fac5153782ddae6f7987f.

--- dev/AppNotifications/AppNotificationManager.cpp
+++ dev/AppNotifications/AppNotificationManager.cpp
@@ -498,7 +498,7 @@ namespace winrt::Microsoft::Windows::AppNotifications::implementation
     {
         if (!IsSupported())
         {
-            return AppNotificationSetting::DisabledForUser;
+            return AppNotificationSetting::Unsupported;
         }
 
         auto logTelemetry{ AppNotificationTelemetry::Setting::Start(g_telemetryHelper, m_appId) };
