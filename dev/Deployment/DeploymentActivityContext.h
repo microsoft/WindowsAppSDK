1b2c7a36bf62f994d90af6cb81d579a16bee6517
guimafelipe <guima.felipec@gmail.com>
2025-11-17 13:56:24 -0800

    Deleting copy constructor for activity context

--- dev/Deployment/DeploymentActivityContext.h
+++ dev/Deployment/DeploymentActivityContext.h
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation and Contributors.
+// Copyright (c) Microsoft Corporation and Contributors.
 // Licensed under the MIT License.
 
 #pragma once
@@ -41,6 +41,8 @@ namespace WindowsAppRuntime::Deployment::Activity
 
     public:
         static WindowsAppRuntime::Deployment::Activity::Context& Get();
+
+        Context(const Context&) = delete;
 
         void Reset();
 
