084f01c7743ef0854a7ef944730bf112497d02fc
guimafelipe <guima.felipec@gmail.com>
2025-11-17 16:39:14 -0800

    Revert "Making the context a proper singleton"
    This reverts commit 864e777473e309620c2b6784887b68cde3843fda.

--- dev/Deployment/DeploymentActivityContext.h
+++ dev/Deployment/DeploymentActivityContext.h
@@ -29,7 +29,6 @@ namespace WindowsAppRuntime::Deployment::Activity
 
     class Context
     {
-        static Context g_DeploymentActivityContext;
         DeploymentStage m_installStage{};
         std::wstring m_currentResourceId;
         HRESULT m_deploymentErrorExtendedHresult{};
@@ -39,10 +38,11 @@ namespace WindowsAppRuntime::Deployment::Activity
         WilFailure m_lastFailure;
         bool m_isFullTrustPackage{};
         bool m_useExistingPackageIfHigherVersion{};
-        Context() = default;
 
     public:
         static WindowsAppRuntime::Deployment::Activity::Context& Get();
+
+        Context() = default;
 
         Context(const Context&) = delete;
 
@@ -130,4 +130,6 @@ namespace WindowsAppRuntime::Deployment::Activity
             m_useExistingPackageIfHigherVersion = true;
         }
     };
+
+    static WindowsAppRuntime::Deployment::Activity::Context g_DeploymentActivityContext;
 }
