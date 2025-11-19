420c4223e991a7f10257468dcd6cde9c4085508c
Lauren Ciha <laurenciha@microsoft.com>
2025-11-19 15:36:44 -0800

    Add localization for NuGet Install Wizard for the VS templates (#5989)
    * Initial localization commit
    
    * Realign and add missing package messages to resources
    
    * Update s_resourceManager to _resourceManager
    
    * Lazy initialize ResourceManager to make it thread safe
    
    * Make Resources not static
    
    * Dev in progress, adding error messaging
    
    * Resources load without error
    
    * Capture localization dev/test state
    
    * Undo dev/test configuration and remove wait dialog error check
    
    * Resolve conflicting Designer file
    
    * Move VSPackage.Designer files back to Common (where resources are)
    
    * Update Cpp error message string to include where to find details
    
    * Include satellite assemblies
    
    * Reset source.extension.vsixmanifests
    
    * Remove WizardStringHelper
    
    * Clean up null check and spacing
    
    * Add copyright headers to VSPackage.Designer.cs files

--- dev/VSIX/Shared/WizardInfoBarEvents.cs
+++ dev/VSIX/Shared/WizardInfoBarEvents.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation and Contributors.
 // Licensed under the MIT License
 
+using System;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 
@@ -10,25 +11,66 @@ namespace WindowsAppSDK.TemplateUtilities
     {
         private readonly string _detailedErrorMessage;
 
-        public NuGetInfoBarUIEvents(string detailedErrorMessage = null)
+        public NuGetInfoBarUIEvents(string detailedErrorMessage)
         {
             _detailedErrorMessage = detailedErrorMessage;
         }
 
+        // This method handles clicks for the InfoBarHyperlinks in WizardImplmentation.cs' CreateNuGetInfoBar()
         public void OnActionItemClicked(IVsInfoBarUIElement infoBarUIElement, IVsInfoBarActionItem actionItem)
         {
             ThreadHelper.ThrowIfNotOnUIThread();
+
+            if (infoBarUIElement == null || actionItem == null)
+            {
+                OutputWindowHelper.ShowMessageInOutputWindow("Hyperlink not found");
+                return;
+            }
+
             if (actionItem is InfoBarHyperlink hyperlink)
             {
-                if (hyperlink.Text == "Manage NuGet Packages")
-                {
-                    var dte = ServiceProvider.GlobalProvider.GetService(typeof(EnvDTE.DTE)) as EnvDTE.DTE;
-                    dte?.ExecuteCommand("Project.ManageNuGetPackages");
-                }
-                else if (hyperlink.Text == "See error details")
+                // ActionContext can be any object, so we need to cast it to a string
+                string actionContext = hyperlink.ActionContext as string;
+
+                if (actionContext == "SeeErrorDetails")
                 {
                     ShowErrorDetails();
                 }
+                else if (actionContext == "ManageNuGetPackages")
+                {
+                    OpenNuGetPackageManager();
+                }
+            }
+        }
+
+        private void OpenNuGetPackageManager()
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            try
+            {
+                var dte = ServiceProvider.GlobalProvider.GetService(typeof(EnvDTE.DTE)) as EnvDTE.DTE;
+                if (dte == null)
+                {
+                    OutputWindowHelper.ShowMessageInOutputWindow("Could not obtain DTE service to open NuGet Package Manager.");
+                    return;
+                }
+
+                // Use the command GUID and ID instead of localized command name
+                // This is the GUID for NuGet Package Manager commands
+                // GUID: {25fd982b-8cae-4cbd-a440-e03ffccde106}
+                // ID: 0x100 for Tools.ManageNuGetPackagesforSolution
+                // https://devblogs.microsoft.com/dotnet/invoke-manage-nuget-packages-dialog-programmatically/
+                var guidString = "{25fd982b-8cae-4cbd-a440-e03ffccde106}";
+                var commandId = 0x100;
+
+                object customIn = null;
+                object customOut = null;
+                dte.Commands.Raise(guidString, commandId, ref customIn, ref customOut);
+            }
+            catch (Exception ex)
+            {
+                OutputWindowHelper.ShowMessageInOutputWindow($"Failed to open NuGet Package Manager: {ex.Message}");
             }
         }
 
