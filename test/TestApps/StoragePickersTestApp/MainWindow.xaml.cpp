02f028200c674636ce306e041e2f0988adb7ced3
Dinah Xiaomgao G <xiaomgao@microsoft.com>
2026-02-04 15:39:10 +0800

    StoragePickersTestApp init

---
+++ test/TestApps/StoragePickersTestApp/MainWindow.xaml.cpp
@@ -0,0 +1,24 @@
+#include "pch.h"
+#include "MainWindow.xaml.h"
+#if __has_include("MainWindow.g.cpp")
+#include "MainWindow.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::StoragePickersTestApp::implementation
+{
+    int32_t MainWindow::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainWindow::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+}
---
+++ test/TestApps/StoragePickersTestApp/MainWindow.xaml.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "MainWindow.g.h"
+
+namespace winrt::StoragePickersTestApp::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow()
+        {
+            // Xaml objects should not call InitializeComponent during construction.
+            // See https://github.com/microsoft/cppwinrt/tree/master/nuget#initializecomponent
+        }
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+    };
+}
+
+namespace winrt::StoragePickersTestApp::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
