7ec55351b5bed1fde64cce334fe92c895cfb52a2
Muyuan Li (from Dev Box) <muyuanli@microsoft.com>
2026-03-06 15:12:27 +0800

    Remove feature and keep only agents

--- dev/AppLifecycle/AppActivationArguments.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Microsoft Corporation and Contributors.
-// Licensed under the MIT License.
-
-#include <pch.h>
-#include "AppActivationArguments.h"
-#include <Microsoft.Windows.AppLifecycle.AppActivationArguments.g.cpp>
