9ae80b12be504234e4c48d5cc967058c3f0624b3
Howard Kapustein <howardk@microsoft.com>
2026-03-10 16:58:00 -0700

    Fix tests

--- test/ApplicationData/ApplicationDataTests.cpp
+++ test/ApplicationData/ApplicationDataTests.cpp
@@ -22,7 +22,6 @@ namespace Test::ApplicationData::Tests
     public:
         BEGIN_TEST_CLASS(ApplicationDataTests)
             TEST_CLASS_PROPERTY(L"ThreadingModel", L"MTA")
-            TEST_CLASS_PROPERTY(L"IsolationLevel", L"Class")
             TEST_CLASS_PROPERTY(L"RunAs", L"RestrictedUser")
         END_TEST_CLASS()
 
