6c48c726d3e0e33b7782bc4ce2b9c70a64ba4919
Dinah Xiaoman G <116714259+DinahK-2SO@users.noreply.github.com>
2026-03-11 01:53:11 +0800

    The Test App for Microsoft.Windows.Storage.Pickers (#6195)

---
+++ test/TestApps/StoragePickersTestApp/App.xaml.cpp
@@ -0,0 +1,42 @@
+// Copyright (c) Microsoft Corporation and Contributors. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#include "pch.h"
+#include "App.xaml.h"
+#include "MainWindow.xaml.h"
+
+#include <winrt/Microsoft.UI.Windowing.h>
+#include <winrt/Windows.Graphics.h>
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+
+namespace winrt::StoragePickersTestApp::implementation
+{
+    App::App()
+    {
+#if defined _DEBUG && !defined DISABLE_XAML_GENERATED_BREAK_ON_UNHANDLED_EXCEPTION
+        UnhandledException([](IInspectable const&, UnhandledExceptionEventArgs const& e)
+        {
+            if (IsDebuggerPresent())
+            {
+                auto errorMessage = e.Message();
+                __debugbreak();
+            }
+        });
+#endif
+    }
+
+    void App::OnLaunched([[maybe_unused]] LaunchActivatedEventArgs const& e)
+    {
+        window = make<MainWindow>();
+        window.as<MainWindow>()->InitializeDefaults();
+
+        if (auto appWindow = window.AppWindow())
+        {
+            appWindow.Resize(winrt::Windows::Graphics::SizeInt32{ 1800, 1220 });
+        }
+
+        window.Activate();
+    }
+}
