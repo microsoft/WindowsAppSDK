52da87743dcf577880684f2d0650699b6447c709
Scott Jones <sjones@microsoft.com>
2026-04-03 17:14:51 -0400

    ApplicationData: Refactor to dual-type pattern for packaged/unpackaged
    Replace runtime branching (if m_unpackagedApplicationData) with separate
    WinRT implementation types. Both ApplicationData and UnpackagedApplicationData
    now derive directly from ApplicationDataT, and the factory constructs the
    appropriate type. Same pattern applied to ApplicationDataContainer.
    
    This eliminates 17+ repeated if-checks, removes the shared_ptr indirection
    for containers, and makes each type self-contained with no branching.

--- dev/ApplicationData/M.W.S.ApplicationDataContainer.h
+++ dev/ApplicationData/M.W.S.ApplicationDataContainer.h
@@ -5,18 +5,12 @@
 
 #include "Microsoft.Windows.Storage.ApplicationDataContainer.g.h"
 
-namespace Microsoft::Windows::Storage
-{
-    struct UnpackagedApplicationDataContainer;
-}
-
 namespace winrt::Microsoft::Windows::Storage::implementation
 {
     struct ApplicationDataContainer : ApplicationDataContainerT<ApplicationDataContainer>
     {
         ApplicationDataContainer() = default;
         ApplicationDataContainer(winrt::Windows::Storage::ApplicationDataContainer const& value);
-        ApplicationDataContainer(std::shared_ptr<::Microsoft::Windows::Storage::UnpackagedApplicationDataContainer> const& value);
 
         winrt::Windows::Foundation::Collections::IMap<hstring, winrt::Microsoft::Windows::Storage::ApplicationDataContainer> Containers();
         hstring Name();
@@ -27,7 +21,6 @@ namespace winrt::Microsoft::Windows::Storage::implementation
         void DeleteContainer(hstring const& name);
 
     private:
-        std::shared_ptr<::Microsoft::Windows::Storage::UnpackagedApplicationDataContainer> m_unpackagedApplicationDataContainer;
         winrt::Windows::Storage::ApplicationDataContainer m_applicationDataContainer;
     };
 }
