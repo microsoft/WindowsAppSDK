c89826a7b6bff5d3a71b25f4b0d539f73c661ef5
Niels Laute <niels.laute@live.nl>
2026-04-08 03:51:41 +0200

    Update project template assets, layouts, and add TabView template (#6384)
    * Updated icons
    
    * Remove assets folder (it already pulls those from the other template)
    
    * Layout tweaks to the navview template
    
    * Adding a TabView template
    
    * Update the MvvM template with counters
    
    * Design tweaks for the TabView template
    
    * Update icon.png

---
+++ dev/VSIX/ProjectTemplates/Desktop/CSharp/TabViewApp/App.xaml.cs
@@ -0,0 +1,32 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.UI.Xaml;
+
+namespace $safeprojectname$;
+
+/// <summary>
+/// Provides application-specific behavior to supplement the default Application class.
+/// </summary>
+public partial class App : Application
+{
+    private Window? _window;
+
+    /// <summary>
+    /// Initializes the singleton application object.
+    /// </summary>
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    /// <summary>
+    /// Invoked when the application is launched.
+    /// </summary>
+    /// <param name="args">Details about the launch request and process.</param>
+    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        _window = new MainWindow();
+        _window.Activate();
+    }
+}
