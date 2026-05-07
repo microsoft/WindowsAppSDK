169ab2a44a66462c5253be9c9769d5c908d12182
Dinah Xiaoman G <116714259+DinahK-2SO@users.noreply.github.com>
2026-05-07 16:51:03 +0800

    Generic.xaml for templatedcontrol

--- dev/Templates/Source/ItemTemplates/Neutral/CSharp/TemplatedControl/CustomControl.cs
+++ dev/Templates/Source/ItemTemplates/Neutral/CSharp/TemplatedControl/CustomControl.cs
@@ -12,6 +12,14 @@ using Microsoft.UI.Xaml.Media;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
+// To add another templated control to this project (which already has a
+// Themes\Generic.xaml), run with `--IncludeDefaultStyle false` or `-I false`:
+//
+//     dotnet new winui-templatedcontrol -n NewName --IncludeDefaultStyle false
+//
+// Then open Themes\Generic.xaml and add a <Style TargetType="local:NewName">
+// block. Without it, the new control will render blank at runtime.
+
 namespace $rootnamespace$;
 
 public sealed partial class $safeitemname$ : Control
