eb11b094d05c4a230d707fbb1455cafab78791fa
Howard Kapustein <howardk@microsoft.com>
2026-06-16 15:18:12 -0700

    MSIX FindPackageFile*() API (#6200)
    Implementation of MSIX FindPackageFile*() API Spec#5885
    
    https://task.ms/59424502
    
    NOTE: SearchStatic/DynamicDependencies options are dropped from spec because Windows doesn't expose this information. Get[Current]PackageInfo*() accepts an input to optionally filter package graph entries but output doesn't indicate if a package was statically or dynamically added to the package graph.
    
    * GetPackageFile()
    
    * Added Package activatable class to manifests
    
    * More tests. Added dev\common to various test projects' Include path to fix compile error. Cleaned up (optimized) several test projects.
    
    * Fixed missing build dependencies
    
    * Added TAEF VERIFY_* support for std::filesystem::path. More fixes and test updates
    
    * Fixed warnings
    
    * Fix to dynamically use GetPackagePathByFullName2 if available
    
    * Can't use RuntimeBehavior/TrustLevel on old pre-20H1 systems. Switched to classic EntryPoint=windows.fullTrustApplication via UUAP:AppXManifest=PackagedCwaFullTrust
    
    * Fixed Decimal and decimal test
    
    * Fixed GetPackagePathByFullName2IfSupported to work on RS5
    
    * Incorporated feedback. Fixed telemetry
    
    * Fixed doc-comment typos
    
    ---------
    
    Co-authored-by: agniuks <41223743+agniuks@users.noreply.github.com>

---
+++ test/Package/API/dllmain.cpp
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+#include "pch.h"
+
+// Including this file once per binary will automatically opt WIL error handling macros into calling RoOriginateError when they
+// begin logging a new error.  This greatly improves the debuggability of errors that propagate before a failfast.
+#include <wil/result_originate.h>
+
+BOOL APIENTRY DllMain(HMODULE hmodule, DWORD  reason, LPVOID reserved)
+{
+    switch (reason)
+    {
+    case DLL_PROCESS_ATTACH:
+        DisableThreadLibraryCalls(hmodule);
+        break;
+    case DLL_PROCESS_DETACH:
+        break;
+    case DLL_THREAD_ATTACH:
+        break;
+    case DLL_THREAD_DETACH:
+        break;
+    }
+
+    // Give WIL a crack at DLLMain processing
+    // See DLLMain() in wil/result_macros.h for why
+    wil::DLLMain(hmodule, reason, reserved);
+
+    return TRUE;
+}
---
+++ test/inc/WindowsAppRuntime.Test.TAEF.stl.h
@@ -0,0 +1,104 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors. All rights reserved.
+// Licensed under the MIT License.
+
+#ifndef __WINDOWSAPPRUNTIME_TEST_TAEF_CPPWINRT_H
+#define __WINDOWSAPPRUNTIME_TEST_TAEF_CPPWINRT_H
+
+namespace WEX::TestExecution
+{
+    // Teach TAEF how to format a std::wstring
+    template <>
+    class VerifyOutputTraits<std::wstring>
+    {
+    public:
+        static WEX::Common::NoThrowString ToString(std::wstring const& value)
+        {
+            return WEX::Common::NoThrowString().Format(L"%s", s);
+        }
+    };
+
+    // Teach TAEF how to compare a std::wstring
+    template <>
+    class VerifyCompareTraits<std::wstring, std::wstring>
+    {
+    public:
+        static bool AreEqual(std::wstring const& expected, std::wstring const& actual)
+        {
+            return Compare(expected, actual) == 0;
+        }
+
+        static bool AreSame(std::wstring const& expected, std::wstring const& actual)
+        {
+            return &expected == &actual;
+        }
+
+        static bool IsLessThan(std::wstring const& expectedLess, std::wstring const& expectedGreater)
+        {
+            return Compare(expectedLess, expectedGreater) < 0;
+        }
+
+        static bool IsGreaterThan(std::wstring const& expectedGreater, std::wstring const& expectedLess)
+        {
+            return Compare(expectedGreater, expectedLess) > 0;
+        }
+
+        static bool IsNull(std::wstring const& object)
+        {
+            return object.c_str() == nullptr;
+        }
+    private:
+        static int Compare(std::wstring const& left, std::wstring const& right)
+        {
+            return CompareStringOrdinal(left.c_str(), -1, right.c_str(), -1, FALSE) - CSTR_EQUAL;
+        }
+    };
+
+    // Teach TAEF how to format a std::filesystem::path
+    template <>
+    class VerifyOutputTraits<std::filesystem::path>
+    {
+    public:
+        static WEX::Common::NoThrowString ToString(std::filesystem::path const& value)
+        {
+            return WEX::Common::NoThrowString().Format(L"%s", s);
+        }
+    };
+
+    // Teach TAEF how to compare a std::filesystem::path
+    template <>
+    class VerifyCompareTraits<std::filesystem::path, std::filesystem::path>
+    {
+    public:
+        static bool AreEqual(std::filesystem::path const& expected, std::filesystem::path const& actual)
+        {
+            return Compare(expected, actual) == 0;
+        }
+
+        static bool AreSame(std::filesystem::path const& expected, std::filesystem::path const& actual)
+        {
+            return &expected == &actual;
+        }
+
+        static bool IsLessThan(std::filesystem::path const& expectedLess, std::filesystem::path const& expectedGreater)
+        {
+            return Compare(expectedLess, expectedGreater) < 0;
+        }
+
+        static bool IsGreaterThan(std::filesystem::path const& expectedGreater, std::filesystem::path const& expectedLess)
+        {
+            return Compare(expectedGreater, expectedLess) > 0;
+        }
+
+        static bool IsNull(std::filesystem::path const& object)
+        {
+            return object.c_str() == nullptr;
+        }
+    private:
+        static int Compare(std::filesystem::path const& left, std::filesystem::path const& right)
+        {
+            return CompareStringOrdinal(left.c_str(), -1, right.c_str(), -1, FALSE) - CSTR_EQUAL;
+        }
+    };
+}
+
+#endif // __WINDOWSAPPRUNTIME_TEST_TAEF_CPPWINRT_H
