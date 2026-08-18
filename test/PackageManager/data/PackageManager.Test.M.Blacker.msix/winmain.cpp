cb42502bb3759c3df387b6d92c6f6ca497fe8880
Vineeth Thomas Alex <vithoma@microsoft.com>
2026-08-18 00:15:12 -0500

    PackageManager test apps: retry OpenEventW briefly before giving up

--- test/PackageManager/data/PackageManager.Test.M.Blacker.msix/winmain.cpp
+++ test/PackageManager/data/PackageManager.Test.M.Blacker.msix/winmain.cpp
@@ -17,7 +17,22 @@ int WINAPI WinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/, PSTR /*
     if (argc >= 2)
     {
         PCWSTR eventName{ argv[1] };
+
+        // OpenEventW can transiently fail to find the caller's event if this process starts
+        // running before that event's CreateEventW() has fully landed system-wide (e.g. broker/
+        // activation scheduling variance). A caller that relies on this process staying alive
+        // until it's signaled -- e.g. to hold a package "in use" for a deployment test -- would
+        // otherwise see this process exit almost instantly, with no visible symptom other than
+        // "the thing I expected to happen didn't happen". Retry briefly instead of failing on the
+        // very first attempt.
         wil::unique_event_nothrow endOfTheLine{ ::OpenEventW(SYNCHRONIZE, FALSE, eventName) };
+        constexpr DWORD c_openEventRetryBudgetMilliseconds{ 5000 };
+        constexpr DWORD c_openEventRetryIntervalMilliseconds{ 50 };
+        for (DWORD elapsedMilliseconds = 0; !endOfTheLine && (elapsedMilliseconds < c_openEventRetryBudgetMilliseconds); elapsedMilliseconds += c_openEventRetryIntervalMilliseconds)
+        {
+            ::Sleep(c_openEventRetryIntervalMilliseconds);
+            endOfTheLine.reset(::OpenEventW(SYNCHRONIZE, FALSE, eventName));
+        }
         RETURN_LAST_ERROR_IF_NULL(endOfTheLine);
 
         auto rc{ WaitForSingleObject(endOfTheLine.get(), INFINITE) };
