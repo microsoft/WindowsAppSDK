0b1cc11b90dcfe47d8335bd0ad6f026f82d91520
Roman Sivak (AKVELON INC) <v-romansivak@microsoft.com>
2026-08-21 13:34:19 +0200

    [Validation] Add Foundation PoliCheck canary
    Add a temporary source file with controlled PoliCheck terminology to validate external repository scan coverage.

---
+++ PoliCheckValidationCanary-Foundation.cpp
@@ -0,0 +1,7 @@
+// PoliCheck validation canary for WindowsAppSDK Foundation.
+// This temporary file intentionally contains legacy terminology for scanner validation:
+// blacklist
+// whitelist
+// master/slave
+// dummy
+// sanity check
