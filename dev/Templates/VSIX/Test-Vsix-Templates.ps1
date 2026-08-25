5df93d29d534c03db9d9fda19ca4e365adf14acc
Lauren Ciha <laurenciha@microsoft.com>
2026-08-25 09:35:43 -0700

    Update VS and template search to path and add localdev template validation

--- dev/Templates/VSIX/Test-Vsix-Templates.ps1
+++ dev/Templates/VSIX/Test-Vsix-Templates.ps1
@@ -1,33 +1,47 @@
+# You must keep your mouse/keyboard still while script is running
+
 # Check VS 2026 is installed on the machine
 # VS 2026 appears as "18". The alternative would be "2022", but the templates are only
 # updated for VS 2026 unless it is a hot bug
-$vs2026Path = "C:\Program Files\Microsoft Visual Studio\18"
-if (-not (Test-Path $vs2026Path -PathType Container))
+$vswhere = "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe"
+
+$installPath = & $vswhere -version "[18.0,19.0)" -latest -property installationPath
+$devenv = Join-Path $installPath 'Common7\IDE\devenv.exe'
+
+if (-not (Test-Path $devenv -PathType Leaf))
 {
     Write-Host "Visual Studio 2026 is not installed. Please install it before running this script."
     exit
 }
-cd $vs2026Path
+Write-Host "Visual Studio 2026 is installed at: $installPath"
+Write-Host "Devenv path: $devenv"
 
-# LocalDev is installed per-user at $env:LocalAppData\Microsoft\VisualStudio\<version>\Extensions\<random>.ap0
-$LocalDevVsixPath = $env:localappdata + "\Microsoft\VisualStudio"
-if (-not (Test-Path $LocalDevVsixPath -PathType Container))
+# LocalDev is installed per-user at $env:LocalAppData\Microsoft\VisualStudio\<version>\Extensions\<random>
+# TODO: Add C++ LocalDev VSIX DLL search
+$localDevCsVsixDll = Get-ChildItem -Path "$env:localappdata" -Filter 'WindowsAppSDK.Cs.Extension.Dev17.dll' -File -Recurse | Select-Object -ExpandProperty FullName
+
+if (-not (Test-Path $localDevCsVsixDll -PathType Leaf))
 {
-    Write-Host "LocalDev VSIX folder not found. Please ensure that the templates are installed."
+    Write-Host "LocalDev CS VSIX DLL is not installed. Please install it before running this script using dev/templates/VSIX/build-local-vsix-package/build-install-localdev-vsix.ps1."
     exit
 }
-cd $LocalDevVsixPath
-Write-Host "LocalDev VSIX folder found at: $LocalDevVsixPath"
+Write-Host "LocalDev CS VSIX DLL is installed at: $localDevCsVsixDll"
 
-# Get the version number
-# Options: 17, 18, and the specific version number of the user's install of Visual Studio (e.g. 17.0_acd2bae8)
-# The extensions are in that full version
-# We're using VS 2026, so we need to find the 18.x version
-if (-not (Test-Path "$LocalDevVsixPath\18.*" -PathType Container))
+# Verify the VSIX there is localdev (it should be by install path, but this proves it)
+$vsixManifestPath = Get-ChildItem -Path (Split-Path $localDevCsVsixDll) -Filter 'extension.vsixmanifest' -File -Recurse | Select-Object -ExpandProperty FullName
+if (-not (Test-Path $vsixManifestPath -PathType Leaf))
 {
-    Write-Host "No Visual Studio 2026 LocalDev VSIX folder found. Please ensure that the templates are installed."
+    Write-Host "VSIX manifest is not found. Please ensure the LocalDev VSIX is correctly installed."
     exit
 }
+Write-Host "VSIX manifest is located at: $vsixManifestPath"
+
+if (-not (sls -Path $vsixManifestPath -Pattern 'LocalDev'))
+{
+    Write-Host "LocalDev is not referenced in the VSIX manifest. Please ensure the LocalDev VSIX is correctly installed."
+    exit
+}
+Write-Host "LocalDev is referenced in the VSIX manifest."
 
 $vs2026LocalDataVersions = Get-ChildItem -Path $LocalDevVsixPath -Directory | Where-Object { $_.Name -like "18.*" } | Select-Object -First 2
 $maximumVersion = [version]$vs2026LocalDataVersions[0].Name
