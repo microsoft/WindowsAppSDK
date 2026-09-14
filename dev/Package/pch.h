e7ac234e73047d5e9bd9ed158f2468f8757029fa
Howard Kapustein <howardk@microsoft.com>
2026-09-14 09:55:07 -0400

    [2.x Servicing] Add new 'GetPackage[Graph]File` APIs (#6575)
    * MSIX FindPackageFile*() API (#6200)
    
    Implementation of MSIX FindPackageFile*() API Spec#5885
    
    https://task.ms/59424502
    
    NOTE: SearchStatic/DynamicDependencies options are dropped from spec because Windows doesn't expose this information. Get[Current]PackageInfo*() accepts an input to optionally filter package graph entries but output doesn't indicate if a package was statically or dynamically added to the package graph.
    
    * GetPackageFile()
    
    * Added Package activatable class to manifests
    
    * More tests. Added dev\common to various test projects' Include path to fix compile error. Cleaned up (optimized) several test projects.
    
    * Fixed missing build dependencies
    
    * Added TAEF VERIFY_* support for std::filesystem::path. More fixes and test updates
    
    * Fixed warnings
    
    * Fix to dynamically use GetPackagePathByFullName2 if available
    
    * Can't use RuntimeBehavior/TrustLevel on old pre-20H1 systems. Switched to classic EntryPoint=windows.fullTrustApplication via UUAP:AppXManifest=PackagedCwaFullTrust
    
    * Fixed Decimal and decimal test
    
    * Fixed GetPackagePathByFullName2IfSupported to work on RS5
    
    * Incorporated feedback. Fixed telemetry
    
    * Fixed doc-comment typos
    
    ---------
    
    Co-authored-by: agniuks <41223743+agniuks@users.noreply.github.com>
    
    * [2.x Servicing][WASDK] Add new 'GetPackage[Graph]File` APIs
    
    * Fixed containment references
    
    * Fix IsPackageFeatureSupported and update RuntimeCompatibilityOptions for vNext servicing update
    
    * Gotta copy FrameworkUdk.dll to $(OutDir) to satisfy tests running on RS5 w/Containment
    
    ---------
    
    Co-authored-by: agniuks <41223743+agniuks@users.noreply.github.com>

---
+++ dev/Package/pch.h
@@ -0,0 +1,32 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include <Windows.h>
+
+#include <unknwn.h>
+#include <appmodel.h>
+
+#include <filesystem>
+#include <thread>
+#include <mutex>
+
+#include <wil/cppwinrt.h>
+#include <wil/filesystem.h>
+#include <wil/registry.h>
+#include <wil/stl.h>
+#include <wil/resource.h>
+#include <wil/result_macros.h>
+#include <wil/token_helpers.h>
+
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+
+#include <winrt/Windows.ApplicationModel.h>
+#include <winrt/Windows.Storage.h>
+#include <winrt/Windows.System.h>
+
+#include <appmodel.identity.h>
+#include <appmodel.package.h>
+#include <microsoft.foundation.string.h>
